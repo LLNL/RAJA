abb98e8066d498833c0e4f0af1ee202073176fac
Jeff Keasler <keasler@llnl.gov>
2014-02-10 14:27:05 -0800

    Correct comment in FT script

--- test/LULESH-v1.0/sigcatch.cmd
+++ test/LULESH-v1.0/sigcatch.cmd
@@ -6,7 +6,7 @@
 echo -n 'The start time is: ';date
 # trap "echo trap" SIGUSR1
 # On rzmerl, % sbatch sigcatch.cmd
-# % scancel -s USR1  <job_id>     do this repeatedly to simulate faults
+# % scancel -s USR2  <job_id>     do this repeatedly to simulate faults
 srun /g/g19/keasler/RAJA_RICH/raja/test/LULESH-v1.0/lulesh-RAJA-parallel-ft.exe
 echo -n 'The completion time is: ';date
 
