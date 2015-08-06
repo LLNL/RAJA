591841ad3aa44c02fcd9500dcdabbd40422beb99
Rich Hornung <hornung1@llnl.gov>
2015-08-06 07:08:15 -0700

    new reduction operations for min and sum (omp and seq only so far).

---
+++ test/reduce/buildIndexSet.hxx
@@ -0,0 +1,10 @@
+//
+// Header file defining methods that build index sets for testing...
+//
+
+#include "RAJA/RAJA.hxx"
+
+//
+//  Initialize index set by adding segments as indicated by enum value.
+//
+void buildIndexSet(RAJA::IndexSet& hindex);
