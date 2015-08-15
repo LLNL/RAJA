c9ffe7ff3c8efa42309f0b0c3714bce6b05c0cff
Rich Hornung <hornung1@llnl.gov>
2015-08-15 10:26:00 -0700

    Fixed issue with CUDA min reduction (hopefully for good) and bolstered test to make it more complete.

--- test/reduce-CUDA/ReduceMin.cu
+++ test/reduce-CUDA/ReduceMin.cu
@@ -11,26 +11,19 @@ using namespace RAJA;
 
 int main(int argc, char *argv[])
 {
-   double BIGMIN = -500.0;
-   ReduceMin<cuda_reduce, double> dmin0(50.0);
-   ReduceMin<cuda_reduce, int>    dmin1(50);
-   ReduceMin<cuda_reduce, double> dmin2(BIGMIN);
-   ReduceMin<cuda_reduce, int>    dmin3(BIGMIN);
+   double BIG_MIN = -500.0;
+   ReduceMin<cuda_reduce, double> dmin0(DBL_MAX);
+   ReduceMin<cuda_reduce, double> dmin1(DBL_MAX);
+   ReduceMin<cuda_reduce, double> dmin2(BIG_MIN);
 
    double *dvalue ;
-   int *ivalue ;
 
    cudaMallocManaged((void **)&dvalue,
       sizeof(double)*TEST_VEC_LEN,
       cudaMemAttachGlobal) ;
 
-   cudaMallocManaged((void **)&ivalue,
-      sizeof(int)*TEST_VEC_LEN,
-      cudaMemAttachGlobal) ;
-
    for (int i=0; i<TEST_VEC_LEN; ++i) {
       dvalue[i] = DBL_MAX ;
-      ivalue[i] = INT_MAX ;
    }
 
 
@@ -39,33 +32,23 @@ int main(int argc, char *argv[])
    std::uniform_real_distribution<double> dist(-10, 10);
    std::uniform_real_distribution<double> dist2(0, TEST_VEC_LEN-1);
 
-// int loops = 16;
-   int loops = 1;
+   int loops = 16;
    double dcurrentMin = DBL_MAX;
-   int icurrentMin = INT_MAX;
 
    for(int k=0; k < loops ; k++) {
-#if 0 // RDH
       double droll = dist(mt) ; 
-#else
-      double droll = -100.0;
-      int iroll = -100;
-#endif
       int index = int(dist2(mt));
       dvalue[index] = droll;
-      ivalue[index] = iroll;
       dcurrentMin = RAJA_MIN(dcurrentMin,dvalue[index]); 
-      icurrentMin = RAJA_MIN(icurrentMin,ivalue[index]); 
       forall<cuda_exec>(0, TEST_VEC_LEN, [=] __device__ (int i) {
          dmin0.min(dvalue[i]) ;
-         dmin1.min(ivalue[i]) ;
-         dmin2.min(2*dvalue[i]) ;
-         dmin3.min(2*ivalue[i]) ;
+         dmin1.min(2*dvalue[i]) ;
+         dmin2.min(dvalue[i]) ;
       } ) ;
-      printf("(droll[%d] = %lf, \t check0 = %lf \t check2 = %lf) \t dmin0 = %lf \t dmin2 = %lf\n",index,droll,
-         (double)dcurrentMin, BIGMIN,double(dmin0),double(dmin2));
-      printf("\n(iroll[%d] = %d, \t check1 = %d \t check3 = %d) \t dmin1 = %d \t dmin3 = %d\n",index,iroll,
-         (int)icurrentMin, (int)BIGMIN,int(dmin1),int(dmin3));
+      printf("(droll[%d] = %lf, \t check0 = %lf \t check1= %lf) \t dmin0 = %lf \t dmin1 = %lf\n",
+         index,droll,dcurrentMin,2*dcurrentMin,double(dmin0),double(dmin1));
+      printf("(check2 = %lf) \t dmin2 = %lf\n\n",
+             BIG_MIN,double(dmin2));
    }
    
    return 0 ;
