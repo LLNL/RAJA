bbe9668165ab77eb0b3e1427e88438dd3159209f
Jeff Keasler <keasler@llnl.gov>
2015-12-02 16:50:06 -0800

    Normalize LULESH v1.0 and LULESH v2.0.

--- test/LULESH-v1.0/LULESH-v1.0_RAJA-variants/luleshPolicy.hxx
+++ test/LULESH-v1.0/LULESH-v1.0_RAJA-variants/luleshPolicy.hxx
@@ -1,6 +1,21 @@
 // This work was performed under the auspices of the U.S. Department of Energy by
 // Lawrence Livermore National Laboratory under Contract DE-AC52-07NA27344.
 //
+
+//
+//   Tiling modes for different exeuction cases (see luleshPolicy.hxx).
+//
+enum TilingMode
+{
+   Canonical,       // canonical element ordering -- single range segment
+   Tiled_Index,     // canonical ordering, tiled using unstructured segments
+   Tiled_Order,     // elements permuted, tiled using range segments
+   Tiled_LockFree,  // tiled ordering, lock-free
+   Tiled_LockFreeColor,     // tiled ordering, lock-free, unstructured
+   Tiled_LockFreeColorSIMD  // tiled ordering, lock-free, range
+};
+
+
 // Use cases for RAJA execution patterns:
 
 #define USE_CASE 5
@@ -27,14 +42,15 @@
 // ----------------------------------------------------
 #if USE_CASE == 1 
 
-TilingMode lulesh_tiling_mode = Canonical;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::simd_exec> node_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::simd_exec> elem_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::simd_exec> mat_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::simd_exec> symnode_exec_policy;
+TilingMode const lulesh_tiling_mode = Canonical;
 
 typedef RAJA::seq_segit              Hybrid_Seg_Iter;
 typedef RAJA::simd_exec              Segment_Exec;
+
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> node_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> elem_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> mat_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> symnode_exec_policy;
 
 typedef RAJA::seq_reduce reduce_policy; 
 
@@ -46,14 +62,15 @@ typedef RAJA::seq_reduce reduce_policy;
 
 // AllocateTouch should definitely be used
 
-TilingMode lulesh_tiling_mode = Canonical;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> node_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> elem_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> mat_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> symnode_exec_policy;
+TilingMode const lulesh_tiling_mode = Canonical;
 
 typedef RAJA::seq_segit              Hybrid_Seg_Iter;
 typedef RAJA::omp_parallel_for_exec  Segment_Exec;
+
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> node_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> elem_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> mat_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> symnode_exec_policy;
 
 typedef RAJA::omp_reduce reduce_policy;
 
@@ -66,14 +83,15 @@ typedef RAJA::omp_reduce reduce_policy;
 // Only use AllocateTouch if tiling is imposed on top of a block decomposition,
 // and that block decomposition is the indexset used for the first touch (see CreateMaskedIndexSet)
 
-TilingMode lulesh_tiling_mode = Tiled_Index;
+TilingMode const lulesh_tiling_mode = Tiled_Index;
+
+typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
+typedef RAJA::simd_exec               Segment_Exec;
+
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  node_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_parallel_for_segit, RAJA::simd_exec> elem_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_parallel_for_segit, RAJA::simd_exec> mat_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  symnode_exec_policy;
-
-typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
-typedef RAJA::simd_exec               Segment_Exec;
 
 typedef RAJA::omp_reduce reduce_policy; 
 
@@ -85,14 +103,15 @@ typedef RAJA::omp_reduce reduce_policy;
 
 // AllocateTouch should definitely be used
 
-TilingMode lulesh_tiling_mode = Tiled_Order;
+TilingMode const lulesh_tiling_mode = Tiled_Order;
+
+typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
+typedef RAJA::simd_exec               Segment_Exec;
+
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  node_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_parallel_for_segit, RAJA::simd_exec> elem_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_parallel_for_segit, RAJA::simd_exec> mat_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  symnode_exec_policy;
-
-typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
-typedef RAJA::simd_exec               Segment_Exec;
 
 typedef RAJA::omp_reduce reduce_policy; 
 
@@ -106,14 +125,15 @@ typedef RAJA::omp_reduce reduce_policy;
 // In reality, only the "lock-free" operations need to use the dependence graph embedded in the
 // lock-free indexset, and the dependence-graph should likely be deactivated for other operations.
 
-TilingMode lulesh_tiling_mode = Tiled_LockFree;
+TilingMode const lulesh_tiling_mode = Tiled_LockFree;
+
+typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
+typedef RAJA::simd_exec               Segment_Exec;
+
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  node_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_taskgraph_segit, RAJA::simd_exec> elem_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::omp_taskgraph_segit, RAJA::simd_exec> mat_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec>  symnode_exec_policy;
-
-typedef RAJA::omp_parallel_for_segit  Hybrid_Seg_Iter;
-typedef RAJA::simd_exec               Segment_Exec;
 
 typedef RAJA::omp_reduce reduce_policy; 
 
@@ -124,14 +144,15 @@ typedef RAJA::omp_reduce reduce_policy;
 
 // AlocateTouch use is very tricky with this lockfree indexset.
 
-TilingMode lulesh_tiling_mode = Tiled_LockFreeColor;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> node_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> elem_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> mat_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> symnode_exec_policy;
+TilingMode const lulesh_tiling_mode = Tiled_LockFreeColor;
 
 typedef RAJA::seq_segit              Hybrid_Seg_Iter;
 typedef RAJA::omp_parallel_for_exec  Segment_Exec;
+
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, RAJA::omp_parallel_for_exec> node_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, RAJA::omp_parallel_for_exec> elem_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, RAJA::omp_parallel_for_exec> mat_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, RAJA::omp_parallel_for_exec> symnode_exec_policy;
 
 typedef RAJA::omp_reduce reduce_policy; 
 
@@ -142,15 +163,15 @@ typedef RAJA::omp_reduce reduce_policy;
 
 // AlocateTouch use is very tricky with this lockfree indexset.
 
-TilingMode lulesh_tiling_mode = Tiled_LockFreeColorSIMD;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> node_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> elem_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> mat_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::seq_segit, RAJA::omp_parallel_for_exec> symnode_exec_policy;
-
+TilingMode const lulesh_tiling_mode = Tiled_LockFreeColorSIMD;
 
 typedef RAJA::seq_segit              Hybrid_Seg_Iter;
 typedef RAJA::omp_parallel_for_exec  Segment_Exec;
+
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> node_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> elem_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> mat_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> symnode_exec_policy;
 
 typedef RAJA::omp_reduce reduce_policy; 
 
@@ -162,14 +183,15 @@ typedef RAJA::omp_reduce reduce_policy;
 
 // AllocateTouch should definitely be used
 
-TilingMode lulesh_tiling_mode = Canonical;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::cilk_for_segit, RAJA::cilk_for_exec> node_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::cilk_for_segit, RAJA::cilk_for_exec> elem_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::cilk_for_segit, RAJA::cilk_for_exec> mat_exec_policy;
-typedef LULESH_INDEXSET::ExecPolicy<RAJA::cilk_for_segit, RAJA::cilk_for_exec> symnode_exec_policy;
+TilingMode const lulesh_tiling_mode = Canonical;
 
 typedef RAJA::cilk_for_segit         Hybrid_Seg_Iter;
 typedef RAJA::cilk_for_exec          Segment_Exec;
+
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> node_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> elem_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> mat_exec_policy;
+typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> symnode_exec_policy;
 
 typedef RAJA::cilk_reduce            reduce_policy ;
 
@@ -179,7 +201,7 @@ typedef RAJA::cilk_reduce            reduce_policy ;
 // Requires OMP_HACK 
 #define OMP_HACK 1
 
-TilingMode lulesh_tiling_mode = Canonical;
+TilingMode const lulesh_tiling_mode = Canonical;
 
 typedef RAJA::seq_segit         Hybrid_Seg_Iter;
 typedef RAJA::cuda_exec         Segment_Exec;
@@ -196,15 +218,15 @@ typedef RAJA::cuda_reduce reduce_policy;
 
 // Can be used with or without OMP_HACK; without will have less data movement and memory use
 
+TilingMode const lulesh_tiling_mode = Tiled_LockFreeColorSIMD;
+
 typedef RAJA::seq_segit         Hybrid_Seg_Iter;
 typedef RAJA::cuda_exec         Segment_Exec;
 
-TilingMode lulesh_tiling_mode = Tiled_LockFreeColorSIMD;
 typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> node_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> elem_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> mat_exec_policy;
 typedef LULESH_INDEXSET::ExecPolicy<Hybrid_Seg_Iter, Segment_Exec> symnode_exec_policy;
-
 
 typedef RAJA::cuda_reduce reduce_policy; 
 
