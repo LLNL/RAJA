4a5eaeb109004c9eb9f2d6331c763a7a825f50b3
Jeff Keasler <keasler@llnl.gov>
2015-12-04 12:55:11 -0800

    Normalize treatment of MAX macro.

--- test/LULESH-v2.0/LULESH-v2.0_RAJA-variants/LULESH-v2.0_RAJA-IndexSet/lulesh.h
+++ test/LULESH-v2.0/LULESH-v2.0_RAJA-variants/LULESH-v2.0_RAJA-IndexSet/lulesh.h
@@ -3,6 +3,8 @@
 #include "luleshPolicy.hxx"
 #include "luleshMemory.hxx"
 
+#define MAX(a, b) ( ((a) > (b)) ? (a) : (b))
+
 #if !defined(LULESH_HEADER)
 #include "lulesh_stl.h"
 #elif (LULESH_HEADER == 1)
