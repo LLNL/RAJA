cfc938a2bce5fa4f46ad3a8d96a127f715428c6a
David Beckingsale <davidbeckingsale@gmail.com>
2016-01-21 14:41:19 -0800

    Add GNU host config for gcc-4.9.3

--- host-configs/chaos/gcc.cmake
+++
@@ -1,8 +0,0 @@
-set(RAJA_PLATFORM RAJA_PLATFORM_X86_AVX)
-set(RAJA_COMPILER RAJA_COMPILER_GNU)
-
-if(CMAKE_BUILD_TYPE MATCHES Opt)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -mavx -finline-functions -finline-limit=20000 -std=c++0x")
-elseif(CMAKE_BUILD_TYPE MATCHES Debug)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fpermissive -std=c++0x")
-endif()
---
+++ host-configs/chaos/gnu_4_9_3.cmake
@@ -0,0 +1,15 @@
+set(RAJA_PLATFORM "RAJA_PLATFORM_X86_AVX" CACHE STRING "")
+set(RAJA_COMPILER "RAJA_COMPILER_GNU" CACHE STRING "")
+
+set(CMAKE_C_COMPILER "/usr/apps/gnu/4.9.3/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.9.3/bin/g++" CACHE PATH "")
+
+message(${CMAKE_BUILD_TYPE})
+
+if(CMAKE_BUILD_TYPE MATCHES Release)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -mavx -finline-functions -finline-limit=20000 -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -mavx -finline-functions -finline-limit=20000 -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES Debug)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fpermissive -std=c++0x" CACHE STRING "")
+endif()
