07ca8d9a68958050b51bd9f9873e19feca5fd964
Rich Hornung <hornung1@llnl.gov>
2016-03-15 12:34:14 -0700

    Broke out thread utils into source and header files to fix build issue.

--- src/ThreadUtils_CPU.cxx
+++ src/ThreadUtils_CPU.cxx
@@ -13,30 +13,46 @@
  *
  * \file
  *
- * \brief   Header file defining prototypes for routines used to manage
+ * \brief   Implementation file for routines used to manage
  *          CPU threading operations.
  *
  ******************************************************************************
  */
 
-#ifndef RAJA_ThreadUtils_CPU_HXX
-#define RAJA_ThreadUtils_CPU_HXX
+#include "RAJA/ThreadUtils_CPU.hxx"
 
-#include "config.hxx"
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
+#if defined(RAJA_USE_CILK)
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#endif
 
 namespace RAJA {
 
-/*!
+/*
 *************************************************************************
 *
 * Return max number of available threads for code run on CPU.
 *
 *************************************************************************
 */
-int getMaxThreadsCPU(); 
+int getMaxThreadsCPU()
+{
+   int nthreads = 1;
+
+#if defined(_OPENMP)
+   nthreads = omp_get_max_threads();
+#endif
+#if defined(RAJA_USE_CILK)
+   int nworkers = __cilkrts_get_nworkers();
+   nthreads = std::max(nthreads, nworkers);
+#endif
+
+   return nthreads;
+}
 
 
 }  // closing brace for RAJA namespace
-
-
-#endif  // closing endif for header file include guard
