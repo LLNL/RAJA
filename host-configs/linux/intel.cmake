a5b7972a63313f55caf6346fa828703aa1d72440
David Beckingsale <davidbeckingsale@gmail.com>
2016-03-23 15:35:08 -0700

    Added generic 'linux' host configs

--- host-configs/linux/intel.cmake
+++ host-configs/linux/intel.cmake
@@ -1,6 +1,6 @@
 set(RAJA_COMPILER "RAJA_COMPILER_ICC" CACHE STRING "")
 
-set(CMAKE_CXX_COMPILER "/usr/local/bin/icpc-16.0.109" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "icpc" CACHE PATH "")
 
 if(CMAKE_BUILD_TYPE MATCHES Release)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -mavx -inline-max-total-size=20000 -inline-forceinline -ansi-alias -std=c++0x" CACHE STRING "")
