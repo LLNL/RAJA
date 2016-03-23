968c07936189993b98414c8f3dd3840eaf13366b
David Beckingsale <davidbeckingsale@gmail.com>
2016-03-23 11:32:46 -0700

    Add default, generic Intel and GNU configs

--- host-configs/chaos/gnu.cmake
+++ host-configs/chaos/gnu.cmake
@@ -1,1 +1,20 @@
-gnu_4_9_3.cmake
+set(RAJA_COMPILER "RAJA_COMPILER_GNU" CACHE STRING "")
+
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+
+message(${CMAKE_BUILD_TYPE})
+
+if(CMAKE_BUILD_TYPE MATCHES Release)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -mavx -finline-functions -finline-limit=20000 -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast -mavx -finline-functions -finline-limit=20000 -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES Debug)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fpermissive -std=c++0x" CACHE STRING "")
+endif()
+
+set(RAJA_USE_OPENMP On CACHE BOOL "")
+
+set(RAJA_RANGE_ALIGN 4 CACHE INT "")
+set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
+set(RAJA_DATA_ALIGN 64 CACHE INT "")
+set(RAJA_COHERENCE_BLOCK_SIZE 64 CACHE INT "")
--- host-configs/chaos/intel.cmake
+++ host-configs/chaos/intel.cmake
@@ -1,1 +1,19 @@
-intel_16_0_109.cmake
+set(RAJA_COMPILER "RAJA_COMPILER_ICC" CACHE STRING "")
+
+set(CMAKE_CXX_COMPILER "icpc" CACHE PATH "")
+
+if(CMAKE_BUILD_TYPE MATCHES Release)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -mavx -inline-max-total-size=20000 -inline-forceinline -ansi-alias -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -mavx -inline-max-total-size=20000 -inline-forceinline -ansi-alias -std=c++0x" CACHE STRING "")
+elseif(CMAKE_BUILD_TYPE MATCHES Debug)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -std=c++0x" CACHE STRING "")
+endif()
+
+set(RAJA_USE_OPENMP On CACHE BOOL "")
+set(RAJA_USE_CILK On CACHE BOOL "")
+
+set(RAJA_RANGE_ALIGN 4 CACHE INT "")
+set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
+set(RAJA_DATA_ALIGN 64 CACHE INT "")
+set(RAJA_COHERENCE_BLOCK_SIZE 64 CACHE INT "")
