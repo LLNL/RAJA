55b0b149a667c6aeaf40ca1c856b276096dc225a
Jeff Keasler <keasler@llnl.gov>
2016-03-29 09:56:10 -0700

    Add the nested reduction test for the CPU.

---
+++ test/unit-tests/CPUtests/main-nested-reduce.cxx
@@ -0,0 +1,27 @@
+
+#include "RAJA/RAJA.hxx"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   RAJA::ReduceSum<RAJA::omp_reduce, double> sumA(0.0) ;
+
+   RAJA::forall<RAJA::omp_parallel_for_exec>(0, 10, [=] (int y) {
+      RAJA::forall<RAJA::seq_exec>(0, 10, [=] (int x) {
+         sumA += double(y*10 + x + 1) ;
+      } ) ;
+   } ) ;
+   printf("sum = %f, check = %f\n", double(sumA), 100.0*101.0 / 2.0) ;
+
+   RAJA::ReduceSum<RAJA::omp_reduce, double> sumB(0.0) ;
+
+   RAJA::forall<RAJA::seq_exec>(0, 10, [=] (int y) {
+      RAJA::forall<RAJA::omp_parallel_for_exec>(0, 10, [=] (int x) {
+         sumB += double(y*10 + x + 1) ;
+      } ) ;
+   } ) ;
+   printf("sum = %f, check = %f\n", double(sumB), 100.0*101.0 / 2.0) ;
+
+
+   return 0 ;
+}
