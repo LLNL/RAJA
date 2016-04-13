d2fa0e0e996dc983de495e0e4ca3fb6d83efacf6
Adam J. Kunen <kunen1@llnl.gov>
2016-04-12 17:28:25 -0700

    Added some comments/documentation for tiling

--- include/RAJA/forallN_tile.hxx
+++ include/RAJA/forallN_tile.hxx
@@ -21,7 +21,6 @@ namespace RAJA {
  *  ForallN tiling policies
  ******************************************************************/
 
-
 // Policy for no tiling
 struct tile_none {};
 
@@ -43,9 +42,14 @@ struct TileList{
 struct ForallN_Tile_Tag {};
 template<typename TILE_LIST, typename NEXT=ForallN_Execute>
 struct Tile {
+  // Identify this policy
   typedef ForallN_Tile_Tag PolicyTag;
-  typedef NEXT NextPolicy;
+
+  // A TileList<> that contains a list of Tile policies (tile_none, etc.)
   typedef TILE_LIST TilePolicy;
+
+  // The next nested-loop execution policy
+  typedef NEXT NextPolicy;
 };
 
 
@@ -60,7 +64,9 @@ template<typename BODY, typename TilePolicy, int TIDX, typename PI, typename ...
 RAJA_INLINE void forallN_peel_tile(BODY body, PI pi, PREST ... prest);
 
 
-
+/*!
+ * \brief Applys the tile_none policy
+ */
 template<typename BODY, typename TilePolicy, int TIDX, typename PI, typename ... PREST>
 RAJA_INLINE void forallN_apply_tile(tile_none, BODY body, PI pi, PREST ... prest){
   //printf("TIDX=%d: policy=tile_none\n", (int)TIDX);
@@ -74,6 +80,9 @@ RAJA_INLINE void forallN_apply_tile(tile_none, BODY body, PI pi, PREST ... prest
 }
 
 
+/*!
+ * \brief Applys the tile_fixed<N> policy
+ */
 template<typename BODY, typename TilePolicy, int TIDX, int TileSize, typename PI, typename ... PREST>
 RAJA_INLINE void forallN_apply_tile(tile_fixed<TileSize>, BODY body, PI pi, PREST ... prest){
   //printf("TIDX=%d: policy=tile_fixed<%d>\n", TIDX, TileSize);
@@ -98,11 +107,11 @@ RAJA_INLINE void forallN_apply_tile(tile_fixed<TileSize>, BODY body, PI pi, PRES
 }
 
 
-/******************************************************************
- *  forallN_policy(), tiling execution
- ******************************************************************/
-
-
+/*!
+ * \brief Functor that wraps calling the next nested-loop execution policy.
+ *
+ * This is passed into the recursive tiling function forallN_peel_tile.
+ */
 template<typename NextPolicy, typename BODY, typename ... ARGS>
 struct ForallN_NextPolicyWrapper {
 
@@ -120,17 +129,28 @@ struct ForallN_NextPolicyWrapper {
 };
 
 
+/*!
+ * \brief Tiling policy peeling function (termination case)
+ *
+ * This just executes the built-up tiling function passed in by outer
+ * forallN_peel_tile calls.
+ */
 template<typename BODY, typename TilePolicy, int TIDX>
 RAJA_INLINE void forallN_peel_tile(BODY body){
-
   // Termination case just calls the tiling function that was constructed
   body();
 }
 
 
+/*!
+ * \brief Tiling policy peeling function, recursively peels off tiling
+ * policies and applys them.
+ *
+ * This peels off the policy, and hands it off to the policy-overloaded
+ * forallN_apply_tile function... which in turn recursively calls this function
+ */
 template<typename BODY, typename TilePolicy, int TIDX, typename PI, typename ... PREST>
 RAJA_INLINE void forallN_peel_tile(BODY body, PI pi, PREST ... prest){
-  //printf("TIDX=%d\n", (int)TIDX);
 
   // Extract the tiling policy for loop nest TIDX
   using TP = typename std::tuple_element<TIDX, typename TilePolicy::tuple>::type;
@@ -141,17 +161,27 @@ RAJA_INLINE void forallN_peel_tile(BODY body, PI pi, PREST ... prest){
 
 
 
+
+
+/******************************************************************
+ *  forallN_policy(), tiling execution
+ ******************************************************************/
+
+
+
+
 /*!
- * \brief Tiling policy function.
+ * \brief Tiling policy front-end function.
  */
 template<typename POLICY, typename BODY, typename ... PARGS>
 RAJA_INLINE void forallN_policy(ForallN_Tile_Tag, BODY body, PARGS ... pargs){
   typedef typename POLICY::NextPolicy            NextPolicy;
   typedef typename POLICY::NextPolicy::PolicyTag NextPolicyTag;
 
+  // Extract the list of tiling policies from the policy
   using TilePolicy = typename POLICY::TilePolicy;
 
-
+  // Apply the tiling policies one-by-one with a peeling approach
   typedef ForallN_NextPolicyWrapper<NextPolicy, BODY, PARGS...> WRAPPER;
   WRAPPER wrapper(body);
   forallN_peel_tile<WRAPPER, TilePolicy, 0>(wrapper, pargs...);
