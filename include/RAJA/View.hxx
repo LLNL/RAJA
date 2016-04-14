005e6497dca0df2ea5aed4f47b67efc4a90b7789
Adam J. Kunen <kunen1@llnl.gov>
2016-04-14 13:21:39 -0700

    Updated remainder of copyright notices

--- include/RAJA/View.hxx
+++ include/RAJA/View.hxx
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+ *
  * Produced at the Lawrence Livermore National Laboratory.
  *
  * All rights reserved.
  *
- * This source code cannot be distributed without permission and
- * further review from Lawrence Livermore National Laboratory.
+ * For release details and restrictions, please see raja/README-license.txt
  */
   
 #ifndef RAJA_VIEW_HXX__
