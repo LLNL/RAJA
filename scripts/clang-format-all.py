3f6831e163f3391f8a337f8f052db73c433d9a44
Holger E. Jones <jones19@llnl.gov>
2016-06-02 12:57:42 -0700

    Check to make sure clang-format is in path; extension .hpp added

--- scripts/clang-format-all.py
+++ scripts/clang-format-all.py
@@ -23,43 +23,63 @@ class ClangFormat :
   def getScriptPath(self):
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
+  def which(self,program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
   def process(self,input_directory,cmd_prefix):
     files = []
     self.pypar_avail = False # Turn off mpi processing for now
-    self.cmd = cmd_prefix + self.cmd
-    print self.cmd + " is processing " + input_directory
-    for root, dirs, ff in os.walk(input_directory):
-      for name in ff:
-        #print name
-        if name.endswith(('.hxx','.cxx','.cpp','.cc','.c','.h')):
-          files.append(os.path.join(root,name))
+    self.cmd = self.which(cmd_prefix + self.cmd)
+    if self.cmd is None:
+      print "Error could not find clang-format"
+    else:
+      print self.cmd + " is processing " + input_directory
+      for root, dirs, ff in os.walk(input_directory):
+        for name in ff:
+          #print name
+          if name.endswith(('.cxx','.cpp','.cc','.c','.hxx','.hpp','.h')):
+            files.append(os.path.join(root,name))
 
-    print "Num to Process = " + str(len(files))
-    numToProcess = len(files)
-    if numToProcess > 0:
-      if self.pypar_avail == True:
-        proc = self.pypar.size()                                # Number of processes as specified by mpirun
-        myid = self.pypar.rank()                                # Id of this process (myid in [0, proc-1]) 
-        for ii in range(0,fpas):
-          if ii % proc == myid:
+      print "Num to Process = " + str(len(files))
+      numToProcess = len(files)
+      if numToProcess > 0:
+        if self.pypar_avail == True:
+          proc = self.pypar.size()                                # Number of processes as specified by mpirun
+          myid = self.pypar.rank()                                # Id of this process (myid in [0, proc-1]) 
+          for ii in range(0,fpas):
+            if ii % proc == myid:
+              fname = files[ii]
+              print "node " + str(myid) + " is processing " + fname
+              call([self.cmd,"-i",fname])
+              self.pypar.barrier()
+              self.pypar.finalize() 
+        else:
+          for ii in range(0,numToProcess):
             fname = files[ii]
-            print "node " + str(myid) + " is processing " + fname
+            print "Processing " + fname
             call([self.cmd,"-i",fname])
-            self.pypar.barrier()
-            self.pypar.finalize() 
-      else:
-        for ii in range(0,numToProcess):
-          fname = files[ii]
-          print "Processing " + fname
-          call([self.cmd,"-i",fname])
 
 def main():
-  parser = argparse.ArgumentParser(description='clang-format directory tree [.hxx,.cxx] : default search path is scriptdirectory/..')
+  parser = argparse.ArgumentParser(description='clang-format directory tree  : default search path is scriptdirectory/..')
   parser.add_argument('--input_directory',default="",help='Input Directory to start recursive auto-format')
   parser.add_argument('--cmdpath',default="",help='Path to clang-format')
   args = parser.parse_args()
   cf = ClangFormat()
-  input_directory = args.input_directory
+  input_directory = os.path.abspath(args.input_directory)
   if input_directory == "":
     input_directory = cf.getScriptPath()
     input_directory += '/..'
