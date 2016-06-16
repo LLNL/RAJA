9ae5ab7f47270d23ffe759ca26a5ceb36e517ea4
Rich Hornung <hornung1@llnl.gov>
2016-06-16 12:54:00 -0700

    Fix some intel build issues for chaos platforms.

--- scripts/icpc-17_beta.sh
+++ scripts/icpc-17_beta.sh
@@ -6,7 +6,7 @@ mkdir build-icpc-17_beta-release && cd build-icpc-17_beta-release
 RAJA_DIR=$(git rev-parse --show-toplevel)
 
 cmake \
-  -C ${RAJA_DIR}/host-configs/icpc-17_beta.cmake \
+  -C ${RAJA_DIR}/host-configs/chaos/icpc_17_beta.cmake \
   -DCMAKE_INSTALL_PREFIX=../install-icpc-1cpc-17_beta-release \
   "$@" \
   ${RAJA_DIR}
