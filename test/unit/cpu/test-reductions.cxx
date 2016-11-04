4f7318f28f88e9ba048289f20bdfe22b590058ae
David Beckingsale <davidbeckingsale@gmail.com>
2016-11-04 14:37:42 -0700

    Adding new reduction tests

---
+++ test/unit/cpu/test-reductions.cxx
@@ -0,0 +1,175 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+//
+// Produced at the Lawrence Livermore National Laboratory
+//
+// LLNL-CODE-689114
+//
+// All rights reserved.
+//
+// This file is part of RAJA.
+//
+// For additional details, please also read raja/README-license.txt.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the disclaimer below.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the disclaimer (as noted below) in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of the LLNS/LLNL nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+// LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "gtest/gtest.h"
+
+#include "RAJA/RAJA.hxx"
+#include <iostream>
+
+#include <tuple>
+
+template <typename T>
+class ReductionConstructorTest : public ::testing::Test
+{
+};
+
+TYPED_TEST_CASE_P(ReductionConstructorTest);
+
+TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor)
+{
+  using ReducePolicy = typename std::tuple_element<0, TypeParam>::type;
+  using NumericType = typename std::tuple_element<1, TypeParam>::type;
+
+  RAJA::ReduceSum<ReducePolicy, NumericType> reduce_sum(0.0);
+  RAJA::ReduceMin<ReducePolicy, NumericType> reduce_sum(0.0);
+  RAJA::ReduceMax<ReducePolicy, NumericType> reduce_sum(0.0);
+  RAJA::ReduceMinLoc<ReducePolicy, NumericType> reduce_sum(0.0, 1);
+  RAJA::ReduceMaxLoc<ReducePolicy, NumericType> reduce_sum(0.0, 1);
+
+  ASSERT_EQ((NumericType) reduce_sum.get(), (NumericType)(0.0));
+  ASSERT_EQ((NumericType) reduce_min.get(), (NumericType)(0.0));
+  ASSERT_EQ((NumericType) reduce_max.get(), (NumericType)(0.0));
+  ASSERT_EQ((NumericType) reduce_minloc.get(), (NumericType)(0.0));
+  ASSERT_EQ((RAJA::Index_type) reduce_minloc.getLoc(), (RAJA::Index_type) 1);
+  ASSERT_EQ((NumericType) reduce_maxloc.get(), (NumericType)(0.0));
+  ASSERT_EQ((RAJA::Index_type) reduce_maxloc.get(), (RAJA::Index_type) 1);
+}
+
+REGISTER_TYPED_TEST_CASE_P(ReductionConstructorTest, ReductionConstructor);
+
+using constructor_types = ::testing::Types<
+                            std::tuple<RAJA::seq_reduce, int>,
+                            std::tuple<RAJA::seq_reduce, float>,
+                            std::tuple<RAJA::seq_reduce, double>,
+                            std::tuple<RAJA::omp_reduce, int>,
+                            std::tuple<RAJA::omp_reduce, float>,
+                            std::tuple<RAJA::omp_reduce, double> >;
+
+INSTANTIATE_TYPED_TEST_CASE_P(ReduceBasicTests, ReductionConstructorTest, constructor_types);
+
+
+template <typename TUPLE>
+class ReductionCorrectnessTest : public ::testing::Test
+{
+protected:
+  virtual void SetUp()
+  {
+    array_length = 102;
+
+    posix_memalign((void **)&array, RAJA::DATA_ALIGN,
+      array_length * sizeof(RAJA::Real_type));
+
+    for (int i = 1; i < array_length-1; ++i) {
+      array[i] = (RAJA::Real_type) i;
+    }
+    array[0] = 0.0;
+    array[array_length-1] = 0.0;
+
+    sum = 0.0;
+    min = array_length * 2;
+    max = 0.0;
+    minloc = -1;
+    maxloc = -1;
+
+    for (int i = 0; i < array_length; ++i) {
+      RAJA::Real_type val = array[i];
+
+      sum += val;
+
+      if (val > max) {
+        max = val;
+        maxloc = i;
+      }
+
+      if (val < min) {
+        min = val;
+        minloc = i;
+      }
+    }
+  }
+
+  virtual void TearDown()
+  {
+    free(array);
+  }
+
+  RAJA::Real_ptr array;
+
+  RAJA::Real_type max;
+  RAJA::Real_type min;
+  RAJA::Real_type sum;
+  RAJA::Real_type maxloc;
+  RAJA::Real_type minloc;
+
+  RAJA::Index_type array_length;
+};
+
+template <typename TUPLE>
+class ReduceSumTest : public ReductionTest<TUPLE>
+{
+  protected:
+    virtual void SetUp()
+    {
+      using ReducePolicy = typename ReductionTest<TUPLE>::ReducePolicy;
+      using ExecPolicy = typename ReductionTest<TUPLE>::ExecPolicy;
+
+      ReductionTest<TUPLE>::SetUp();
+
+      RAJA::ReduceSum<ReducePolicy, double> sum_reducer(0.0);
+
+      RAJA::forall<ExecPolicy>(RAJA::RangeSegment(0, this->array_length), [=] (int i) {
+        sum_reducer += this->array[i];
+      });
+
+      raja_sum = (double) sum_reducer.get();
+    }
+
+
+REGISTER_TYPED_TEST_CASE_P(ReduceSumTest, ReduceSumCorrectness);
+
+using types = ::testing::Types< 
+                std::tuple<RAJA::seq_exec, RAJA::seq_reduce>,
+                std::tuple<RAJA::simd_exec, RAJA::seq_reduce>,
+                std::tuple<RAJA::omp_parallel_for_exec, RAJA::omp_reduce>
+      >;
+
+INSTANTIATE_TYPED_TEST_CASE_P(Reduce, ReduceSumTest, types);//, ::testing::Types< std::tuple<RAJA::seq_exec, RAJA::seq_reduce> >);
