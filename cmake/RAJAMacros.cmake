567922795a6a1db192688c84a7e2ee2a56f76ffd
David Beckingsale <davidbeckingsale@gmail.com>
2016-12-14 12:55:30 -0800

    Add RAJA_CUDA_ARCH variable to control CUDA code generation

--- cmake/RAJAMacros.cmake
+++ cmake/RAJAMacros.cmake
@@ -11,7 +11,7 @@ macro(raja_add_executable)
     if (RAJA_ENABLE_CLANG_CUDA) 
       add_executable(${arg_NAME} ${arg_SOURCES})
       target_compile_options(${arg_NAME} PRIVATE 
-        -x cuda --cuda-gpu-arch=sm_35 --cuda-path=${CUDA_TOOLKIT_ROOT_DIR})
+        -x cuda --cuda-gpu-arch=${RAJA_CUDA_ARCH} --cuda-path=${CUDA_TOOLKIT_ROOT_DIR})
       target_include_directories(${arg_NAME} 
         PUBLIC ${EXPT_CUDA_INCLUDE_LOCATION})
       target_link_libraries(${arg_NAME} ${CUDA_LIBRARIES} RAJA ${arg_DEPENDS_ON})
@@ -42,7 +42,7 @@ macro(raja_add_library)
 
       add_library(${arg_NAME} ${arg_SOURCES})
       target_compile_options(${arg_NAME} PRIVATE 
-        -x cuda --cuda-gpu-arch=sm_35 --cuda-path=${CUDA_TOOLKIT_ROOT_DIR})
+        -x cuda --cuda-gpu-arch=${RAJA_CUDA_ARCH} --cuda-path=${CUDA_TOOLKIT_ROOT_DIR})
       target_include_directories(${arg_NAME} 
         PUBLIC ${EXPT_CUDA_INCLUDE_LOCATION})
       target_link_libraries(${arg_NAME} ${CUDA_LIBRARIES})
