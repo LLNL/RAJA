b242d941bc4228fb212c98f3fb8367e15b3216f3
Dan Obermiller <dobermiller16@cmc.edu>
2017-02-13 16:35:03 -0800

    Use templated tests

--- test/unit/cpu/test-forall.cxx
+++ test/unit/cpu/test-forall.cxx
@@ -21,15 +21,29 @@ using namespace std;
 #include "Compare.hxx"
 #include "buildIndexSet.hxx"
 
+using forall_types = ::testing::Types<
+    std::tuple<seq_segit, seq_exec>,
+    std::tuple<seq_segit, simd_exec>
+#if defined(RAJA_ENABLE_OPENMP)    
+    ,std::tuple<seq_segit, omp_parallel_for_exec>,
+    std::tuple<omp_parallel_for_segit, seq_exec>,
+    std::tuple<omp_parallel_for_segit, simd_exec>
+#endif 
+#if defined(RAJA_ENABLE_CILK)
+    ,std::tuple<seq_segit, cilk_for_exec>,
+    std::tuple<cilk_for_segit, seq_exec>,
+    std::tuple<clik_for_segit, simd_exec>
+#endif
+>;
+
+template <typename ISET_POLICY_T>
 class ForallTest : public ::testing::Test
 {
-private:
+protected:
   Real_ptr in_array;
   Index_type alen;
   IndexSet iset;
   RAJAVec<Index_type> is_indices;
-
-protected:
   
   virtual void SetUp()
   {
@@ -49,174 +63,82 @@ protected:
   {
       free(in_array);
   }
-
-  template <typename ISET_POLICY_T>
-  void runBasicForallTest()
-  {
-      Real_ptr test_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
-      Real_ptr ref_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
-
-      std::fill(&test_array[0], &test_array[alen], 0.0);
-      std::fill(&ref_array[0], &ref_array[alen], 0.0);
-
-      for (Index_type i = 0; i < is_indices.size(); ++i) {
-        ref_array[is_indices[i]] = in_array[is_indices[i]] * in_array[is_indices[i]];
-      }
-
-      forall<ISET_POLICY_T>(iset, [=](Index_type idx) {
-        test_array[idx] = in_array[idx] * in_array[idx];
-      });
-
-      for (Index_type i = 0; i < alen; ++i) {
-          EXPECT_EQ(ref_array[i], test_array[i]);
-      }
-
-      free(test_array);
-      free(ref_array);
-  }
     
 };
 
-TEST_F(ForallTest, seq_segit_seq_exec)
+TYPED_TEST_CASE_P(ForallTest);
+
+TYPED_TEST_P(ForallTest, runBasicForallTest)
 {
-  runBasicForallTest<IndexSet::ExecPolicy<seq_segit, seq_exec> >();
-}
-
-TEST_F(ForallTest, seq_segit_simd_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<seq_segit, simd_exec> >();
-}
-
-#if defined(RAJA_ENABLE_OPENMP)
-TEST_F(ForallTest, seq_segit_omp_parallel_for_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<seq_segit, omp_parallel_for_exec> >();
-}
-
-TEST_F(ForallTest, omp_parallel_for_segit_seq_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<omp_parallel_for_segit, seq_exec> >();
-}
-
-TEST_F(ForallTest, omp_parallel_for_segit_simd_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<omp_parallel_for_segit, simd_exec> >();
-}  
-#endif // defined(RAJA_ENABLE_OPENMP)
-
-#if defined(RAJA_ENABLE_CILK)
-TEST_F(ForallTest, seq_segit_clik_for_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<seq_segit, cilk_for_exec> >();
-}
-
-TEST_F(ForallTest, cilk_for_segit_seq_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<cilk_for_segit, seq_exec> >();
-}
-
-TEST_F(ForallTest, cilk_for_segit_simd_exec)
-{
-  runBasicForallTest<IndexSet::ExecPolicy<cilk_for_segit, simd_exec> >();
-}
-#endif // defined(RAJA_ENABLE_CILK)  
-
-class ForallIcountTest : public ::testing::Test
-{
-private:
-  Real_ptr in_array;
-  Index_type alen;
-  IndexSet iset;
-  RAJAVec<Index_type> is_indices;
-
-protected:
-  
-  virtual void SetUp()
-  {
-      // AddSegments chosen arbitrarily; index set equivalence is tested elsewhere
-      alen = buildIndexSet(&iset, IndexSetBuildMethod::AddSegments) + 1;
-
-      in_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
-
-      for (Index_type i = 0; i < alen; ++i) {
-        in_array[i] = Real_type(rand() % 65536);
-      }
-
-      getIndices(is_indices, iset);
-  }
-
-  virtual void TearDown()
-  {
-      free(in_array);
-  }
-
-  template <typename ISET_POLICY_T>
-  void runBasicForallIcountTest()
-  {
-      Real_ptr test_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
-      Real_ptr ref_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
-
-      std::fill(&test_array[0], &test_array[alen], 0.0);
-      std::fill(&ref_array[0], &ref_array[alen], 0.0);
-
-      for (Index_type i = 0; i < is_indices.size(); ++i) {
-        ref_array[i] = in_array[is_indices[i]] * in_array[is_indices[i]];
-      }
-
-      forall_Icount<ISET_POLICY_T>(iset, [=](Index_type icount, Index_type idx) {
-        test_array[icount] = in_array[idx] * in_array[idx];
-      });
-
-      for (Index_type i = 0; i < alen; ++i) {
-          EXPECT_EQ(ref_array[i], test_array[i]);
-      }
-
-      free(test_array);
-      free(ref_array);
-  }
+    using Iset_Policy = typename std::tuple_element<0, TypeParam>::type;
+    using Execution_Policy = typename std::tuple_element<1, TypeParam>::type;
+    using ISET_POLICY_T = IndexSet::ExecPolicy<Iset_Policy, Execution_Policy>;
     
-};
+    Real_ptr in_array = this->in_array;
+    Index_type alen = this->alen;
+    IndexSet iset = this->iset;
+    RAJAVec<Index_type> is_indices = this->is_indices;
 
-TEST_F(ForallIcountTest, seq_segit_seq_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<seq_segit, seq_exec> >();
+    Real_ptr test_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
+    Real_ptr ref_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
+
+    for (Index_type i = 0; i < alen; ++i) {
+        test_array[i] = 0.0;
+        ref_array[i] = 0.0;
+    }
+
+    for (Index_type i = 0; i < is_indices.size(); ++i) {
+      ref_array[is_indices[i]] = in_array[is_indices[i]] * in_array[is_indices[i]];
+    }
+
+    forall<ISET_POLICY_T>(iset, [=](Index_type idx) {
+      test_array[idx] = in_array[idx] * in_array[idx];
+    });
+
+    for (Index_type i = 0; i < alen; ++i) {
+        EXPECT_EQ(ref_array[i], test_array[i]);
+    }
+
+    free(test_array);
+    free(ref_array);
 }
 
-TEST_F(ForallIcountTest, seq_segit_simd_exec)
+TYPED_TEST_P(ForallTest, runBasicForallIcountTest)
 {
-  runBasicForallIcountTest<IndexSet::ExecPolicy<seq_segit, simd_exec> >();
+    using Iset_Policy = typename std::tuple_element<0, TypeParam>::type;
+    using Execution_Policy = typename std::tuple_element<1, TypeParam>::type;
+    using ISET_POLICY_T = IndexSet::ExecPolicy<Iset_Policy, Execution_Policy>;
+    
+    Real_ptr in_array = this->in_array;
+    Index_type alen = this->alen;
+    IndexSet iset = this->iset;
+    RAJAVec<Index_type> is_indices = this->is_indices;
+
+    Real_ptr test_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
+    Real_ptr ref_array = (Real_ptr) allocate_aligned(DATA_ALIGN, alen * sizeof(Real_type));
+
+    for (Index_type i = 0; i < alen; ++i) {
+        test_array[i] = 0.0;
+        ref_array[i] = 0.0;
+    }
+
+    for (Index_type i = 0; i < is_indices.size(); ++i) {
+      ref_array[i] = in_array[is_indices[i]] * in_array[is_indices[i]];
+    }
+
+    forall_Icount<ISET_POLICY_T>(iset, [=](Index_type icount, Index_type idx) {
+      test_array[icount] = in_array[idx] * in_array[idx];
+    });
+
+    for (Index_type i = 0; i < alen; ++i) {
+        EXPECT_EQ(ref_array[i], test_array[i]);
+    }
+
+    free(test_array);
+    free(ref_array);
 }
 
-#if defined(RAJA_ENABLE_OPENMP)
-TEST_F(ForallIcountTest, seq_segit_omp_parallel_for_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<seq_segit, omp_parallel_for_exec> >();
-}
+REGISTER_TYPED_TEST_CASE_P(ForallTest, runBasicForallTest, runBasicForallIcountTest);
 
-TEST_F(ForallIcountTest, omp_parallel_for_segit_seq_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<omp_parallel_for_segit, seq_exec> >();
-}
+INSTANTIATE_TYPED_TEST_CASE_P(ForallTests, ForallTest, forall_types);
 
-TEST_F(ForallIcountTest, omp_parallel_for_segit_simd_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<omp_parallel_for_segit, simd_exec> >();
-}  
-#endif // defined(RAJA_ENABLE_OPENMP)
-
-#if defined(RAJA_ENABLE_CILK)
-TEST_F(ForallIcountTest, seq_segit_clik_for_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<seq_segit, cilk_for_exec> >();
-}
-
-TEST_F(ForallIcountTest, cilk_for_segit_seq_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<cilk_for_segit, seq_exec> >();
-}
-
-TEST_F(ForallIcountTest, cilk_for_segit_simd_exec)
-{
-  runBasicForallIcountTest<IndexSet::ExecPolicy<cilk_for_segit, simd_exec> >();
-}
-#endif // defined(RAJA_ENABLE_CILK)  
