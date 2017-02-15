3d755b35c9c29a085a90b6d7d7b107f25aed1663
David Poliakoff <david.poliakoff@gmail.com>
2017-02-15 13:01:00 -0800

    Commit with shift from TBB to std::future

--- include/RAJA/Stream.hxx
+++ include/RAJA/Stream.hxx
@@ -50,33 +50,38 @@
 
 #include "RAJA/int_datatypes.hxx"
 
-#include <tbb/task_group.h>
+//#include <tbb/task_group.h>
+//#include <tbb/flow_graph.h>
+#include <future>
 #ifdef RAJA_ENABLE_CUDA
 #include "cuda.h"
 #endif //RAJA_ENABLE_CUDA
 namespace RAJA
 {
 
-class StreamPoolNoGPU {
-public:
-  template<typename LB>
-  void run(int stream_num, LB&& body){
+//class StreamPoolNoGPU {
+//public:
+//  template<typename LB>
+//  void run(int stream_num, LB&& body){
+//
+//    streams[stream_num].run(body);
+//  }
+//  void wait(int stream_num){
+//    streams[stream_num].wait();
+//  }
+//  void operator=(StreamPoolNoGPU const&);
+//  StreamPoolNoGPU(size_t num_streams){
+//    streams = new tbb::task_group[num_streams];
+//  }
+//private:
+//  tbb::task_group* streams;  
+//};
 
-    streams[stream_num].run(body);
-  }
-  void wait(int stream_num){
-    streams[stream_num].wait();
-  }
-  void operator=(StreamPoolNoGPU const&);
-  StreamPoolNoGPU(size_t num_streams){
-    streams = new tbb::task_group[num_streams];
-  }
-private:
-  tbb::task_group* streams;  
-};
-
-
+template <typename T>
+using FutureType = std::shared_future<T>;
 // This function is mainly meant for documentation, so you can know which arguments meant what
+// In examples, I write StreamPool calls as
+// stream_pool.forall<RAJA::seq_exc>(RAJA::StreamIndex(0), 0, 10, [=] (int i) {....}
 constexpr int StreamIndex(int num){
   return num;
 }
@@ -95,36 +100,17 @@ constexpr bool is_gpu_policy(omp_parallel_for_exec in){
 constexpr bool is_gpu_policy(seq_exec in){
   return false;
 }
-//TODO: forgive me Bjarne, for I have sinned
-template<typename InnerIter>
-struct iterWithStream {
-  InnerIter my_iter;
-  cudaStream_t stream;
-  //std::result_of<my_iter.begin()>::type begin(){
-  //  return my_iter.begin();
-  //}
-  //std::result_of<my_iter.end()>::type end(){
-  //  return my_iter.end();
-  //}
-  auto begin() -> decltype(my_iter.begin()){
-    return my_iter.begin();
-  }
-  auto end() -> decltype(my_iter.end()){
-    return my_iter.end();
-  }
-};
-template<typename wrappee>
-iterWithStream<wrappee> makeStreamIter(wrappee in, cudaStream_t stream){
-    iterWithStream<wrappee> out;
-    out.my_iter = in;
-    out.my_stream = stream;
-    return out;
-}
 class StreamPool {
 public:
   template<typename LOOP_BODY>
   void enqueueTask(int stream_num, LOOP_BODY body){
-     tbb_streams[stream_num].run(body);
+     //next_streams[stream_num] = host_streams[stream_num];
+     auto last_in_stream = host_streams[stream_num];
+     host_streams[stream_num] = std::async([=]{
+        last_in_stream.get();
+        body();
+     }).share();
+     //host_streams[stream_num].run(body);
   } 
   template<typename Policy, typename LOOP_BODY, typename... Args>
   typename std::enable_if<std::is_base_of<RAJA::cuda_exec_base,Policy>::value,void>::type safeCPUCall(LOOP_BODY body, Args... args){
@@ -155,7 +141,7 @@ public:
     GPU
   }; //TODO: can we have multiple GPUs per stream here? Also, more generally, it's not that simple
   void gpu_nonblocking_sync(int stream_num){
-     enqueue_tasks(stream_num,
+     enqueueTask(stream_num,
      [=](){
        cudaStreamSynchronize(cuda_streams[stream_num]);
      });
@@ -167,18 +153,18 @@ public:
     if((last_space == ExecutionSpace::GPU) && (next_space == ExecutionSpace::CPU)){
       gpu_nonblocking_sync(stream_num);
     }
-    tbb_streams[stream_num].run(body);
+    enqueueTask(stream_num,[=](){
+      body();
+    });
     last_execution_space[stream_num] = next_space;
   }
   template <typename EXEC_POLICY_T, typename... Args>
   RAJA_INLINE typename std::enable_if<!std::is_base_of<cuda_exec_base,EXEC_POLICY_T>::value,void>::type forall(int stream_num, Args... args)
   {
-    std::cout<<"Queueing forall on "<<stream_num<<" on CPU\n";
     if(last_execution_space[stream_num] == ExecutionSpace::GPU){
       gpu_nonblocking_sync(stream_num);
     }
-    tbb_streams[stream_num].run([=](){
-      std::cout<<"Running forall on "<<stream_num<<" on CPU\n";
+    enqueueTask(stream_num,[=](){
       RAJA::forall<EXEC_POLICY_T>(args...);
     });
     last_execution_space[stream_num] = ExecutionSpace::CPU;
@@ -186,9 +172,7 @@ public:
   template <typename EXEC_POLICY_T, typename... Args>
   RAJA_INLINE typename std::enable_if<std::is_base_of<cuda_exec_base,EXEC_POLICY_T>::value,void>::type forall(int stream_num, Args... args)
   {
-    std::cout<<"Queueing forall on "<<stream_num<<" on CPU\n";
-    tbb_streams[stream_num].run([=](){
-      std::cout<<"Running forall on "<<stream_num<<" on GPU\n";
+    enqueueTask(stream_num,[=](){
       RAJA::runtime_context::forall(cuda_stream_exec<256>(getGPUStream(stream_num)),args...);
     });
     last_execution_space[stream_num] = ExecutionSpace::GPU;
@@ -196,6 +180,7 @@ public:
   cudaStream_t getGPUStream(const int stream_num) const{
     return cuda_streams[stream_num];
   }
+  //TODO: implement
   template <typename EXEC_POLICY_T, typename... Args>
   RAJA_INLINE void forall_Icount(const int stream_num, Args... args){
     ExecutionSpace last_space = last_execution_space[stream_num];
@@ -206,32 +191,38 @@ public:
     RAJA::forall_Icount(args...);
     last_execution_space[stream_num] = next_space;
   }
+  //TODO: implement
   void wait(int stream_num){
     ExecutionSpace last_space = last_execution_space[stream_num];
-    if(last_space == ExecutionSpace::GPU){
-      cudaStreamSynchronize(cuda_streams[stream_num]);
-    }
-    tbb_streams[stream_num].wait();
+    //if(last_space == ExecutionSpace::GPU){
+    //  cudaStreamSynchronize(cuda_streams[stream_num]);
+    //}
+    host_streams[stream_num].get();
   }
   StreamPool(size_t num_streams){
-    tbb_streams = new tbb::task_group[num_streams];
+    pool_size = num_streams;
+    host_streams = new FutureType<void>[num_streams+1];
+    //next_streams = new FutureType<void>[num_streams+1];
     last_execution_space = new ExecutionSpace[num_streams];
     cuda_streams = new cudaStream_t[num_streams]; 
     for(int i=0;i<num_streams;i++){
-      //tbb_streams[i].run([=](){
+      host_streams[i] = std::async([=](){return;}).share();
+      //next_streams[i] = std::async([=](){return;}).share();
+      //host_streams[i].run([=](){
         last_execution_space[i] = ExecutionSpace::CPU;
         cudaStreamCreate(&cuda_streams[i]);
       //});
     }
   }
   ~StreamPool(){
-#ifndef __CUDA_ARCH__
-    cudaDeviceSynchronize();
-    std::cout<<"Synchronizing device\n";
-#endif
+    for(int i=0;i<pool_size;i++){
+        wait(i);
+    }
   }
 private:
-  tbb::task_group* tbb_streams;  
+  size_t pool_size;
+  FutureType<void>* host_streams;
+  //FutureType<void>* next_streams;
   ExecutionSpace* last_execution_space; 
   cudaStream_t* cuda_streams;
 };
--- test/unit/cpu/dnm_stream_mockup.cxx
+++ test/unit/cpu/dnm_stream_mockup.cxx
@@ -13,15 +13,17 @@ int main(){
   cudaMalloc((void**)&gpuB,sizeof(float)*dataSize);
   cudaMalloc((void**)&gpuC,sizeof(float)*dataSize);  
   RAJA::StreamPool dog_pool(8);
+  //dog_pool.run<RAJA::omp_parallel_for_exec>(RAJA::StreamIndex(0), [=] (){
+
+  //});
   dog_pool.forall<RAJA::cuda_stream_exec<256>>(RAJA::StreamIndex(0), 0, splitPoint, [=] __device__(int i){
     gpuA[i] = i * 1.0;
     gpuB[i] = i * 2.0;
     if(i==5){
        printf("GPU1: %f, %f, %f\n",gpuA[5],gpuB[5],gpuC[5]);
     }
-
   });
-  dog_pool.forall<RAJA::seq_exec>(RAJA::StreamIndex(1), splitPoint, dataSize, [=] (int i){
+  dog_pool.forall<RAJA::omp_parallel_for_exec>(RAJA::StreamIndex(1), splitPoint, dataSize, [=] (int i){
     cpuA[i] = i * 1.0;
     cpuB[i] = i * 2.0;
     if(i==(splitPoint+5)){
@@ -34,20 +36,20 @@ int main(){
        printf("GPU2: %f, %f, %f\n",gpuA[5],gpuB[5],gpuC[5]);
     }
   });
-  dog_pool.forall<RAJA::seq_exec>(RAJA::StreamIndex(1), splitPoint, dataSize, [=] (int i){
+  dog_pool.forall<RAJA::omp_parallel_for_exec>(RAJA::StreamIndex(1), splitPoint, dataSize, [=] (int i){
     cpuC[i] = cpuA[i] * cpuB[i];
     if(i==(splitPoint+5)){
-       printf("CPU2 %i: %f, %f, %f\n",i,cpuA[splitPoint+5],cpuB[splitPoint+5],cpuC[splitPoint+5]);
+       printf("CPU2 %i: %f, %f, %f, (%f)\n",i,cpuA[i],cpuB[i],cpuC[i], (cpuA[i]*cpuB[i]));
     }
   });
-  dog_pool.run<RAJA::seq_exec>(RAJA::StreamIndex(0),[=](){
+  dog_pool.run<RAJA::omp_parallel_for_exec>(RAJA::StreamIndex(0),[=](){
     cudaMemcpyAsync(cpuC,gpuC,sizeof(float)*(splitPoint),cudaMemcpyDeviceToHost,dog_pool.getGPUStream(0));
   });
   dog_pool.wait(1);
-  dog_pool.wait(0);
-  dog_pool.run<RAJA::seq_exec>(RAJA::StreamIndex(0),[=](){
+  dog_pool.run<RAJA::omp_parallel_for_exec>(RAJA::StreamIndex(0),[=](){
     printf("%f, %f, %f, %f, %f, %f\n",cpuC[0],cpuC[1],cpuC[2],cpuC[dataSize-3],cpuC[dataSize-2],cpuC[dataSize-1]);
   });
+  dog_pool.wait(1);
     printf("%f, %f, %f, %f, %f, %f\n",cpuC[0],cpuC[1],cpuC[2],cpuC[dataSize-3],cpuC[dataSize-2],cpuC[dataSize-1]);
     cudaDeviceSynchronize();
 }
