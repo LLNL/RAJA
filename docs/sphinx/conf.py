54226dd52cd3104584f6fc7d09c37b68cd80027f
David Beckingsale <davidbeckingsale@gmail.com>
2017-03-22 09:07:16 -0700

    Make RTD build our Doxygen

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -14,11 +14,17 @@
 
 import sys
 import os
+import subprocess
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('_exts'))
+
+# Generate Doxygen on RTD
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+if read_the_docs_build:
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
 
 # -- General configuration ------------------------------------------------
 
