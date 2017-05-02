66b0aa01eb1115c6a526202a2f8cd45344e00f19
William Killian <william.killian@gmail.com>
2017-05-01 22:39:38 -0400

    fixed warnings with CUDA8 with RAJA_SUPPRESS_HD_WARN to ForallNPolicy operator()

--- include/RAJA/internal/ForallNPolicy.hpp
+++ include/RAJA/internal/ForallNPolicy.hpp
@@ -1,13 +1,14 @@
 #ifndef RAJA_internal_ForallNPolicy_HXX_
 #define RAJA_internal_ForallNPolicy_HXX_
 
-namespace RAJA {
+namespace RAJA
+{
 
 /******************************************************************
  *  ForallN generic policies
  ******************************************************************/
 
-template<typename P, typename I>
+template <typename P, typename I>
 struct ForallN_PolicyPair : public I {
   typedef P POLICY;
   typedef I ISET;
@@ -16,7 +17,7 @@ struct ForallN_PolicyPair : public I {
   explicit constexpr ForallN_PolicyPair(ISET const &i) : ISET(i) {}
 };
 
-template<typename... PLIST>
+template <typename... PLIST>
 struct ExecList {
   constexpr const static size_t num_loops = sizeof...(PLIST);
   typedef std::tuple<PLIST...> tuple;
@@ -30,7 +31,7 @@ struct Execute {
   typedef ForallN_Execute_Tag PolicyTag;
 };
 
-template<typename EXEC, typename NEXT = Execute>
+template <typename EXEC, typename NEXT = Execute>
 struct NestedPolicy {
   typedef NEXT NextPolicy;
   typedef EXEC ExecPolicies;
@@ -100,16 +101,18 @@ struct ForallN_PeelOuter {
   {
   }
 
+  RAJA_SUPPRESS_HD_WARN
   RAJA_INLINE
-      RAJA_HOST_DEVICE
+  RAJA_HOST_DEVICE
   void operator()(Index_type i) const
   {
     ForallN_BindFirstArg_HostDevice<BODY> inner(body, i);
     next_exec(inner);
   }
 
+  RAJA_SUPPRESS_HD_WARN
   RAJA_INLINE
-      RAJA_HOST_DEVICE
+  RAJA_HOST_DEVICE
   void operator()(Index_type i, Index_type j) const
   {
     ForallN_BindFirstArg_HostDevice<BODY> inner_i(body, i);
@@ -117,8 +120,9 @@ struct ForallN_PeelOuter {
     next_exec(inner_j);
   }
 
+  RAJA_SUPPRESS_HD_WARN
   RAJA_INLINE
-      RAJA_HOST_DEVICE
+  RAJA_HOST_DEVICE
   void operator()(Index_type i, Index_type j, Index_type k) const
   {
     ForallN_BindFirstArg_HostDevice<BODY> inner_i(body, i);
@@ -128,6 +132,6 @@ struct ForallN_PeelOuter {
   }
 };
 
-} // end of RAJA namespace
+}  // end of RAJA namespace
 
 #endif
