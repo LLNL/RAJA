d61560f99d7232b6ba83ee360b8292e6e965bcef
Tom Scogland <scogland1@llnl.gov>
2017-07-13 09:05:43 -0700

    Collapse policy construct added
    The main addition is the extraction of sub-constructs from collapse to
    generate the for list and allow a single executor policy to handle
    multiple levels.  The mechanism used for this is extensible by deriving
    a new clas from ForList and ensuring an as_for_list alias exists in the
    type to provide the list of For-compatible constructs that it
    represents.

--- include/RAJA/pattern/nested.hpp
+++ include/RAJA/pattern/nested.hpp
@@ -25,17 +25,34 @@ using remove_all_t =
     typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
 // Universal base of all For wrappers for type traits
+struct ForList {
+};
 struct ForBase {
+};
+struct CollapseBase {
+};
+template <metal::int_ ArgumentId, typename IndexType = RAJA::Index_type>
+struct ForTraitBase : public ForBase {
+  using index_type = IndexType;
+  using index_pair = metal::pair<metal::number<ArgumentId>, IndexType>;
+  using index = metal::number<ArgumentId>;
 };
 
 using is_for_policy = metal::
     bind<metal::trait<std::is_base_of>, metal::always<ForBase>, metal::_1>;
-}
+
+using has_for_list = metal::
+    bind<metal::trait<std::is_base_of>, metal::always<ForList>, metal::_1>;
+template<typename T>
+using get_for_list = typename T::as_for_list;
 
 template <typename Seq>
-using get_for_policies = metal::remove_if<
-    Seq,
-    metal::bind<metal::lambda<metal::not_>, internal::is_for_policy>>;
+using get_for_policies = metal::flatten<
+    metal::transform<
+        metal::lambda<get_for_list>,
+        metal::remove_if<
+            Seq,
+            metal::bind<metal::lambda<metal::not_>, internal::has_for_list>>>>;
 
 template <typename ForPolicy>
 using get_for_index_pair = typename ForPolicy::index_pair;
@@ -56,14 +73,15 @@ using get_for_index_types = metal::transform<
 template <typename Seq>
 using index_tuple_from_policies =
     metal::apply<metal::lambda<RAJA::util::tuple>, get_for_index_types<Seq>>;
+}
 
-template <typename Pol,
-          metal::int_ ArgumentId,
+template <metal::int_ ArgumentId,
+          typename Pol,
           typename IndexType = RAJA::Index_type>
-struct For : public internal::ForBase {
-  using index_type = IndexType;
-  using index_pair = metal::pair<metal::number<ArgumentId>, IndexType>;
-  using index = metal::number<ArgumentId>;
+struct For : public internal::ForList,
+             public internal::ForTraitBase<ArgumentId, IndexType> {
+  using as_for_list = metal::list<For<ArgumentId, Pol, IndexType>>;
+  // TODO: add static_assert for valid policy in Pol
   const Pol pol;
   For() : pol{} {}
   For(const Pol &p) : pol{p} {}
@@ -76,16 +94,16 @@ template <typename Policy>
 struct Executor;
 
 template <typename Pol, metal::int_ ArgumentId, typename IndexType>
-struct Executor<For<Pol, ArgumentId, IndexType>> {
-  using ForPol = For<Pol, ArgumentId, IndexType>;
+struct Executor<For<ArgumentId, Pol, IndexType>> {
+  using ForPol = For<ArgumentId, Pol, IndexType>;
   template <typename BaseWrapper>
-  struct ForWrapper
-  {
+  struct ForWrapper {
     ForWrapper(BaseWrapper const &w) : bw{w} {}
     BaseWrapper bw;
     template <typename InIndexType>
     void operator()(InIndexType i)
     {
+      // TODO : see if the explicit construction is required
       RAJA::util::get<ArgumentId>(bw.data.index_tuple) = IndexType{i};
       bw();
     }
@@ -93,9 +111,50 @@ struct Executor<For<Pol, ArgumentId, IndexType>> {
   template <typename WrappedBody>
   void operator()(ForPol const &fp, WrappedBody const &wrap)
   {
-    impl::forall(fp.pol, RAJA::util::get<ArgumentId>(wrap.data.st), ForWrapper<WrappedBody>{wrap});
+    impl::forall(fp.pol,
+                 RAJA::util::get<ArgumentId>(wrap.data.st),
+                 ForWrapper<WrappedBody>{wrap});
   }
 };
+
+template <metal::int_ ArgumentId,
+          typename IndexType = RAJA::Index_type,
+          typename... Rest>
+struct CFor : public internal::ForTraitBase<ArgumentId, IndexType> {
+};
+
+template <typename ExecPolicy, typename... CFors>
+struct Collapse : public internal::ForList, public internal::CollapseBase {
+  using as_for_list = metal::list<CFors...>;
+  const ExecPolicy pol;
+  Collapse() : pol{} {}
+  Collapse(ExecPolicy const &ep) : pol{ep} {}
+};
+
+template <metal::int_ Idx0, metal::int_ Idx1, typename IdxT0, typename IdxT1>
+struct Executor<Collapse<seq_exec, CFor<Idx0, IdxT0>, CFor<Idx1, IdxT1>>> {
+  template <typename WrappedBody>
+  void operator()(
+      Collapse<seq_exec, CFor<Idx0, IdxT0>, CFor<Idx1, IdxT1>> const &p,
+      WrappedBody const &wrap)
+  {
+    auto b0 = std::begin(RAJA::util::get<Idx0>(wrap.data.st));
+    auto b1 = std::begin(RAJA::util::get<Idx1>(wrap.data.st));
+
+    auto e0 = std::end(RAJA::util::get<Idx0>(wrap.data.st));
+    auto e1 = std::end(RAJA::util::get<Idx1>(wrap.data.st));
+
+    // Skip a level
+    for (auto i0 = b0; i0 < e0; ++i0) {
+      RAJA::util::get<Idx0>(wrap.data.index_tuple) = IdxT0{*i0};
+      for (auto i1 = b1; i1 < e1; ++i1) {
+        RAJA::util::get<Idx1>(wrap.data.index_tuple) = IdxT1{*i1};
+        wrap();
+      }
+    }
+  }
+};
+
 
 template <typename PolicyTuple, typename SegmentTuple, typename Fn>
 struct LoopData {
@@ -104,7 +163,7 @@ struct LoopData {
   const PolicyTuple &pt;
   const SegmentTuple &st;
   const typename std::remove_reference<Fn>::type f;
-  index_tuple_from_policies<typename PolicyTuple::TList> index_tuple;
+  internal::index_tuple_from_policies<typename PolicyTuple::TList> index_tuple;
   LoopData(PolicyTuple const &p, SegmentTuple const &s, Fn const &fn)
       : pt{p}, st{s}, f{fn}
   {
@@ -116,7 +175,7 @@ struct Wrapper {
   using Next = Wrapper<idx + 1, n_policies, Data>;
   Data &data;
   explicit Wrapper(Data &d) : data{d} {}
-  void operator()()
+  void operator()() const
   {
     auto const &pol = RAJA::util::get<idx>(data.pt);
     Executor<internal::remove_all_t<decltype(pol)>> e{};
@@ -129,7 +188,7 @@ template <int n_policies, typename Data>
 struct Wrapper<n_policies, n_policies, Data> {
   Data &data;
   explicit Wrapper(Data &d) : data{d} {}
-  void operator()() { RAJA::util::invoke(data.index_tuple, data.f); }
+  void operator()() const { RAJA::util::invoke(data.index_tuple, data.f); }
 };
 
 template <typename Data>
@@ -146,7 +205,10 @@ RAJA_INLINE void forall(const Pol &p, const SegmentTuple &st, const Body &b)
   // but must be made before loop_body is copied
   beforeCudaKernelLaunch();
 #endif
-  using fors = get_for_policies<typename Pol::TList>;
+  using fors = internal::get_for_policies<typename Pol::TList>;
+  // TODO: ensure no duplicate indices in For<>s
+  // TODO: ensure no gaps in For<>s
+  // TODO: test that all policy members model the Executor policy concept
   static_assert(RAJA::util::tuple_size<SegmentTuple>::value
                     == metal::size<fors>::value,
                 "policy and segment index counts do not match");
--- test/unit/nested.cpp
+++ test/unit/nested.cpp
@@ -8,7 +8,20 @@ TEST(Nested, Basic)
 {
   using namespace RAJA::nested;
   using Index_type = RAJA::Index_type;
-  using pol = Policy<RAJA::seq_exec, RAJA::seq_exec, For<RAJA::seq_exec, 0>, RAJA::seq_exec, For<RAJA::seq_exec, 1>>;
+  using pol = Policy<For<1, RAJA::seq_exec>, For<0, RAJA::seq_exec>>;
+  RAJA::nested::forall(pol{},
+                       RAJA::util::make_tuple(RAJA::RangeSegment(0, 5),
+                                              RAJA::RangeSegment(0, 5)),
+                       [=](Index_type i, Index_type j) {
+                         std::cout << i << " " << j << std::endl;
+                       });
+}
+
+TEST(Nested, collapse)
+{
+  using namespace RAJA::nested;
+  using Index_type = RAJA::Index_type;
+  using pol = Policy<Collapse<RAJA::seq_exec, CFor<1>, CFor<0>>>;
   RAJA::nested::forall(pol{},
                        RAJA::util::make_tuple(RAJA::RangeSegment(0, 5),
                                               RAJA::RangeSegment(0, 5)),
