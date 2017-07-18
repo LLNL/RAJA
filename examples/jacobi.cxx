651fb42ffda97b1c73784e277d46653088a9ded4
Arturo Vargas <arv3.89@gmail.com>
2017-07-18 16:08:05 -0700

    jacobi solver with all RAJA variants

--- examples/jacobi.cxx
+++ examples/jacobi.cxx
@@ -50,9 +50,9 @@
 
 #define block_size 256
 
-// Solve for loop currents in structured resistor array
-//Similar discretization to solving the Possion Equation
-//with zero dirichlet boundary condtions
+// Solve for loop currents in structured resistor array.
+// Similar structure to discretizing the poisson equation 
+// with a second order finite difference scheme
 int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 {
 
@@ -70,7 +70,7 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
   memset(Iold,0,NN*sizeof(double));
 
   //----[Standard C approach]-----
-  resI2 = 1; 
+  resI2 = 1;  iteration = 0; 
   while(resI2>tol*tol){
     
     resI2 = 0;    
@@ -97,7 +97,7 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
     }
 
     if(iteration > maxIter){        
-      std::cout<<"too many iterations!"<<std::endl;
+      printf("Standard C - too many iterations!\n");
       exit(-1);
     }
 
@@ -105,7 +105,7 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
   }
   printf("Standard C/Cpp Loops: \n");
   printf("Top right current: %lg \n", I[N+N*(N+2)]);
-  printf("No of iterations: %d \n \n \n",iteration);
+  printf("No of iterations: %d \n \n",iteration);
   //======================================
 
   
@@ -134,43 +134,130 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
       }
 
   });
-
-
+    
     //Reduction step    
     RAJA::ReduceSum<RAJA::seq_reduce, double> RAJA_resI2(0.0);
     RAJA::forall<RAJA::seq_reduce>(0, NN, [=](int k) {
         RAJA_resI2 += (I[k]-Iold[k])*(I[k]-Iold[k]);
         Iold[k]=I[k];
       });
-
-
+    
+    
     resI2 = RAJA_resI2; 
     if(iteration > maxIter){        
-      std::cout<<"too many iterations!"<<std::endl;
+      printf("RAJA-Seq - too many iterations\n");
       exit(-1);
     }
     iteration++;
   }
   printf("RAJA Nested Loop Sequential Policy: \n");
   printf("Top right current: %lg \n", I[N+N*(N+2)]);
-  printf("No of iterations: %d \n",iteration);
+  printf("No of iterations: %d \n \n",iteration);
+  //======================================
+
+
+  //----[RAJA: Nested OpenMP Policy]---------
+  //RAJA does not allow variables to be modified inside the loop
+  resI2 = 1; iteration = 0; 
+  memset(I,0,NN*sizeof(double));
+  memset(Iold,0,NN*sizeof(double));
+  while(resI2 > tol*tol){
+    
+    
+    RAJA::forallN< RAJA::NestedPolicy<
+    RAJA::ExecList<RAJA::omp_parallel_for_exec,RAJA::omp_parallel_for_exec>>>(
+    RAJA::RangeSegment(1, (N+1)),
+    RAJA::RangeSegment(1, (N+1)),
+    [=](int m, int n) { 
+      
+      unsigned int id = n*(N+2) + m;
+      if(n==1 && m==1){
+        double invD = 1./3.; double V = 1; 
+        I[id] = invD*(V-Iold[id-N-2]-Iold[id+N+2]-Iold[id-1]-Iold[id+1]);
+      }else{
+        double invD2 = 1./4.0; double V2 = 0.0;
+        I[id] = invD2*(V2-Iold[id-N-2]-Iold[id+N+2]-Iold[id-1]-Iold[id+1]);
+      }
+
+  });
+    
+    //Reduction step    
+    RAJA::ReduceSum<RAJA::omp_reduce, double> RAJA_resI2(0.0);
+    RAJA::forall<RAJA::omp_reduce>(0, NN, [=](int k) {
+        RAJA_resI2 += (I[k]-Iold[k])*(I[k]-Iold[k]);
+        Iold[k]=I[k];
+      });
+    
+    
+    resI2 = RAJA_resI2; 
+    if(iteration > maxIter){        
+      std::cout<<"RAJA:OpenMP - too many iterations!"<<std::endl;
+      exit(-1);
+    }
+    iteration++;
+  }
+  printf("RAJA Nested Loop Sequential Policy: \n");
+  printf("Top right current: %lg \n", I[N+N*(N+2)]);
+  printf("No of iterations: %d \n \n",iteration);
   //======================================
 
 
 
-
+  //----[RAJA: Nested CUDA Policy]---------
+  resI2 = 1; iteration = 0; 
+  memset(I,0,NN*sizeof(double));
+  memset(Iold,0,NN*sizeof(double));
   
-
+  double *d_I, *d_Iold;
+  cudaMallocManaged((void**)&d_I,sizeof(double)*NN,cudaMemAttachGlobal);
+  cudaMallocManaged((void**)&d_Iold,sizeof(double)*NN,cudaMemAttachGlobal);
   
-
-
-
-
+  //copy data from host
+  cudaMemcpy(d_I,I,sizeof(double)*NN,cudaMemcpyHostToDevice);
+  cudaMemcpy(d_Iold,Iold,sizeof(double)*NN,cudaMemcpyHostToDevice);
   
+  while(resI2 > tol*tol){
+    
+    if(iteration==0){
+      std::cout<<"Entered the for looop"<<std::endl;
+    }
+    
+    RAJA::forallN<RAJA::NestedPolicy<RAJA::ExecList<RAJA::cuda_threadblock_y_exec<16>,
+    RAJA::cuda_threadblock_x_exec<16>>>>(
+    RAJA::RangeSegment(1, (N+1)), RAJA::RangeSegment(1, (N+1)), [=] __device__ (int m, int n) {
+     
+      unsigned int id = n*(N+2) + m;
+      if(n==1 && m==1){
+        double invD = 1./3.; double V = 1; 
+        d_I[id] = invD*(V-d_Iold[id-N-2]-d_Iold[id+N+2]-d_Iold[id-1]-d_Iold[id+1]);
+      }else{
+        double invD2 = 1./4.0; double V2 = 0.0;
+        d_I[id] = invD2*(V2-d_Iold[id-N-2]-d_Iold[id+N+2]-d_Iold[id-1]-d_Iold[id+1]);
+      }
+    });
+
+    //Reduction step    
+    RAJA::ReduceSum<RAJA::cuda_reduce<256>, double> RAJA_resI2(0.0);
+    RAJA::forall<RAJA::cuda_exec<256> >(0, NN, [=] __device__ (int k) {
+        RAJA_resI2 += (d_I[k]-d_Iold[k])*(d_I[k]-d_Iold[k]);
+        d_Iold[k]=d_I[k];
+      });
+
+    resI2 = RAJA_resI2; 
+    
+    if(iteration > maxIter){        
+      std::cout<<"CUDA : too many iterations!"<<std::endl;
+      exit(-1);
+    }
+    iteration++;
+  }
+  printf("RAJA Nested Loop CUDA Policy: \n");
+  printf("Top right current: %lg \n", d_I[N+N*(N+2)]);
+  printf("No of iterations: %d \n \n",iteration);
+  //======================================
   
   //Clean up
   delete [] I, Iold;
-
 
   return 0;
 }
