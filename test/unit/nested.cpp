1e2c3b51c66d47e3b32213e20af8bbd97e8f6010
Tom Scogland <scogland1@llnl.gov>
2017-08-18 13:45:57 -0700

    make tests more meaningful and less noisy

--- test/unit/nested.cpp
+++ test/unit/nested.cpp
@@ -4,19 +4,34 @@
 
 #include <cstdio>
 
+using RAJA::Index_type;
+using RAJA::View;
+using RAJA::Layout;
+using layout_2d = Layout<2, RAJA::Index_type>;
+using view_2d = View<Index_type, layout_2d>;
+static constexpr Index_type x_len = 5;
+static constexpr Index_type y_len = 5;
+
 RAJA_INDEX_VALUE(TypedIndex, "TypedIndex");
 TEST(Nested, Basic)
 {
   using namespace RAJA::nested;
-  using Index_type = RAJA::Index_type;
   using pol =
       Policy<For<1, RAJA::seq_exec>, TypedFor<0, RAJA::seq_exec, TypedIndex>>;
+  Index_type* data = new Index_type[x_len * y_len];
+  view_2d v(data, x_len, y_len);
   RAJA::nested::forall(pol{},
                        camp::make_tuple(RAJA::RangeSegment(0, 5),
                                         RAJA::RangeSegment(0, 5)),
                        [=](TypedIndex i, Index_type j) {
-                         printf("%ld, %ld\n", *i, j);
+                         v(*i, j) = (*i) * x_len + j;
                        });
+  for (Index_type i = 0; i < x_len; ++i) {
+    for (Index_type j = 0; j < y_len; ++j) {
+      ASSERT_EQ(v(i, j), i * x_len + j);
+    }
+  }
+  delete[] data;
 }
 
 TEST(Nested, collapse)
@@ -24,12 +39,20 @@ TEST(Nested, collapse)
   using namespace RAJA::nested;
   using Index_type = RAJA::Index_type;
   using pol = Policy<Collapse<RAJA::seq_exec, For<0>, For<1>>>;
+  Index_type* data = new Index_type[x_len * y_len];
+  view_2d v(data, x_len, y_len);
   RAJA::nested::forall(pol{},
                        camp::make_tuple(RAJA::RangeSegment(0, 5),
                                         RAJA::RangeSegment(0, 5)),
                        [=](Index_type i, Index_type j) {
-                         printf("%ld, %ld\n", i, j);
+                         v(i, j) = i * x_len + j;
                        });
+  for (Index_type i = 0; i < x_len; ++i) {
+    for (Index_type j = 0; j < y_len; ++j) {
+      ASSERT_EQ(v(i, j), i * x_len + j);
+    }
+  }
+  delete[] data;
 }
 #if defined(RAJA_ENABLE_OPENMP)
 TEST(Nested, BasicOMP)
@@ -38,12 +61,20 @@ TEST(Nested, BasicOMP)
   using Index_type = RAJA::Index_type;
   using pol = Policy<For<1, RAJA::seq_exec>,
                      TypedFor<0, RAJA::omp_parallel_for_exec, TypedIndex>>;
+  Index_type* data = new Index_type[x_len * y_len];
+  view_2d v(data, x_len, y_len);
   RAJA::nested::forall(pol{},
                        camp::make_tuple(RAJA::RangeSegment(0, 5),
                                         RAJA::RangeSegment(0, 5)),
                        [=](TypedIndex i, Index_type j) {
-                         printf("%ld, %ld\n", *i, j);
+                         v(*i, j) = (*i) * x_len + j;
                        });
+  for (Index_type i = 0; i < x_len; ++i) {
+    for (Index_type j = 0; j < y_len; ++j) {
+      ASSERT_EQ(v(i, j), i * x_len + j);
+    }
+  }
+  delete[] data;
 }
 #endif  // RAJA_ENABLE_CUDA
 
@@ -55,11 +86,23 @@ CUDA_TEST(Nested, BasicCuda)
   using Index_type = RAJA::Index_type;
   using pol = Policy<For<1, RAJA::seq_exec>,
                      TypedFor<0, RAJA::cuda_exec<128>, TypedIndex>>;
+
+  Index_type* data;
+  cudaMallocManaged(&data,
+                    sizeof(Index_type) * x_len * y_len,
+                    cudaMemAttachGlobal);
+  view_2d v(data, x_len, y_len);
   RAJA::nested::forall(pol{},
                        camp::make_tuple(RAJA::RangeSegment(0, 5),
                                         RAJA::RangeSegment(0, 5)),
                        [=] __device__(TypedIndex i, Index_type j) {
-                         printf("%ld, %ld\n", *i, j);
+                         v(*i, j) = (*i) * x_len + j;
                        });
+  for (Index_type i = 0; i < x_len; ++i) {
+    for (Index_type j = 0; j < y_len; ++j) {
+      ASSERT_EQ(v(i, j), i * x_len + j);
+    }
+  }
+  cudaFree(data);
 }
 #endif  // RAJA_ENABLE_CUDA
