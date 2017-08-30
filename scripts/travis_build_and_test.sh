cb444c577d4cdac7da73263a13bbf169e56a509f
Tom Scogland <scogland1@llnl.gov>
2017-08-30 10:31:31 -0700

    tweak travis to avoid intel on external PRs

---
+++ scripts/travis_build_and_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ~/.bashrc
+cd ${TRAVIS_BUILD_DIR}
+mkdir travis-build
+cd travis-build
+if [[ "$DO_BUILD" == "yes" ]] ; then
+    cmake -DCMAKE_CXX_COMPILER="${COMPILER}" ${CMAKE_EXTRA_FLAGS} ../
+    make -j 3 VERBOSE=1
+    if [[ "${DO_TEST}" == "yes" ]] ; then
+        ctest -V
+        stat = $?
+        [[ $stat -ne 0 ]] && exit $stat
+    fi
+fi
+
