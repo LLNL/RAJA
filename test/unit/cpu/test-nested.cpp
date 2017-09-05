0dc8da1ab47aaeec69225049685478f73e7c245f
artv3 <artv3.89@gmail.com>
2017-09-05 16:27:27 -0700

    forgot to comment comma on test-nested.cpp

--- test/unit/cpu/test-nested.cpp
+++ test/unit/cpu/test-nested.cpp
@@ -103,7 +103,7 @@ using TRANSFORMS =
 
 template <typename TRANSFORMS>
 using POLICIES =
-  std::tuple<ExecInfo<TRANSFORMS, seq_exec, seq_exec>, //Broken for SIMD... 
+  std::tuple<ExecInfo<TRANSFORMS, seq_exec, seq_exec> //Broken for SIMD... 
                //ExecInfo<TRANSFORMS, seq_exec, simd_exec>,
                //ExecInfo<TRANSFORMS, simd_exec, simd_exec>,
 #if defined(RAJA_ENABLE_OPENMP)
@@ -111,9 +111,8 @@ using POLICIES =
                //OMPExecInfo<TRANSFORMS, simd_exec, omp_for_nowait_exec>,
                //OMPExecInfo<TRANSFORMS, simd_exec, omp_for_nowait_exec>
 #endif
-#if defined(RAJA_ENABLE_TBB)
-               ,
-               ExecInfo<TRANSFORMS, seq_exec, tbb_for_exec>
+#if defined(RAJA_ENABLE_TBB)               
+             ,ExecInfo<TRANSFORMS, seq_exec, tbb_for_exec>
                //ExecInfo<TRANSFORMS, simd_exec, tbb_for_exec>
 #endif
                >;
