57546e4bc48f7b48ac2533a0c518446056955c44
Tom Scogland <scogland1@llnl.gov>
2017-09-08 17:33:49 -0700

    fix download url, scheme changed on us

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -1,7 +1,7 @@
 export LLVM_PATH=${HOME}/llvm/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04
 export PATH=${LLVM_PATH}/bin:${PATH}
 export LD_LIBRARY_PATH=${LLVM_PATH}/lib:${LD_LIBRARY_PATH}
-export DOWNLOAD_URL=http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+[[ -z ${DOWNLOAD_URL+x} ]] && export DOWNLOAD_URL=http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 export TARFILE=${HOME}/download/llvm-${LLVM_VERSION}.tar.xz
 if ! [[ -d "${LLVM_PATH}" ]]; then 
     if ! [[ -f ${TARFILE} ]]; then
