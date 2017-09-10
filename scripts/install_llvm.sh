ae9598efca5684393eff991500d1e9c990513c20
Tom Scogland <scogland1@llnl.gov>
2017-09-09 19:48:56 -0700

    fix cache

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -3,7 +3,7 @@ export PATH=${LLVM_PATH}/bin:${PATH}
 export LD_LIBRARY_PATH=${LLVM_PATH}/lib:${LD_LIBRARY_PATH}
 [[ -z ${DOWNLOAD_URL+x} ]] && export DOWNLOAD_URL=http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 export TARFILE=${HOME}/download/llvm-${LLVM_VERSION}.tar.xz
-if ! [[ -d "${LLVM_PATH}" ]]; then 
+if ! [[ -x "${LLVM_PATH}/bin/clang++" ]]; then 
     if ! [[ -f ${TARFILE} ]]; then
         echo "curl -o ${TARFILE} ${DOWNLOAD_URL}"
         curl -o ${TARFILE} ${DOWNLOAD_URL}
