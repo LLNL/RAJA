8698ef8f4562d2286d3bac76c190ab366a71902e
artv3 <artv3.89@gmail.com>
2017-09-12 10:47:38 -0700

    added loop test

--- test/unit/cpu/test-reduce.cpp
+++ test/unit/cpu/test-reduce.cpp
@@ -65,16 +65,16 @@ using namespace RAJA;
 
 using TestingTypes = ::testing::
     Types<
-  std::tuple<ExecPolicy<seq_segit, seq_exec>, seq_reduce> 
-  std::tuple<ExecPolicy<seq_segit, loop_exec>, seq_reduce>,
+  std::tuple<ExecPolicy<seq_segit, seq_exec>, seq_reduce>, 
+  std::tuple<ExecPolicy<seq_segit, loop_exec>, loop_reduce> 
 #ifdef RAJA_ENABLE_OPENMP
-  std::tuple<ExecPolicy<omp_parallel_for_segit, loop_exec>, omp_reduce>,
-  std::tuple<ExecPolicy<omp_parallel_for_segit, loop_exec>,
-  omp_reduce_ordered>
+  
+  ,std::tuple<ExecPolicy<omp_parallel_for_segit, loop_exec>, omp_reduce>
+  ,std::tuple<ExecPolicy<omp_parallel_for_segit, loop_exec>,omp_reduce_ordered>              
 #endif
 #ifdef RAJA_ENABLE_TBB
-  ,std::tuple<ExecPolicy<seq_segit, tbb_for_exec>, tbb_reduce>
-  ,std::tuple<ExecPolicy<tbb_for_exec, loop_exec>, tbb_reduce>
+          ,std::tuple<ExecPolicy<seq_segit, tbb_for_exec>, tbb_reduce>
+           ,std::tuple<ExecPolicy<tbb_for_exec, loop_exec>, tbb_reduce>
 #endif
         >;
 
