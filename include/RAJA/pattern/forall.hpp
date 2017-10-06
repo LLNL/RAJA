d32bcfa1f357d5ab65aa3adab1ad8e1759d8221b
Tom Scogland <scogland1@llnl.gov>
2017-10-06 15:55:06 -0700

    make privatizer forallN+cuda compatible

--- include/RAJA/pattern/forall.hpp
+++ include/RAJA/pattern/forall.hpp
@@ -110,11 +110,7 @@ struct Privatizer {
   using value_type = camp::decay<T>;
   using reference_type = value_type&;
   value_type priv;
-#ifdef __CUDA_ARCH__
-  RAJA_DEVICE Privatizer(const T& o) : priv{o} {}
-#else
-  Privatizer(const T& o) : priv{o} {}
-#endif
+  RAJA_HOST_DEVICE Privatizer(const T& o) : priv{o} {}
   RAJA_HOST_DEVICE reference_type get_priv() { return priv; }
 };
 
