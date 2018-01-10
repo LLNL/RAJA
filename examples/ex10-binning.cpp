d93052cebc0d55bd9cf4523861d3b2d7671f1d7d
artv3 <artv3.89@gmail.com>
2018-01-10 11:04:05 -0800

    clang format

--- examples/ex10-binning.cpp
+++ examples/ex10-binning.cpp
@@ -25,8 +25,8 @@
  *  Binning Example
  *
  *  Given an array of length N containing integers ranging from [0, M),
- *  this example uses RAJA atomics to count the number of instances a 
- *  number between 0 and M appear. 
+ *  this example uses RAJA atomics to count the number of instances a
+ *  number between 0 and M appear.
  *
  *  RAJA features shown:
  *    - `forall` loop iteration template method
@@ -42,78 +42,78 @@
 const int CUDA_BLOCK_SIZE = 256;
 #endif
 
-template<typename T>
+template <typename T>
 void printBins(T* bins, int M);
 
 int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 {
   //
-  //Define the inital array containing values between 0 and M and
-  //create the iteration bounds
+  // Define the inital array containing values between 0 and M and
+  // create the iteration bounds
   //
-  int M = 10; 
+  int M = 10;
   int N = 30;
-  RAJA::RangeSegment ArraySz(0,N);
+  RAJA::RangeSegment array_range(0, N);
 
-  int *array = memoryManager::allocate<int>(N);
-  int *bins  = memoryManager::allocate<int>(M);
+  int* array = memoryManager::allocate<int>(N);
+  int* bins = memoryManager::allocate<int>(M);
 
-  RAJA::forall<RAJA::seq_exec>(ArraySz, [=] (int i) {
-
-      array[i] = rand () % M; 
-
-  });
+  RAJA::forall<RAJA::seq_exec>(array_range, [=](int i) {
+                               
+      array[i] = rand() % M;
+      
+    });
   //----------------------------------------------------------------------------//
 
-  std::cout << "\n\n Running RAJA sequential binning"<<std::endl;
-  memset(bins,0,M*sizeof(int));
-  using EXEC_POL1   = RAJA::seq_exec;
+  std::cout << "\n\n Running RAJA sequential binning" << std::endl;
+  memset(bins, 0, M * sizeof(int));
+  using EXEC_POL1 = RAJA::seq_exec;
   using ATOMIC_POL1 = RAJA::atomic::seq_atomic;
-  RAJA::forall<EXEC_POL1>(ArraySz, [=] (int i){
-     
-      RAJA::atomic::atomicAdd< ATOMIC_POL1 >( &bins[array[i]] , 1 );
-     
-  });
-  
-  printBins(bins,M);
-  //----------------------------------------------------------------------------//  
+  RAJA::forall<EXEC_POL1>(array_range, [=](int i) {
+                                                      
+      RAJA::atomic::atomicAdd<ATOMIC_POL1>(&bins[array[i]], 1);
+
+    });
+
+  printBins(bins, M);
+//----------------------------------------------------------------------------//
 
 #if defined(RAJA_ENABLE_OPENMP)
 
-  std::cout << "\n\n Running RAJA OMP binning"<<std::endl;
-  memset(bins,0,M*sizeof(int));
-  using EXEC_POL2   = RAJA::omp_parallel_for_exec;
+  std::cout << "\n\n Running RAJA OMP binning" << std::endl;
+  memset(bins, 0, M * sizeof(int));
+  using EXEC_POL2 = RAJA::omp_parallel_for_exec;
   using ATOMIC_POL2 = RAJA::atomic::omp_atomic;
-  RAJA::forall<EXEC_POL2>(ArraySz, [=] (int i){
-     
-      RAJA::atomic::atomicAdd< ATOMIC_POL2 >( &bins[array[i]] , 1 );
-      
-  });
-  
-  printBins(bins,M);
+  RAJA::forall<EXEC_POL2>(array_range, [=](int i) {
+                          
+      RAJA::atomic::atomicAdd<ATOMIC_POL2>(&bins[array[i]], 1);
+                                           
+    });
+
+  printBins(bins, M);
 #endif
-  //----------------------------------------------------------------------------//  
+//----------------------------------------------------------------------------//
 
 
 #if defined(RAJA_ENABLE_CUDA)
 
-  std::cout << "\n\nRunning RAJA CUDA binning"<<std::endl;
-  memset(bins,0,M*sizeof(int));
-  using EXEC_POL3   = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
+  std::cout << "\n\nRunning RAJA CUDA binning" << std::endl;
+  memset(bins, 0, M * sizeof(int));
+  using EXEC_POL3 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
   using ATOMIC_POL3 = RAJA::atomic::cuda_atomic;
-  RAJA::forall<EXEC_POL3> (ArraySz, [=] RAJA_DEVICE (int i){
-     
-      RAJA::atomic::atomicAdd< ATOMIC_POL3 >( &bins[array[i]] , 1 );
-      
-  });
-  
-  printBins(bins,M);
-#endif
-  //----------------------------------------------------------------------------//  
+  RAJA::forall<EXEC_POL3>(array_range, [=] RAJA_DEVICE(int i) {
+                          
+      RAJA::atomic::atomicAdd<ATOMIC_POL3>(&bins[array[i]], 1);
+                                                 
+    });
 
-  
+  printBins(bins, M);
+#endif
+  //----------------------------------------------------------------------------//
+
+
   //
-  //Clean up dellacate data
+  // Clean up dellacate data
   //
   memoryManager::deallocate(array);
   memoryManager::deallocate(bins);
@@ -123,25 +123,26 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
   return 0;
 }
 
-template<typename T>
-void printBins(T* bins, int M){
+template <typename T>
+void printBins(T* bins, int M)
+{
 
-  std::cout<<"Number of instances |";
-  for(int i=0; i<M; ++i){
-    std::cout<<bins[i]<<" ";
+  std::cout << "Number of instances |";
+  for (int i = 0; i < M; ++i) {
+    std::cout << bins[i] << " ";
   }
-  std::cout<<""<<std::endl;
+  std::cout << "" << std::endl;
 
-  std::cout<<"---------------------------";
-  for(int i=0; i<M; ++i){
-    std::cout<<"-"<<"";
+  std::cout << "---------------------------";
+  for (int i = 0; i < M; ++i) {
+    std::cout << "-"
+              << "";
   }
-  std::cout<<""<<std::endl;
+  std::cout << "" << std::endl;
 
-  std::cout<<"Index id            |";
-  for(int i=0; i<M; ++i){
-    std::cout<<i<<" ";
+  std::cout << "Index id            |";
+  for (int i = 0; i < M; ++i) {
+    std::cout << i << " ";
   }
-  std::cout<<"\n"<<std::endl;
-
+  std::cout << "\n" << std::endl;
 }
