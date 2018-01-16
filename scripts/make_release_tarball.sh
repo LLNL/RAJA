a14c228b9b02845205d08b2fa1aa07396b70f581
David Beckingsale <davidbeckingsale@gmail.com>
2018-01-16 10:38:21 -0800

    Add script to create release tarball

---
+++ scripts/make_release_tarball.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TAR_CMD=gtar
+VERSION=0.5.0
+
+git archive --prefix=RAJA-${VERSION}/ -o RAJA-${VERSION}.tar HEAD 2> /dev/null
+
+echo "Running git archive submodules..."
+
+p=`pwd` && (echo .; git submodule foreach) | while read entering path; do
+    temp="${path%\'}";
+    temp="${temp#\'}";
+    path=$temp;
+    [ "$path" = "" ] && continue;
+    (cd $path && git archive --prefix=RAJA-${VERSION}/$path/ HEAD > $p/tmp.tar && ${TAR_CMD} --concatenate --file=$p/RAJA-${VERSION}.tar $p/tmp.tar && rm $p/tmp.tar);
+done
+
+gzip RAJA-${VERSION}.tar
