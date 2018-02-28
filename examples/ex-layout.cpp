3571a847f8fc1ab24c970f9b5c4c2c354572aae3
Arturo Vargas <artv3.89@gmail.com>
2018-02-27 18:36:16 -0800

    finished draft of layout example

--- examples/ex-layout.cpp
+++ examples/ex-layout.cpp
@@ -18,11 +18,12 @@
 #include <iostream>
 
 #include "RAJA/RAJA.hpp"
+#include "memoryManager.hpp"
 
 /*
  *  Layout Example
  *
- *  Populates a multi-dimensional array
+ *  Populates a 4-dimensional array
  *
  *  RAJA features shown:
  *    - `forall` loop iteration template method
@@ -30,7 +31,7 @@
  */
 
 //
-// Functions for checking and printing results
+// Function to check result
 //
 template<typename T>
 void checkResult(T A, T B, int len);
@@ -39,74 +40,75 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 {
 
 //
-//Define Dimension
+//Define dimension
 //
   const int dim = 4;
 
 //
 // Define stride length
 //  
-  const int stride1 = 4;
-  const int stride2 = 9;
-  const int stride3 = 12;
-  const int stride4 = 6;
+  const int Istride = 2;
+  const int Jstride = 3;
+  const int Mstride = 4;
+  const int Nstride = 5;
 
 //
-//Define Range Segments 
+//Define range segment
 //
-  RAJA::RangeSegment range_1(0,stride1);
-  RAJA::RangeSegment range_2(0,stride2);
-  RAJA::RangeSegment range_3(0,stride3);
-  RAJA::RangeSegment range_4(0,stride4);
+  RAJA::RangeSegment Irange(0,Istride);
+  RAJA::RangeSegment Jrange(0,Jstride);
+  RAJA::RangeSegment Mrange(0,Mstride);
+  RAJA::RangeSegment Nrange(0,Nstride);
 
 //
-// Allocate and initialize vector data.
+// Allocate 1D arrays
 //
-  int array_len = stride1*stride2*stride3*stride4;
+  int array_len = Istride*Jstride*Mstride*Nstride;
+
   int* A = new int[array_len];
   int* B = new int[array_len];
 
   //Wrap A in a view
-  RAJA::View<int, RAJA::Layout<dim> > Aview(A, stride1, stride2, stride3, stride4);
+  RAJA::View<int, RAJA::Layout<dim> > A_IJMN_view(A, Istride, Jstride, Mstride, Nstride);
 
-  //Create layout object
-  RAJA::Layout<dim> layout(stride1, stride2, stride3, stride4);
+  //Create layout objects assuming an IJMN indexing
+  RAJA::Layout<dim> IJMN_layout = RAJA::make_permuted_layout({Istride,Jstride,Mstride,Nstride}, RAJA::as_array<RAJA::Perm<0,1,2,3>>::get());
+  //Equivalent to : RAJA::Layout<dim> IJMN_Layout(Istride,JStride,MStride,Nstride);
 
 //----------------------------------------------------------------------------//
 //
-// In the following, we show how a layout
-// object may be used to convert from 1D index 
-// to a 4D index. 
+// In the following example we show how a layout
+// object may be used to convert a 1D index to a 4D index.
 //----------------------------------------------------------------------------//   
   RAJA::forall<RAJA::seq_exec>(RAJA::RangeSegment(0, array_len), [=] (int id) {
       
       //Map from 1D index to 4D index
       int i,j,m,n;
-      layout.toIndices(id,i,j,m,n);
-
-      Aview(i,j,m,n) = id%dim;
+      IJMN_layout.toIndices(id,i,j,m,n);
+      
+      A_IJMN_view(i,j,m,n) = id%dim;
   });
 
 
 //----------------------------------------------------------------------------//
 //
-// In the following, we show how a layout
+// In the following example we show how a layout
 // object may be used to convert from a 4D index 
 // to a 1D index.
-//----------------------------------------------------------------------------//   
-  using NESTED_EXEC_POL =
+//----------------------------------------------------------------------------//
+  using IJMN_EXEC_POL =
     RAJA::nested::Policy< 
     RAJA::nested::For<3, RAJA::seq_exec>,
     RAJA::nested::For<2, RAJA::seq_exec>,
     RAJA::nested::For<1, RAJA::seq_exec>,                
     RAJA::nested::For<0, RAJA::seq_exec> >;
 
-
-  RAJA::nested::forall(NESTED_EXEC_POL{},
-                       RAJA::make_tuple(range_1,range_2,range_3, range_4),
+  
+  RAJA::nested::forall(IJMN_EXEC_POL{},
+                       RAJA::make_tuple(Irange, Jrange, Mrange, Nrange),
                        [=](int i, int j, int m, int n) {
-
-       int id = layout(i,j,m,n);
+                         
+       int id = IJMN_layout(i,j,m,n);
        B[id] = id%dim;
   });
 
@@ -115,12 +117,61 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 //
   checkResult(A, B, array_len);
 
+
+
+//============================================================================//
+//  
+//As a second example we can permute the the layout object
+//============================================================================//
+
+  //Create a view with permuted strides
+  RAJA::View<int, RAJA::Layout<dim> > A_NMJI_view(A, Nstride, Mstride, Jstride, Istride);
+  
+  //Permute the layout
+  RAJA::Layout<dim> NMJI_layout = RAJA::make_permuted_layout({Istride,Jstride,Mstride,Nstride}, RAJA::as_array<RAJA::Perm<3,2,1,0>>::get());
+  
+//----------------------------------------------------------------------------//
+//
+//Conversion of 1D index to 4D index with an NMJI layout
+//----------------------------------------------------------------------------//
+  RAJA::forall<RAJA::seq_exec>(RAJA::RangeSegment(0, array_len), [=] (int id) {
+      
+      //Map from 1D index to 4D index
+      int m,n,i,j;
+      NMJI_layout.toIndices(id,i,j,m,n);
+      A_NMJI_view(n,m,j,i) = id/dim;
+
+    });
+  
+//----------------------------------------------------------------------------//
+//
+// Conversion on from a 4D index with permuted layout (NMJI) to
+// to a 1D index.
+//----------------------------------------------------------------------------//
+    using NMJI_EXEC_POL =
+    RAJA::nested::Policy< 
+    RAJA::nested::For<3, RAJA::seq_exec>, 
+    RAJA::nested::For<2, RAJA::seq_exec>,
+    RAJA::nested::For<1, RAJA::seq_exec>,
+    RAJA::nested::For<0, RAJA::seq_exec>>;
+
+
+  RAJA::nested::forall(NMJI_EXEC_POL{},
+                       RAJA::make_tuple(Irange, Jrange, Mrange, Nrange),
+                       [=] (int i, int j, int m, int n) {
+                         
+                         int id = NMJI_layout(i,j,m,n);
+                         B[id] = id/dim;                          
+  });
+  checkResult(A, B, array_len);
+
+
+  
 //
 // Clean up. 
 //
   delete[] A;
   delete[] B;
-
   std::cout << "\n DONE!...\n";
 
   return 0;
