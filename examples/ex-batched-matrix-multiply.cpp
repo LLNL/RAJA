70a5c72172fdb3259f1650ac4051b17f0e7f7b3a
artv3 <artv3.89@gmail.com>
2018-05-02 17:41:21 -0700

    pass at batched matrix multiply example

--- examples/ex-batched-matrix-multiply.cpp
+++ examples/ex-batched-matrix-multiply.cpp
@@ -13,23 +13,27 @@
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <chrono>
-
-#include "memoryManager.hpp"
-
 #include "RAJA/RAJA.hpp"
+#include "RAJA/util/Timer.hpp"
+
+#include <iostream>
+#include "memoryManager.hpp"
 
 /*
  *  Batched Matrix Multiply Example
  *
- *  Multiplies NMAT NCOLSxNROWS matrices.
- *  Illustrates how RAJA views allow developers
- *  to easily change between data layouts. 
- *  Notibly we demonstrate how tailoring data layouts
- *  to specific compute architures enhances performances.
+ *  Carries out matrix multiplication
+ *  for NMAT matrices. Here we assume
+ *  matrices are small (3 x 3).
+ *  Each iteration of the RAJA forall loop
+ *  will multiply a matrix. 
+ *
+ *  Additionally, we explore performance for two data layouts. 
+ *  Layout 1: Assumes matrices are contiguous in memory
+ *  i.e. [a00, a01, a02, ... ]
+ *
+ * Layout 2: Multiplies matrices assuming matries entries are grouped together
+ * i.e. [a00, b00, c00, a01, b01, c01 ... ]
  *
  *  RAJA features shown:
  *    -  RAJA View
@@ -39,64 +43,42 @@
  */
 
 using RAJA::Index_type;
-using Clock = std::chrono::high_resolution_clock;
-
-#define A(elem, row, col) A[col + NCOLS*(row + elem*NROWS)]
-#define B(elem, row, col) B[col + NCOLS*(row + elem*NROWS)]
-#define C1(elem, row, col) C1[col + NCOLS*(row + elem*NROWS)]
-
-#define Al2(elem, row, col) Al2[elem + NMAT*(col + row*NCOLS)]
-#define Bl2(elem, row, col) Bl2[elem + NMAT*(col + row*NCOLS)]
-#define C2(elem, row, col) C2[elem + NMAT*(col + row*NCOLS)]
-
 
 /*
   CUDA_BLOCK_SIZE - specifies the number of threads in a CUDA thread block
 */
 #if defined(RAJA_ENABLE_CUDA)
 const int CUDA_BLOCK_SIZE = 256;
-using KernelPol = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
-#else
-using KernelPol = RAJA::simd_exec;
+using GPUPol = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
 #endif
+using CPUPol = RAJA::loop_exec;
+
+
 
 //Dimensions of matrices
 const Index_type NCOLS = 3;
 const Index_type NROWS = 3;
-const Index_type NMAT  = 8000000;
+const Index_type NMAT  = 80000000;
 const Index_type NELEM = NCOLS*NROWS*NMAT;
 
 //Number of iterations
-const int NITER = 23;
+const int NITER = 21;
 
 //
 // Functions for comparing outputs
 //
-void compareOutput(double *C, double *CComp,Index_type N);
+template<typename T, typename U>
+void compareOutput(T C, U Cl2,Index_type N);
 
 //
-//Version 1:
-//Multiplies matrices assuming entries in a matrix are grouped together
-//i.e. [a00, a01, a02, ... ]
-//
-template<typename myPolicy>
-void multiplyVer1(const double * const RAJA_RESTRICT A, const double * const RAJA_RESTRICT B,
-                  double * const RAJA_RESTRICT C, const Index_type N);
-//
-//Version 2:
-//Multiplies matrices assuming matries entries are grouped together
-//i.e. [a00, b00, c00, a01, b01, c01 ... ]
-//
-template<typename myPolicy>
-void multiplyVer2(const double * const RAJA_RESTRICT A, const double * const RAJA_RESTRICT B,
-                   double * const RAJA_RESTRICT C, const Index_type N);
+template<typename Pol, typename T>
+void multiplyViewCPU(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
+                     T const RAJA_RESTRICT Cview, const Index_type N);
 
-//
-//Version 3
-//Layout agnostic function
-template<typename myPolicy, typename T>
-void multiplyView(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
-                  T const RAJA_RESTRICT Cview, const Index_type N);
+
+template<typename Pol, typename T>
+void multiplyViewGPU(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
+                     T const RAJA_RESTRICT Cview, const Index_type N);
 
 
 int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
@@ -106,193 +88,152 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 
   double myMin;
   srand(time(NULL));
+  auto timer = RAJA::Timer();
 
-  //Stores data in layout 1
+//
+//Space for data in layout 1
+//
   double * A = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
   double * B = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
+  double * C = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
 
-  //Stores data in layout 2
+//
+//Default layout - equivalent to indexing via 
+//A[c + NCOLS*(r + NROWS*e)]
+//
+  auto layout = RAJA::make_permuted_layout({{NMAT, NROWS, NCOLS}}, RAJA::as_array<RAJA::Perm<0,1,2> >::get() );
+  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Aview(A,layout);
+  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Bview(B,layout);
+  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Cview(C,layout);
+
+//
+//Stores data in layout 2
+//
   double * Al2 = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
   double * Bl2 = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
+  double * Cl2 = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
+  
+//
+//Permuted layout - equivalent to indexing via 
+//A[e + NELEM*(r + NROWS*e)]
+//
+  auto layout2 = RAJA::make_permuted_layout({{NMAT, NROWS, NCOLS}}, RAJA::as_array<RAJA::Perm<1,2,0> >::get() );
+  RAJA::View<double, RAJA::Layout<3, Index_type, 0> > Al2view(Al2,layout2);
+  RAJA::View<double, RAJA::Layout<3, Index_type, 0> > Bl2view(Bl2,layout2);
+  RAJA::View<double, RAJA::Layout<3, Index_type, 0> > Cl2view(Cl2,layout2);
 
-  //Allocates space for output
-  double * C1 = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
-  double * C2 = memoryManager::allocate<double>(NCOLS*NROWS*NMAT);
-
-  //
-  //Initialize data
-  //
+//
+//Initialize data
+//
   for(Index_type e=0; e<NMAT; ++e){
     for(Index_type row=0; row<NROWS; ++row){
       for(Index_type col=0; col<NCOLS; ++col){
-        A(e,row,col) = rand() % 50 + 1; 
-        B(e,row,col) = rand() % 50 + 1; 
-        Al2(e,row,col) = A(e,row,col);
-        Bl2(e,row,col) = B(e,row,col);
+        Aview(e,row,col) = rand() % 50 + 1; 
+        Bview(e,row,col) = rand() % 50 + 1; 
+        Al2view(e,row,col) = Aview(e,row,col);
+        Bl2view(e,row,col) = Bview(e,row,col);
       }
     }                 
   }
 
-  //----------------------------------------------------------------------------//                                                                                         
-  //Version 1 with macros
-  //----------------------------------------------------------------------------//                                                                                          
-  myMin = std::numeric_limits<double>::max();
-  for(Index_type i=0; i<NITER; ++i){
-
-    auto start = Clock::now();
-    multiplyVer1<KernelPol>(A,B,C1,NMAT);
-#if defined(RAJA_ENABLE_CUDA)
-    cudaDeviceSynchronize();    
-#endif
-    auto end = Clock::now();
-
-    double tMin = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();   
-    if(tMin < myMin) myMin = tMin;
-  }
-  std::cout<<"Version 1 with macros min run time : "<<myMin<<" miliseconds"<<std::endl;
-  //----------------------------------------------------------------------------//                                                                                          
-
-  //----------------------------------------------------------------------------//                                                                                         
-  //Version 1 with views
-  //----------------------------------------------------------------------------//                                                                                          
-  //Equivalent to indexing via B[col + NCOLS*(row + NROWS*elem)] 
-  auto layout = RAJA::make_permuted_layout({{NMAT, NROWS, NCOLS}}, RAJA::as_array<RAJA::Perm<0,1,2> >::get() );
-  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Aview(A,layout);
-  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Bview(B,layout);
-  RAJA::View<double, RAJA::Layout<3, Index_type, 2> > Cview(C2,layout);
-
-  myMin = std::numeric_limits<double>::max();
+//-------------------------------------------
+//Matrix multiply with layout 1 on the CPU
+//
+  myMin = std::numeric_limits<double>::max();  
   for(Index_type i=0; i<NITER; ++i){
     
-    auto start = Clock::now();
-    multiplyView<KernelPol>(Aview,Bview,Cview,NMAT);
-#if defined(RAJA_ENABLE_CUDA)
-    cudaDeviceSynchronize();    
-#endif
-    auto end = Clock::now();
-    
-    double tMin = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); 
-    if(tMin < myMin) myMin = tMin;
-  }
-  std::cout<<"Version 1 with RAJA views min run time : "<<myMin<<" milliseconds"<<std::endl;
+  timer.start();
+  multiplyViewCPU<CPUPol>(Aview,Bview,Cview,NMAT);
+  timer.stop();
   
-  //
-  //Compare output
-  //
-  compareOutput(C1, C2, NELEM);
-
-
-  //----------------------------------------------------------------------------//                                                                                         
-  //Version 2 with macros
-  //----------------------------------------------------------------------------//                                                                                          
-  myMin = std::numeric_limits<double>::max();
-  for(Index_type i=0; i<NITER; ++i){
-
-    auto start = Clock::now();
-    multiplyVer2<KernelPol>(Al2,Bl2,C1,NMAT);
-#if defined(RAJA_ENABLE_CUDA)
-    cudaDeviceSynchronize();    
-#endif
-    auto end = Clock::now();
-
-    double tMin = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();   
-    if(tMin < myMin) myMin = tMin;
+  RAJA::Timer::ElapsedType tMin = timer.elapsed();
+  if(tMin < myMin) myMin = tMin;
   }
-  std::cout<<"Version 2 with macros min run time : "<<myMin<<" seconds"<<std::endl;
-  //----------------------------------------------------------------------------//                                                                                          
+  std::cout<<"Matrix Multiplication with layout 1 run time : "<<myMin<<" seconds"<<std::endl;
+//-------------------------------------------
 
-  //----------------------------------------------------------------------------//                                                                                         
-  //Version 2 with views
-  //----------------------------------------------------------------------------//
-  auto layout2 = RAJA::make_permuted_layout({{NMAT, NROWS, NCOLS}}, RAJA::as_array<RAJA::Perm<1,2,0> >::get() );
-  RAJA::View<double, RAJA::Layout<3, Index_type, 0>> Al2view(Al2,layout2);
-  RAJA::View<double, RAJA::Layout<3, Index_type, 0>> Bl2view(Bl2,layout2);
-  RAJA::View<double, RAJA::Layout<3, Index_type, 0>> Cl2view(C2,layout2);
-
-  myMin = std::numeric_limits<double>::max();
+//-------------------------------------------
+//Matrix multiply with layout 2 on the CPU
+//
+  myMin = std::numeric_limits<double>::max();  
   for(Index_type i=0; i<NITER; ++i){
     
-    auto start = Clock::now();
-    multiplyView<KernelPol>(Al2view,Bl2view,Cl2view,NMAT);
-#if defined(RAJA_ENABLE_CUDA)
-    cudaDeviceSynchronize();    
-#endif
-    auto end = Clock::now();
-    
-    double tMin = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); 
-    if(tMin < myMin) myMin = tMin;
+  timer.start();
+  multiplyViewCPU<CPUPol>(Al2view,Bl2view,Cl2view,NMAT);
+  timer.stop();
+  
+  RAJA::Timer::ElapsedType tMin = timer.elapsed();
+  if(tMin < myMin) myMin = tMin;
   }
-  std::cout<<"Version 2 with RAJA views min run time : "<<myMin<<" milliseconds"<<std::endl;
+  std::cout<<"Matrix Multiplication with layout 2 run time : "<<myMin<<" seconds"<<std::endl;
+//---------------------------------------------
+
+//
+//Compare output
+//
+  compareOutput(Cview, Cl2view, NELEM);
+
+#if defined(RAJA_ENABLE_CUDA)
+//-------------------------------------------
+//Matrix multiply with layout 1 on the GPU
+//
+  myMin = std::numeric_limits<double>::max();  
+  for(Index_type i=0; i<NITER; ++i){
+    
+  timer.start();
+  multiplyViewGPU<GPUPol>(Aview,Bview,Cview,NMAT);
+  cudaDeviceSyncronize();
+  timer.stop();
   
-  //
-  //Compare output
-  //
-  compareOutput(C1, C2, NELEM);
+  RAJA::Timer::ElapsedType tMin = timer.elapsed();
+  if(tMin < myMin) myMin = tMin;
+  }
+  std::cout<<"Matrix Multiplication with layout 1 run time : "<<myMin<<" seconds"<<std::endl;
+//-------------------------------------------
+
+//-------------------------------------------
+//Matrix multiply with layout 2 on the GPU
+//
+  myMin = std::numeric_limits<double>::max();  
+  for(Index_type i=0; i<NITER; ++i){
+    
+  timer.start();
+  multiplyViewGPU<GPUPol>(Al2view,Bl2view,Cl2view,NMAT);
+  cudaDeviceSyncronize();
+  timer.stop();
   
+  RAJA::Timer::ElapsedType tMin = timer.elapsed();
+  if(tMin < myMin) myMin = tMin;
+  }
+  std::cout<<"Matrix Multiplication with layout 2 run time : "<<myMin<<" seconds"<<std::endl;
+//---------------------------------------------
+
+//
+//Compare output
+//
+  compareOutput(Cview, Cl2view, NELEM);  
+#endif
 
 //
 // Clean up.
 //
   memoryManager::deallocate(A);
   memoryManager::deallocate(B);
-  memoryManager::deallocate(C1);
-  memoryManager::deallocate(C2);
+  memoryManager::deallocate(C);
+  memoryManager::deallocate(Cl2);
 
   std::cout << "\n DONE!...\n";
 
   return 0;
 }
 
-template<typename myPolicy>
-void multiplyVer1(const double * const RAJA_RESTRICT A, const double * const RAJA_RESTRICT B,
-                  double * const RAJA_RESTRICT C1, const Index_type N)
-{
-
-  RAJA::forall<myPolicy>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (Index_type i) {
-      
-      C1(i,0,0) = A(i,0,0)*B(i,0,0) + A(i,0,1)*B(i,1,0) + A(i,0,2)*B(i,2,0);
-      C1(i,0,1) = A(i,0,0)*B(i,0,1) + A(i,0,1)*B(i,1,1) + A(i,0,2)*B(i,2,1);
-      C1(i,0,2) = A(i,0,0)*B(i,0,2) + A(i,0,1)*B(i,1,2) + A(i,0,2)*B(i,2,2);
-
-      C1(i,1,0) = A(i,1,0)*B(i,0,0) + A(i,1,1)*B(i,1,0) + A(i,1,2)*B(i,2,0);
-      C1(i,1,1) = A(i,1,0)*B(i,0,1) + A(i,1,1)*B(i,1,1) + A(i,1,2)*B(i,2,1);
-      C1(i,1,2) = A(i,1,0)*B(i,0,2) + A(i,1,1)*B(i,1,2) + A(i,1,2)*B(i,2,2);
-
-      C1(i,2,0) = A(i,2,0)*B(i,0,0) + A(i,2,1)*B(i,1,0) + A(i,2,2)*B(i,2,0);
-      C1(i,2,1) = A(i,2,0)*B(i,0,1) + A(i,2,1)*B(i,1,1) + A(i,2,2)*B(i,2,1);
-      C1(i,2,2) = A(i,2,0)*B(i,0,2) + A(i,2,1)*B(i,1,2) + A(i,2,2)*B(i,2,2);
-    });
-}
-
-
-template<typename myPolicy>
-void multiplyVer2(const double * const RAJA_RESTRICT Al2, const double * const RAJA_RESTRICT Bl2,
-                  double * const RAJA_RESTRICT C2, const Index_type N)
-{
-
-  RAJA::forall<myPolicy>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (Index_type i) {
-
-      C2(i,0,0) = Al2(i,0,0)*Bl2(i,0,0) + Al2(i,0,1)*Bl2(i,1,0) + Al2(i,0,2)*Bl2(i,2,0);
-      C2(i,0,1) = Al2(i,0,0)*Bl2(i,0,1) + Al2(i,0,1)*Bl2(i,1,1) + Al2(i,0,2)*Bl2(i,2,1);
-      C2(i,0,2) = Al2(i,0,0)*Bl2(i,0,2) + Al2(i,0,1)*Bl2(i,1,2) + Al2(i,0,2)*Bl2(i,2,2);
-
-      C2(i,1,0) = Al2(i,1,0)*Bl2(i,0,0) + Al2(i,1,1)*Bl2(i,1,0) + Al2(i,1,2)*Bl2(i,2,0);
-      C2(i,1,1) = Al2(i,1,0)*Bl2(i,0,1) + Al2(i,1,1)*Bl2(i,1,1) + Al2(i,1,2)*Bl2(i,2,1);
-      C2(i,1,2) = Al2(i,1,0)*Bl2(i,0,2) + Al2(i,1,1)*Bl2(i,1,2) + Al2(i,1,2)*Bl2(i,2,2);
-
-      C2(i,2,0) = Al2(i,2,0)*Bl2(i,0,0) + Al2(i,2,1)*Bl2(i,1,0) + Al2(i,2,2)*Bl2(i,2,0);
-      C2(i,2,1) = Al2(i,2,0)*Bl2(i,0,1) + Al2(i,2,1)*Bl2(i,1,1) + Al2(i,2,2)*Bl2(i,2,1);
-      C2(i,2,2) = Al2(i,2,0)*Bl2(i,0,2) + Al2(i,2,1)*Bl2(i,1,2) + Al2(i,2,2)*Bl2(i,2,2);
-    });
-}
-
-template<typename myPolicy, typename T>
-void multiplyView(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
-                  T const RAJA_RESTRICT Cview, const Index_type N)
+template<typename Pol, typename T>
+void multiplyViewCPU(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
+                     T const RAJA_RESTRICT Cview, const Index_type N)
 {
   
-  RAJA::forall<myPolicy>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (Index_type i) {
-      
+    RAJA::forall<Pol>(RAJA::RangeSegment(0, N), [=] (Index_type i) {
+
       Cview(i,0,0) = Aview(i,0,0)*Bview(i,0,0) + Aview(i,0,1)*Bview(i,1,0) + Aview(i,0,2)*Bview(i,2,0);
       Cview(i,0,1) = Aview(i,0,0)*Bview(i,0,1) + Aview(i,0,1)*Bview(i,1,1) + Aview(i,0,2)*Bview(i,2,1);
       Cview(i,0,2) = Aview(i,0,0)*Bview(i,0,2) + Aview(i,0,1)*Bview(i,1,2) + Aview(i,0,2)*Bview(i,2,2);
@@ -304,23 +245,46 @@ void multiplyView(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
       Cview(i,2,0) = Aview(i,2,0)*Bview(i,0,0) + Aview(i,2,1)*Bview(i,1,0) + Aview(i,2,2)*Bview(i,2,0);
       Cview(i,2,1) = Aview(i,2,0)*Bview(i,0,1) + Aview(i,2,1)*Bview(i,1,1) + Aview(i,2,2)*Bview(i,2,1);
       Cview(i,2,2) = Aview(i,2,0)*Bview(i,0,2) + Aview(i,2,1)*Bview(i,1,2) + Aview(i,2,2)*Bview(i,2,2);
-
     });
 
 }
 
+#if defined(RAJA_ENABLE_CUDA)
+template<typename Pol, typename T>
+void multiplyViewGPU(const T RAJA_RESTRICT Aview, const T RAJA_RESTRICT Bview,
+                     T const RAJA_RESTRICT Cview, const Index_type N)
+{
+  
+    RAJA::forall<Pol>(RAJA::RangeSegment(0, N), [=] __device__ (Index_type i) {
+
+      Cview(i,0,0) = Aview(i,0,0)*Bview(i,0,0) + Aview(i,0,1)*Bview(i,1,0) + Aview(i,0,2)*Bview(i,2,0);
+      Cview(i,0,1) = Aview(i,0,0)*Bview(i,0,1) + Aview(i,0,1)*Bview(i,1,1) + Aview(i,0,2)*Bview(i,2,1);
+      Cview(i,0,2) = Aview(i,0,0)*Bview(i,0,2) + Aview(i,0,1)*Bview(i,1,2) + Aview(i,0,2)*Bview(i,2,2);
+
+      Cview(i,1,0) = Aview(i,1,0)*Bview(i,0,0) + Aview(i,1,1)*Bview(i,1,0) + Aview(i,1,2)*Bview(i,2,0);
+      Cview(i,1,1) = Aview(i,1,0)*Bview(i,0,1) + Aview(i,1,1)*Bview(i,1,1) + Aview(i,1,2)*Bview(i,2,1);
+      Cview(i,1,2) = Aview(i,1,0)*Bview(i,0,2) + Aview(i,1,1)*Bview(i,1,2) + Aview(i,1,2)*Bview(i,2,2);
+
+      Cview(i,2,0) = Aview(i,2,0)*Bview(i,0,0) + Aview(i,2,1)*Bview(i,1,0) + Aview(i,2,2)*Bview(i,2,0);
+      Cview(i,2,1) = Aview(i,2,0)*Bview(i,0,1) + Aview(i,2,1)*Bview(i,1,1) + Aview(i,2,2)*Bview(i,2,1);
+      Cview(i,2,2) = Aview(i,2,0)*Bview(i,0,2) + Aview(i,2,1)*Bview(i,1,2) + Aview(i,2,2)*Bview(i,2,2);
+    });
+}
+#endif
+
+
 //
 // Compare output
 //
-void compareOutput(double *C, double *C2, Index_type Nelem)
+template<typename T, typename U>
+void compareOutput(T C, U Cl2, Index_type Nelem)
 {
 
   bool status = true;
   for(Index_type e = 0; e<NMAT; ++e){
     for(Index_type row=0; row<NROWS; ++row){
       for(Index_type col=0; col<NCOLS; ++col){
-        const Index_type id = col + NCOLS*(row + NROWS*e);
-        double terr = std::abs(C[id] - C2[id]);
+        double terr = std::abs(C(e,row,col) - Cl2(e,row,col));
         if((terr) > 1e-8)
           {
             status = false;
