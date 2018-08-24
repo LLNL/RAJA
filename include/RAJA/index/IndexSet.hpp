f2ba00fc80fed3feee4e4d76cdc2f025cb924e4b
Olga T. Pearce <pearce8@llnl.gov>
2018-08-24 09:33:08 -0700

    Putting back in the data delete in IndexSet

--- include/RAJA/index/IndexSet.hpp
+++ include/RAJA/index/IndexSet.hpp
@@ -139,7 +139,7 @@ public:
     for (size_t i = 0; i < num_seg; ++i) {
       // Only free segment of we allocated it
       if (owner[i]) {
-        //delete data[i];
+        delete data[i];
       }
     }
   }
