51288cb66292514cf636ce31b1504620ddebb490
Arturo Vargas <artv3.89@gmail.com>
2018-08-29 15:54:11 -0700

    fixed second lambda in tbb simd test

--- test/unit/test-simd.cpp
+++ test/unit/test-simd.cpp
@@ -104,8 +104,8 @@ TEST(SIMD, OMPAndSimd_MultiLambda){
     RAJA::KernelPolicy<
       RAJA::statement::For<1, RAJA::omp_parallel_for_exec,
         RAJA::statement::For<0, RAJA::simd_exec,
-                             RAJA::statement::Lambda<0>,
-                             RAJA::statement::Lambda<1>
+          RAJA::statement::Lambda<0>,
+          RAJA::statement::Lambda<1>
         >
       >
     >;
@@ -163,8 +163,7 @@ TEST(SIMD, TBBAndSimd){
     RAJA::KernelPolicy<
       RAJA::statement::For<1, RAJA::tbb_for_exec,
         RAJA::statement::For<0, RAJA::simd_exec,
-          RAJA::statement::Lambda<0>,
-            RAJA::statement::Lambda<1>
+          RAJA::statement::Lambda<0>
         >
       >
     >;
@@ -206,7 +205,8 @@ TEST(SIMD, TBBAndSimd_MultiLambda){
     RAJA::KernelPolicy<
       RAJA::statement::For<1, RAJA::tbb_for_exec,
         RAJA::statement::For<0, RAJA::simd_exec,
-          RAJA::statement::Lambda<0>
+          RAJA::statement::Lambda<0>,
+            RAJA::statement::Lambda<1>
         >
       >
     >;
