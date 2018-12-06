7a22e6ce3f324d1a32bfb268c10d10bafff334a5
Jason Burmark <burmark1@llnl.gov>
2018-12-06 13:37:49 -0800

    Add support for TileTCount

--- include/RAJA/pattern/kernel/Tile.hpp
+++ include/RAJA/pattern/kernel/Tile.hpp
@@ -35,6 +35,7 @@
 #include "camp/concepts.hpp"
 #include "camp/tuple.hpp"
 
+#include "RAJA/pattern/kernel/internal.hpp"
 #include "RAJA/util/macros.hpp"
 #include "RAJA/util/types.hpp"
 
@@ -53,6 +54,24 @@ template <camp::idx_t ArgumentId,
           typename ExecPolicy,
           typename... EnclosedStmts>
 struct Tile : public internal::Statement<ExecPolicy, EnclosedStmts...> {
+  using tile_policy_t = TilePolicy;
+  using exec_policy_t = ExecPolicy;
+};
+
+/*!
+ * A RAJA::kernel statement that implements a tiling (or blocking) loop.
+ * Assigns the tile index to param ParamId
+ *
+ */
+template <camp::idx_t ArgumentId,
+          typename ParamId,
+          typename TilePolicy,
+          typename ExecPolicy,
+          typename... EnclosedStmts>
+struct TileTCount : public internal::Statement<ExecPolicy, EnclosedStmts...> {
+  static_assert(std::is_base_of<internal::ParamBase, ParamId>::value,
+                "Inappropriate ParamId, ParamId must be of type "
+                "RAJA::Statement::Param< # >");
   using tile_policy_t = TilePolicy;
   using exec_policy_t = ExecPolicy;
 };
@@ -77,11 +96,11 @@ struct TileWrapper : public GenericWrapper<Data, EnclosedStmts...> {
   using Base::Base;
   using privatizer = NestedPrivatizer<TileWrapper>;
 
-  template <typename InSegmentType>
-  RAJA_INLINE void operator()(InSegmentType s)
+  template <typename InSegmentIndexType>
+  RAJA_INLINE void operator()(InSegmentIndexType si)
   {
     // Assign the tile's segment to the tuple
-    camp::get<ArgumentId>(Base::data.segment_tuple) = s;
+    camp::get<ArgumentId>(Base::data.segment_tuple) = si.s;
 
     // Assign the beginning index to the index_tuple for proper use
     // in shmem windows
@@ -93,9 +112,42 @@ struct TileWrapper : public GenericWrapper<Data, EnclosedStmts...> {
 };
 
 
+template <camp::idx_t ArgumentId, typename ParamId, typename Data,
+          typename... EnclosedStmts>
+struct TileTCountWrapper : public GenericWrapper<Data, EnclosedStmts...> {
+
+  using Base = GenericWrapper<Data, EnclosedStmts...>;
+  using Base::Base;
+  using privatizer = NestedPrivatizer<TileTCountWrapper>;
+
+  template <typename InSegmentIndexType>
+  RAJA_INLINE void operator()(InSegmentIndexType si)
+  {
+    // Assign the tile's segment to the tuple
+    camp::get<ArgumentId>(Base::data.segment_tuple) = si.s;
+
+    // Assign the beginning index to the index_tuple for proper use
+    // in shmem windows
+    camp::get<ArgumentId>(Base::data.offset_tuple) = 0;
+
+    // Assign the tile's index
+    camp::get<ParamId::param_idx>(Base::data.param_tuple) = si.i;
+
+    // Execute enclosed statements
+    Base::exec();
+  }
+};
+
+
 template <typename Iterable>
 struct IterableTiler {
   using value_type = camp::decay<Iterable>;
+
+  struct iterate
+  {
+    value_type s;
+    Index_type i;
+  };
 
   class iterator
   {
@@ -104,7 +156,7 @@ struct IterableTiler {
     const Index_type block_id;
 
   public:
-    using value_type = camp::decay<Iterable>;
+    using value_type = iterate;
     using difference_type = camp::idx_t;
     using pointer = value_type *;
     using reference = value_type &;
@@ -122,7 +174,7 @@ struct IterableTiler {
     value_type operator*()
     {
       auto start = block_id * itiler.block_size;
-      return itiler.it.slice(start, itiler.block_size);
+      return iterate{itiler.it.slice(start, itiler.block_size), block_id};
     }
 
     RAJA_HOST_DEVICE
@@ -218,7 +270,42 @@ struct StatementExecutor<
     IterableTiler<decltype(segment)> tiled_iterable(segment, chunk_size);
 
     // Wrap in case forall_impl needs to thread_privatize
-    TileWrapper<ArgumentId, Data, EnclosedStmts...> tile_wrapper(data);
+    TileWrapper<ArgumentId, Data,
+                EnclosedStmts...> tile_wrapper(data);
+
+    // Loop over tiles, executing enclosed statement list
+    forall_impl(EPol{}, tiled_iterable, tile_wrapper);
+
+    // Set range back to original values
+    camp::get<ArgumentId>(data.segment_tuple) = tiled_iterable.it;
+  }
+};
+
+template <camp::idx_t ArgumentId,
+          typename ParamId,
+          typename TPol,
+          typename EPol,
+          typename... EnclosedStmts>
+struct StatementExecutor<
+    statement::TileTCount<ArgumentId, ParamId, TPol, EPol, EnclosedStmts...>> {
+
+
+  template <typename Data>
+  static RAJA_INLINE void exec(Data &data)
+  {
+    // Get the segment we are going to tile
+    auto const &segment = camp::get<ArgumentId>(data.segment_tuple);
+
+    // Get the tiling policies chunk size
+    auto chunk_size = TPol::chunk_size;
+
+    // Create a tile iterator, needs to survive until the forall is
+    // done executing.
+    IterableTiler<decltype(segment)> tiled_iterable(segment, chunk_size);
+
+    // Wrap in case forall_impl needs to thread_privatize
+    TileTCountWrapper<ArgumentId, ParamId, Data,
+                      EnclosedStmts...> tile_wrapper(data);
 
     // Loop over tiles, executing enclosed statement list
     forall_impl(EPol{}, tiled_iterable, tile_wrapper);
