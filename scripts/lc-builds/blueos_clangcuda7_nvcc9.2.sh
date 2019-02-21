5f07c5f027d976a7c367de80ed8868d83aae31c5
Jason Burmark <burmark1@llnl.gov>
2019-02-21 14:50:32 -0800

    Example scripts for building with clang cuda
    Replace /your/path/to/llvm-7.0.0 with the path to your install of llvm/clang

---
+++ scripts/lc-builds/blueos_clangcuda7_nvcc9.2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2016-18, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-689114
+##
+## All rights reserved.
+##
+## This file is part of RAJA.
+##
+## For details about use and distribution, please read RAJA/LICENSE.
+##
+
+BUILD_SUFFIX=lc_toss3-clangcuda-7_nvcc-9.2
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.9.2
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ../host-configs/lc-builds/blueos/clangcuda_7_nvcc_9_2.cmake \
+  -DENABLE_OPENMP=Off \
+  -DENABLE_CLANG_CUDA=On \
+  -DBLT_CLANG_CUDA_ARCH=sm_60 \
+  -DENABLE_CUDA=On \
+  -DCUDA_ARCH=sm_60 \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-9.2.148 \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
