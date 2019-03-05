86dd9e66aaf768ffae344481092b5c862cbc0f9b
David Beckingsale <davidbeckingsale@gmail.com>
2019-03-05 09:08:49 -0800

    Add CMAKE_CUDA_COMPILER to script

--- scripts/lc-builds/blueos_nvcc8_gcc4.9.3.sh
+++ scripts/lc-builds/blueos_nvcc8_gcc4.9.3.sh
@@ -22,11 +22,12 @@ mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
 module load cmake/3.9.2
 
 cmake \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-8.0 \
+  -DCMAKE_CUDA_COMPILER=/usr/tce/packages/cuda/cuda-8.0/bin/nvcc \
   -DCMAKE_BUILD_TYPE=Release \
   -C ../host-configs/lc-builds/blueos/nvcc8_gcc_4_9_3.cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_CUDA=On \
-  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-8.0 \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
   ..
