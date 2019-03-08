a799f1eb98b6ec11f7aae440b8761b074b2158cf
David Poliakoff <david.poliakoff@gmail.com>
2019-03-08 13:58:23 -0800

    Made Kernel cthonic

--- jit_test/simple_test.cpp
+++ jit_test/simple_test.cpp
@@ -1,4 +1,5 @@
 #include<RAJA/RAJA.hpp>
+#include <tuple>
 
 #define MAT2D(r,c,size) r*size+c
 
@@ -9,13 +10,16 @@ using MatMulPolicy = RAJA::KernelPolicy<
       For<
         0,seq_exec,
         For<
-          1, seq_exec,
-            For<2, seq_exec,Lambda<0>>
+          1,seq_exec,
+          For<
+            2, seq_exec,
+              For<3, seq_exec,Lambda<0>>
+          >
         >
       >
     >;
 
-template<typename Policy, std::size_t... ends, typename Kernel>
+template<typename Policy, long... ends, typename Kernel>
 [[clang::jit]] void affine_jit_kernel_full(Kernel&& kernel){
   static auto rs =
     camp::make_tuple(
@@ -24,6 +28,31 @@ template<typename Policy, std::size_t... ends, typename Kernel>
   RAJA::kernel<Policy>(
       rs,
       std::forward<Kernel>(kernel)
+  );
+}
+
+template<typename Policy, typename Kernel, typename... Args>
+void affine_jit_kernel_difficult_helper2(Kernel&& kernel, Args... args){
+   affine_jit_kernel_full<Policy,*std::end(args)...>(
+       std::forward<Kernel>(kernel)
+   );
+}
+
+template<typename Policy, typename TupleLike, typename Kernel, std::size_t... indices>
+void affine_jit_kernel_difficult_helper(TupleLike IndexTuple, Kernel&& kernel, std::index_sequence<indices...>){
+  affine_jit_kernel_difficult_helper2<Policy>(
+   std::forward<Kernel>(kernel),
+   camp::get<indices>(IndexTuple)...
+  );
+    
+}
+
+template<typename Policy, typename TupleLike, typename Kernel>
+void affine_jit_kernel_difficult(TupleLike IndexTuple, Kernel&& kernel){
+  affine_jit_kernel_difficult_helper<Policy,TupleLike>(
+      std::forward<TupleLike>(IndexTuple),
+      std::forward<Kernel>(kernel),
+      std::make_index_sequence<camp::tuple_size<TupleLike>::value>()
   );
 }
 
@@ -39,17 +68,18 @@ template<std::size_t... ends, typename Kernel>
   );
 }
 
-template<std::size_t size>
+template<std::size_t n_matrices, std::size_t size>
 [[clang::jit]] void affine_jit_kernel_simplified(float* out_matrix, float* input_matrix1, float* input_matrix2){
   static auto rs = 
       camp::make_tuple(
+        RAJA::RangeSegment(0,n_matrices),
         RAJA::RangeSegment(0,size),
         RAJA::RangeSegment(0,size),
         RAJA::RangeSegment(0,size)
       );
   RAJA::kernel<MatMulPolicy>(
       rs,
-      [=](const std::size_t i,const std::size_t j,const std::size_t k){
+      [=](const std::size_t matrices, const std::size_t i,const std::size_t j,const std::size_t k){
         out_matrix[MAT2D(i,j,size)] += input_matrix1[MAT2D(i,k,size)] * input_matrix2[MAT2D(k,j,size)];
       }  
   );
@@ -73,9 +103,10 @@ void affine_kernel(Kernel&& k, Args... ends){
 
 int main(int argc, char* argv[]){
   std::size_t size = atoi(argv[1]);
+  std::size_t batch_size = atoi(argv[2]);
   srand(time(nullptr));
-  std::cout << "Size is "<<size<<"\n";
-  const long repeats = 500000000;
+  std::cout << "Size is "<<size<<", batch size "<< batch_size<<"\n";
+  const std::size_t repeats = 5000000000;
   float* out_matrix = (float*)malloc(sizeof(float)*size*size);
   float* input_matrix1 = (float*)malloc(sizeof(float)*size*size);
   float* input_matrix2 = (float*)malloc(sizeof(float)*size*size);
@@ -83,29 +114,38 @@ int main(int argc, char* argv[]){
     input_matrix1[r] = (1.0f*rand())/RAND_MAX;
     input_matrix2[r] = (1.0f*rand())/RAND_MAX;
   } 
-  for(long rep = 0; rep<repeats;rep++){
+  for(long rep = 0; rep<(repeats/batch_size);rep++){
 
 #ifdef USE_JIT
-    affine_jit_kernel_simplified<
-       size
-     >(
-        out_matrix, input_matrix1, input_matrix2
-      );
+    affine_jit_kernel_difficult<MatMulPolicy>(
+        camp::make_tuple(
+          RAJA::RangeSegment(0,batch_size),
+          RAJA::RangeSegment(0,size),
+          RAJA::RangeSegment(0,size),
+          RAJA::RangeSegment(0,size)
+        ),
+        [=](const std::size_t matrices, const std::size_t i, const std::size_t j, const std::size_t k){
+            out_matrix[MAT2D(i,j,size)] += input_matrix1[MAT2D(i,k,size)] * input_matrix2[MAT2D(k,j,size)];
+        }
+    );
 #endif 
     
 #ifdef NO_JIT
     affine_kernel<RAJA::KernelPolicy<
       For<
         0,seq_exec,
+      For<
+        1,seq_exec,
         For<
-          1, seq_exec,
-            For<2, seq_exec,Lambda<0>>
+          2, seq_exec,
+            For<3, seq_exec,Lambda<0>>
         >
       >
-    >>(
-        [=](const std::size_t i, const std::size_t j, const std::size_t k){
+    >>>(
+        [=](const std::size_t matrices, const std::size_t i, const std::size_t j, const std::size_t k){
           out_matrix[MAT2D(i,j,size)] += input_matrix1[MAT2D(i,k,size)] * input_matrix2[MAT2D(k,j,size)];
         },
+        batch_size,
         size,
         size,
         size
