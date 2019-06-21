e749f9ea23e2c9ef6bebcb92baa80ee2bc35459b
Robert Chang Che Chen <chen59@rzansel61.coral.llnl.gov>
2019-06-21 16:01:04 -0700

    Fixes for nvcc8-gcc4.9. However, original and new CUDA reduce loc tests fail.

--- host-configs/lc-builds/blueos/nvcc_gcc_4_9_3.cmake
+++ host-configs/lc-builds/blueos/nvcc_gcc_4_9_3.cmake
@@ -15,9 +15,9 @@ set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
 
 set(HOST_OPT_FLAGS "-Xcompiler -O3 -Xcompiler -finline-functions -Xcompiler -fopenmp")
 
-set(CMAKE_CUDA_FLAGS_RELEASE "-O3 ${HOST_OPT_FLAGS}" CACHE STRING "")
-set(CMAKE_CUDA_FLAGS_DEBUG "-g -G -O0" CACHE STRING "")
-set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo -O3 ${HOST_OPT_FLAGS}" CACHE STRING "")
+set(CMAKE_CUDA_FLAGS_RELEASE "-O3 --expt-relaxed-constexpr ${HOST_OPT_FLAGS}" CACHE STRING "")
+set(CMAKE_CUDA_FLAGS_DEBUG "-g -G -O0 --expt-relaxed-constexpr" CACHE STRING "")
+set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-g -lineinfo -O3 --expt-relaxed-constexpr ${HOST_OPT_FLAGS}" CACHE STRING "")
 
 set(RAJA_RANGE_ALIGN 4 CACHE INT "")
 set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
