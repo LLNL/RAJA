e560180f30a89581eef5f05d4d60518f35af26e4
mdavis36 <davis291@llnl.gov>
2019-07-08 09:26:05 -0700

    Adding ListSegmment tests

---
+++ test/unit/index/test-listsegment.cpp
@@ -0,0 +1,111 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC.
+//
+// Produced at the Lawrence Livermore National Laboratory
+//
+// LLNL-CODE-689114
+//
+// All rights reserved.
+//
+// This file is part of RAJA.
+//
+// For details about use and distribution, please read RAJA/LICENSE.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "RAJA/RAJA.hpp"
+#include "gtest/gtest.h"
+#include <vector>
+
+template<typename T>
+class ListSegmentTest : public ::testing::Test {};
+
+using MyTypes = ::testing::Types<RAJA::Index_type,
+                                 char,
+				 unsigned char,
+				 short,
+				 unsigned short,
+				 int,
+				 unsigned int,
+				 long,
+				 unsigned long,
+				 long int,
+				 unsigned long int,
+				 unsigned long long,
+				 double,
+				 float>;
+
+TYPED_TEST_CASE(ListSegmentTest, MyTypes);
+
+
+TYPED_TEST(ListSegmentTest, Constructors)
+{
+  std::vector<TypeParam> idx;
+  for (TypeParam i = 0; i < 5; ++i){
+    idx.push_back(i);
+  }
+
+  RAJA::TypedListSegment<TypeParam> list1( &idx[0], idx.size());
+  RAJA::TypedListSegment<TypeParam> copied(list1);
+
+  ASSERT_EQ(list1, copied);
+
+  RAJA::TypedListSegment<TypeParam> moved(std::move(list1));
+
+  ASSERT_EQ(moved, copied);
+
+  RAJA::TypedListSegment<TypeParam> container(idx);
+
+  ASSERT_EQ(list1, container); 
+}
+
+TYPED_TEST(ListSegmentTest, Swaps)
+{
+  std::vector<TypeParam> idx1;
+  std::vector<TypeParam> idx2;
+  for (TypeParam i = 0; i < 5; ++i){
+    idx1.push_back(i);
+    idx2.push_back(i+5);
+  }
+
+  RAJA::TypedListSegment<TypeParam> list1( idx1 );
+  RAJA::TypedListSegment<TypeParam> list2( idx2 );
+  auto list3 = RAJA::TypedListSegment<TypeParam>(list1);
+  auto list4 = RAJA::TypedListSegment<TypeParam>(list2);
+
+  list1.swap(list2);
+
+  ASSERT_EQ(list2, list3);
+  ASSERT_EQ(list1, list4);
+
+  std::swap(list1, list2);
+
+  ASSERT_EQ(list1, list3);
+  ASSERT_EQ(list2, list4);
+}
+
+TYPED_TEST(ListSegmentTest, Equality)
+{
+  std::vector<TypeParam> idx1{5,3,1,2};
+  RAJA::TypedListSegment<TypeParam> list( idx1 );
+
+  std::vector<TypeParam> idx2{2,1,3,5};
+  
+  ASSERT_EQ(list.indicesEqual( &idx2.begin()[0], idx2.size() ), false);
+
+  std::reverse( idx2.begin(), idx2.end() );
+
+  ASSERT_EQ(list.indicesEqual( &idx2.begin()[0], idx2.size() ), true);
+}
+
+TYPED_TEST(ListSegmentTest, Iterators)
+{
+  std::vector<TypeParam> idx1{5,3,1,2};
+  RAJA::TypedListSegment<TypeParam> list( idx1 );
+
+  ASSERT_EQ(5, *list.begin());
+  ASSERT_EQ(2, *(list.end()-1));
+
+  ASSERT_EQ(4, list.size());
+}
+
