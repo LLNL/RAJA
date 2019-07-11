730f88a6d14bafdc33c846cb8855556d1e264412
Robert Chang Che Chen <chen59@rzansel61.coral.llnl.gov>
2019-07-11 16:09:22 -0700

    Tests for tuple-ized min/maxloc. CPU maxloc tests added.

--- test/unit/cpu/test-reductions.cpp
+++ test/unit/cpu/test-reductions.cpp
@@ -43,6 +43,10 @@ TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor)
   RAJA::ReduceMinLoc<ReducePolicy, NumericType> reduce_minloc(0.0, 1);
   RAJA::ReduceMaxLoc<ReducePolicy, NumericType> reduce_maxloc(0.0, 1);
 
+  RAJA::tuple<RAJA::Index_type, RAJA::Index_type> LocTup(1, 1);
+  RAJA::ReduceMinLoc<ReducePolicy, NumericType, RAJA::tuple<RAJA::Index_type, RAJA::Index_type>> reduce_minloctup(0.0, LocTup);
+  RAJA::ReduceMaxLoc<ReducePolicy, NumericType, RAJA::tuple<RAJA::Index_type, RAJA::Index_type>> reduce_maxloctup(0.0, LocTup);
+
   ASSERT_EQ((NumericType)reduce_sum.get(), (NumericType)(0.0));
   ASSERT_EQ((NumericType)reduce_min.get(), (NumericType)(0.0));
   ASSERT_EQ((NumericType)reduce_max.get(), (NumericType)(0.0));
@@ -50,6 +54,13 @@ TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor)
   ASSERT_EQ((NumericType)reduce_minloc.get(), (NumericType)(0.0));
   ASSERT_EQ((NumericType)reduce_maxloc.get(), (NumericType)(0.0));
   ASSERT_EQ((RAJA::Index_type)reduce_maxloc.getLoc(), (RAJA::Index_type)1);
+
+  ASSERT_EQ((NumericType)reduce_minloctup.get(), (NumericType)(0.0));
+  ASSERT_EQ((NumericType)reduce_maxloctup.get(), (NumericType)(0.0));
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<0>(reduce_minloctup.getLoc())), (RAJA::Index_type)1);
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<1>(reduce_minloctup.getLoc())), (RAJA::Index_type)1);
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<0>(reduce_maxloctup.getLoc())), (RAJA::Index_type)1);
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<1>(reduce_maxloctup.getLoc())), (RAJA::Index_type)1);
 }
 
 TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor2)
@@ -63,6 +74,9 @@ TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor2)
   RAJA::ReduceMinLoc<ReducePolicy, NumericType> reduce_minloc;
   RAJA::ReduceMaxLoc<ReducePolicy, NumericType> reduce_maxloc;
 
+  RAJA::ReduceMinLoc<ReducePolicy, NumericType, RAJA::tuple<RAJA::Index_type, RAJA::Index_type>> reduce_minloctup;
+  RAJA::ReduceMaxLoc<ReducePolicy, NumericType, RAJA::tuple<RAJA::Index_type, RAJA::Index_type>> reduce_maxloctup;
+
   ASSERT_EQ((NumericType)reduce_sum.get(), NumericType());
   ASSERT_EQ((NumericType)reduce_min.get(), NumericType());
   ASSERT_EQ((NumericType)reduce_max.get(), NumericType());
@@ -70,6 +84,13 @@ TYPED_TEST_P(ReductionConstructorTest, ReductionConstructor2)
   ASSERT_EQ((NumericType)reduce_minloc.get(), NumericType());
   ASSERT_EQ((NumericType)reduce_maxloc.get(), NumericType());
   ASSERT_EQ((RAJA::Index_type)reduce_maxloc.getLoc(), RAJA::Index_type());
+
+  ASSERT_EQ((NumericType)reduce_minloctup.get(), NumericType());
+  ASSERT_EQ((NumericType)reduce_maxloctup.get(), NumericType());
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<0>(reduce_minloctup.getLoc())), RAJA::Index_type());
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<1>(reduce_minloctup.getLoc())), RAJA::Index_type());
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<0>(reduce_maxloctup.getLoc())), RAJA::Index_type());
+  ASSERT_EQ((RAJA::Index_type)(RAJA::get<1>(reduce_maxloctup.getLoc())), RAJA::Index_type());
 }
 
 REGISTER_TYPED_TEST_CASE_P(ReductionConstructorTest,
@@ -537,6 +558,154 @@ TYPED_TEST_P(ReductionGenericLocTest, ReduceMinLoc2DIndexTupleViewKernel)
   ASSERT_EQ(this->minlocy, RAJA::get<1>(raja_loc));
 }
 
+TYPED_TEST_P(ReductionGenericLocTest, ReduceMaxLoc2DIndex)
+{
+  using ExecPolicy = typename std::tuple_element<0, TypeParam>::type;
+  using ReducePolicy = typename std::tuple_element<1, TypeParam>::type;
+
+  struct Index2D {
+    RAJA::Index_type idx, idy;
+    RAJA::Index_type idarray;  // actual array index
+    constexpr Index2D() : idx(-1), idy(-1), idarray(-1) {}
+
+    // 2 dimensional array, 10 elements per row
+    Index2D(RAJA::Index_type idx, RAJA::Index_type idy) : idx(idx), idy(idy)
+    {
+      idarray = idx % 10 + idy * 10;
+    }
+    Index2D(RAJA::Index_type idarray) : idarray(idarray)
+    {
+      idx = idarray % 10;
+      idy = floor( idarray / 10 );
+    }
+  };
+
+  RAJA::ReduceMaxLoc<ReducePolicy, double, Index2D> maxloc_reducer(-1024.0, Index2D(0, 0));
+
+  RAJA::forall<ExecPolicy>(RAJA::RangeSegment(0, this->array_length),
+                           [=](int i) {
+                             maxloc_reducer.maxloc(this->data[i], Index2D(i));
+                           });
+
+  Index2D raja_loc = maxloc_reducer.getLoc();
+  double raja_max = (double)maxloc_reducer.get();
+
+  ASSERT_FLOAT_EQ(this->max, raja_max);
+  ASSERT_EQ(this->maxlocx, raja_loc.idx);
+  ASSERT_EQ(this->maxlocy, raja_loc.idy);
+}
+
+TYPED_TEST_P(ReductionGenericLocTest, ReduceMaxLoc2DIndexKernel)
+{
+  using ExecPolicy =
+    RAJA::KernelPolicy<
+      RAJA::statement::For<1, RAJA::loop_exec,  // row
+        RAJA::statement::For<0, RAJA::loop_exec,  // col
+          RAJA::statement::Lambda<0>
+        >
+      >
+    >;
+
+  using ReducePolicy = RAJA::seq_reduce;
+
+  RAJA::RangeSegment colrange(0, 10);
+  RAJA::RangeSegment rowrange(0, 10);
+
+  struct Index2D {
+     RAJA::Index_type idx, idy;
+     constexpr Index2D() : idx(-1), idy(-1) {}
+     constexpr Index2D(RAJA::Index_type idx, RAJA::Index_type idy) : idx(idx), idy(idy) {}
+  };
+
+  RAJA::ReduceMaxLoc<ReducePolicy, double, Index2D> maxloc_reducer(-1024.0, Index2D(0, 0));
+
+  RAJA::kernel<ExecPolicy>(RAJA::make_tuple(colrange, rowrange),
+                           [=](int c, int r) {
+                             maxloc_reducer.maxloc(this->array[r][c], Index2D(c, r));
+                           });
+
+  Index2D raja_loc = maxloc_reducer.getLoc();
+  double raja_max = (double)maxloc_reducer.get();
+
+  ASSERT_FLOAT_EQ(this->max, raja_max);
+  ASSERT_EQ(this->maxlocx, raja_loc.idx);
+  ASSERT_EQ(this->maxlocy, raja_loc.idy);
+}
+
+TYPED_TEST_P(ReductionGenericLocTest, ReduceMaxLoc2DIndexViewKernel)
+{
+  using ExecPolicy =
+    RAJA::KernelPolicy<
+      RAJA::statement::For<1, RAJA::loop_exec,  // row
+        RAJA::statement::For<0, RAJA::loop_exec,  // col
+          RAJA::statement::Lambda<0>
+        >
+      >
+    >;
+
+  using ReducePolicy = RAJA::seq_reduce;
+
+  RAJA::RangeSegment colrange(0, 10);
+  RAJA::RangeSegment rowrange(0, 10);
+
+  RAJA::View<double, RAJA::Layout<2>> ArrView(this->data, 10, 10);
+
+  struct Index2D {
+     RAJA::Index_type idx, idy;
+     constexpr Index2D() : idx(-1), idy(-1) {}
+     constexpr Index2D(RAJA::Index_type idx, RAJA::Index_type idy) : idx(idx), idy(idy) {}
+  };
+
+  RAJA::ReduceMaxLoc<ReducePolicy, double, Index2D> maxloc_reducer(-1024.0, Index2D(0, 0));
+
+  RAJA::kernel<ExecPolicy>(RAJA::make_tuple(colrange, rowrange),
+                           [=](int c, int r) {
+                             maxloc_reducer.maxloc(ArrView(r, c), Index2D(c, r));
+                           });
+
+  Index2D raja_loc = maxloc_reducer.getLoc();
+  double raja_max = (double)maxloc_reducer.get();
+
+  ASSERT_FLOAT_EQ(this->max, raja_max);
+  ASSERT_EQ(this->maxlocx, raja_loc.idx);
+  ASSERT_EQ(this->maxlocy, raja_loc.idy);
+}
+
+TYPED_TEST_P(ReductionGenericLocTest, ReduceMaxLoc2DIndexTupleViewKernel)
+{
+  using ExecPolicy =
+    RAJA::KernelPolicy<
+      RAJA::statement::For<1, RAJA::loop_exec,  // row
+        RAJA::statement::For<0, RAJA::loop_exec,  // col
+          RAJA::statement::Lambda<0>
+        >
+      >
+    >;
+
+  using ReducePolicy = RAJA::seq_reduce;
+
+  RAJA::RangeSegment colrange(0, 10);
+  RAJA::RangeSegment rowrange(0, 10);
+
+  RAJA::View<double, RAJA::Layout<2>> ArrView(this->data, 10, 10);
+
+  RAJA::tuple<int, int> LocTup(0, 0);
+
+  RAJA::ReduceMaxLoc<ReducePolicy, double, RAJA::tuple<int, int>> maxloc_reducer(-1024.0, LocTup);
+
+  RAJA::kernel<ExecPolicy>(RAJA::make_tuple(colrange, rowrange),
+                           [=](int c, int r) {
+                             maxloc_reducer.maxloc(ArrView(r, c), RAJA::make_tuple(c, r));
+                           });
+
+  RAJA::tuple<int, int> raja_loc = maxloc_reducer.getLoc();
+  double raja_max = (double)maxloc_reducer.get();
+
+  ASSERT_FLOAT_EQ(this->max, raja_max);
+  ASSERT_EQ(this->maxlocx, RAJA::get<0>(raja_loc));
+  ASSERT_EQ(this->maxlocy, RAJA::get<1>(raja_loc));
+}
+
 TYPED_TEST_P(ReductionCorrectnessTest, ReduceMinLoc2)
 {
   using ExecPolicy =
@@ -708,7 +877,11 @@ REGISTER_TYPED_TEST_CASE_P(ReductionGenericLocTest,
                            ReduceMinLoc2DIndex,
                            ReduceMinLoc2DIndexKernel,
                            ReduceMinLoc2DIndexViewKernel,
-                           ReduceMinLoc2DIndexTupleViewKernel);
+                           ReduceMinLoc2DIndexTupleViewKernel,
+                           ReduceMaxLoc2DIndex,
+                           ReduceMaxLoc2DIndexKernel,
+                           ReduceMaxLoc2DIndexViewKernel,
+                           ReduceMaxLoc2DIndexTupleViewKernel);
 
 using types = ::testing::Types<
     std::tuple<RAJA::seq_exec, RAJA::seq_reduce>,
