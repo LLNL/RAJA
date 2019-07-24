c3f9947ca66c9058db94a7f26c4972348c95fc39
Rich Hornung <hornung1@llnl.gov>
2019-07-24 14:48:09 -0700

    Update/cleanup build files for toss3 intel compilers.

--- host-configs/lc-builds/toss3/icpc_X_gcc8headers.cmake
+++ host-configs/lc-builds/toss3/icpc_X_gcc8headers.cmake
@@ -7,9 +7,6 @@
 
 set(RAJA_COMPILER "RAJA_COMPILER_ICC" CACHE STRING "")
 
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/intel/intel-19.0.3/bin/icpc" CACHE PATH "")
-set(CMAKE_C_COMPILER "/usr/tce/packages/intel/intel-19.0.3/bin/icc" CACHE PATH "")
-
 set(COMMON_FLAGS "-gxx-name=/usr/tce/packages/gcc/gcc-8.1.0/bin/g++")
 set(CMAKE_CXX_FLAGS_RELEASE "${COMMON_FLAGS} -O3 -march=native -ansi-alias -axCORE-AVX2 -diag-disable cpu-dispatch" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${COMMON_FLAGS} -O3 -g -march=native -ansi-alias -axCORE-AVX2 -diag-disable cpu-dispatch" CACHE STRING "")
--- scripts/lc-builds/toss3_icpc19.0.4.sh
+++ scripts/lc-builds/toss3_icpc19.0.4.sh
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-BUILD_SUFFIX=lc_toss3-icpc-19.0.3
+BUILD_SUFFIX=lc_toss3-icpc-19.0.4
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
@@ -16,7 +16,8 @@ module load cmake/3.9.2
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -C ../host-configs/lc-builds/toss3/icpc_19_0_3.cmake \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/intel/intel-19.0.4/bin/icpc \
+  -C ../host-configs/lc-builds/toss3/icpc_X_gcc8headers.cmake \
   -DENABLE_OPENMP=On \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
