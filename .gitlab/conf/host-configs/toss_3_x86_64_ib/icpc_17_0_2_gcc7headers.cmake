ee55a41497e5954e5ff217648c85a3f57b61fdfa
Adrien M. Bernede <bernede1@llnl.gov>
2019-09-05 09:38:49 -0700

    Moving gcc headers definition to toolchain
    I Think gcc headers should be defined in the toolchain configuration
    files.

--- .gitlab/conf/host-configs/toss_3_x86_64_ib/icpc_17_0_2_gcc7headers.cmake
+++ .gitlab/conf/host-configs/toss_3_x86_64_ib/icpc_17_0_2_gcc7headers.cmake
@@ -5,5 +5,7 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/intel/intel-19.0.4/bin/icpc" CACHE PATH "")
-set(CMAKE_C_COMPILER "/usr/tce/packages/intel/intel-19.0.4/bin/icc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/intel/intel-17.0.2/bin/icpc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/intel/intel-17.0.2/bin/icc" CACHE PATH "")
+
+set(COMMON_FLAGS "-gxx-name=/usr/tce/packages/gcc/gcc-7.1.0/bin/g++")
