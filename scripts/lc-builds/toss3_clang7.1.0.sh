d274701d627fd1cdaaa8843c1737784bfb201859
Rich Hornung <hornung1@llnl.gov>
2019-10-11 14:38:30 -0700

    Updated build files for toss3.

--- scripts/lc-builds/toss3_clang7.1.0.sh
+++ scripts/lc-builds/toss3_clang7.1.0.sh
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-BUILD_SUFFIX=lc_toss3-clang-4.0.0
+BUILD_SUFFIX=lc_toss3-clang-7.1.0
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
@@ -16,7 +16,7 @@ module load cmake/3.14.5
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_COMPILER=/usr/tce/packages/clang/clang-4.0.0/bin/clang++ \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/clang/clang-7.1.0/bin/clang++ \
   -C ../host-configs/lc-builds/toss3/clang_X.cmake \
   -DENABLE_OPENMP=On \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
