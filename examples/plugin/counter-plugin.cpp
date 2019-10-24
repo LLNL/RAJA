25f3263837b89667c025b3e8805b25bce993f9c8
Rich Hornung <hornung1@llnl.gov>
2019-10-24 09:30:22 -0700

    Fix another travis build error.

--- examples/plugin/counter-plugin.cpp
+++ examples/plugin/counter-plugin.cpp
@@ -21,16 +21,14 @@ class CounterPlugin :
   public RAJA::util::PluginStrategy
 {
   public:
-  void 
-  preLaunch(RAJA::util::PluginContext p) {
+  void preLaunch(RAJA::util::PluginContext p) {
     if (p.platform == RAJA::Platform::host)
       std::cout << " [CounterPlugin]: Launching host kernel for the " << ++host_counter << " time!" << std::endl;
     else
       std::cout << " [CounterPlugin]: Launching device kernel for the " << ++device_counter << " time!" << std::endl;
   }
 
-  void 
-  postLaunch(RAJA::util::PluginContext p) {
+  void postLaunch(RAJA::util::PluginContext RAJA_UNUSED_ARG(p)) {
   }
 
   private:
