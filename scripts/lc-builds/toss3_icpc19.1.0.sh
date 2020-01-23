98ff09624c400fd87c92ccf491906fe92530fc56
Rich Hornung <hornung1@llnl.gov>
2020-01-23 09:48:24 -0800

    Update version of Intel 19 compiler.

--- scripts/lc-builds/toss3_icpc19.1.0.sh
+++ scripts/lc-builds/toss3_icpc19.1.0.sh
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-BUILD_SUFFIX=lc_toss3-icpc-19.0.4
+BUILD_SUFFIX=lc_toss3-icpc-19.1.0
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
@@ -16,7 +16,7 @@ module load cmake/3.14.5
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_COMPILER=/usr/tce/packages/intel/intel-19.0.4/bin/icpc \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/intel/intel-19.1.0/bin/icpc \
   -C ../host-configs/lc-builds/toss3/icpc_X_gcc8headers.cmake \
   -DENABLE_OPENMP=On \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
