b20f173e00b857f862d2b6d2d1525360d8b70809
Rich Hornung <hornung1@llnl.gov>
2020-03-13 15:37:11 -0700

    Initial cut at reworked scan tests

---
+++ test/functional/scan/test-scan-exclusive-tbb.cpp
@@ -0,0 +1,23 @@
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "test-scan-inclusive.hpp"
+
+#if defined(RAJA_ENABLE_TBB)
+
+// TBB policy types to test
+using TBBExecTypes = list< RAJA::tbb_for_exec >;
+
+using TBBInclusiveScanTypes = 
+  Test<cartesian_product< TBBExecTypes, ListHostRes, OpTypes >>::Types;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(TBB, 
+                               ScanFunctionalTest, 
+                               TBBExclusiveScanTypes);
+
+#endif
