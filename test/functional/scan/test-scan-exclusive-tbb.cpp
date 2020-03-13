412e621f5923db136782829fd52afaceb0da5433
Rich Hornung <hornung1@llnl.gov>
2020-03-13 16:12:35 -0700

    Fix header file inclusion.

--- test/functional/scan/test-scan-exclusive-tbb.cpp
+++ test/functional/scan/test-scan-exclusive-tbb.cpp
@@ -6,14 +6,14 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-#include "test-scan-inclusive.hpp"
+#include "test-scan-exclusive.hpp"
 
 #if defined(RAJA_ENABLE_TBB)
 
 // TBB policy types to test
 using TBBExecTypes = list< RAJA::tbb_for_exec >;
 
-using TBBInclusiveScanTypes = 
+using TBBExclusiveScanTypes = 
   Test<cartesian_product< TBBExecTypes, ListHostRes, OpTypes >>::Types;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(TBB, 
