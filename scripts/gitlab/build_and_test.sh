dd21cc071616b1f47a37de3609c9d5dc43268eb5
Adrien M. Bernede <bernede1@llnl.gov>
2020-03-16 09:22:15 -0700

    Raja CI: naming / comments / newer cmake / defaults values

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -7,25 +7,50 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
+
 set -o errexit
 set -o nounset
 
-echo "--- Configuration to match :"
-echo "* ${CONFIGURATION}"
+sys_type=${SYS_TYPE:-""}
+if [[ -z ${sys_type} ]]
+then
+    sys_type=${OSTYPE:-""}
+    if [[ -z ${sys_type} ]]
+    then
+        echo "System type not found (both SYS_TYPE and OSTYPE are undefined)"
+        exit 1
+    fi
+fi
 
-# 'configuration' = toolchain__tuning
+build_root=${BUILD_ROOT:-""}
+if [[ -z ${build_root} ]]
+then
+    build_root=$(pwd)
+fi
+
+conf=${CONFIGURATION:-""}
+if [[ -z ${conf} ]]
+then
+    echo "CONFIGURATION is undefined... canceling"
+    exit 1
+fi
+
+echo "--- Configuration to match :"
+echo "* ${conf}"
+
+# 'conf' = toolchain__tuning
 # 'host_config' = filter__tuning
 #   where 'filter' can represent several toolchains
 #   like <nvcc_10_gcc_X> covers any gcc paired with nvcc10
 # 'toolchain' is a unique set of tools, and 'tuning' allows to have
 # several configurations for this set, like <omptarget>.
 
-toolchain=${CONFIGURATION/__*/}
-tuning=${CONFIGURATION/${toolchain}/}
+toolchain=${conf/__*/}
+tuning=${conf/${toolchain}/}
 
 # FIRST STEP :
 # Find raja host_configs matching the configuration
-host_configs="$(ls host-configs/${SYS_TYPE}/ | grep "\.cmake$")"
+host_configs="$(ls host-configs/${sys_type}/ | grep "\.cmake$")"
 echo "--- Available host_configs"
 echo "${host_configs}"
 
@@ -45,7 +70,7 @@ do
         continue
     fi
 
-    if [[ "${CONFIGURATION}" =~ ^${pattern}$ ]]
+    if [[ "${conf}" =~ ^${pattern}$ ]]
     then
         (( ++match_count ))
         host_config="${hc}"
@@ -61,25 +86,27 @@ fi
 
 # SECOND STEP :
 # Build
-build_suffix="${SYS_TYPE}_${CONFIGURATION}"
-build_dir="build_${build_suffix}"
+
+project_dir="$(pwd)"
+build_dir="${build_root}/build_${sys_type}_${conf}"
+install_dir="${build_root}/install_${sys_type}_${conf}"
+
 echo "--- Build (${build_dir})"
 
 rm -rf ${build_dir} 2>/dev/null
-mkdir ${build_dir} && cd ${build_dir}
+mkdir -p ${build_dir} && cd ${build_dir}
 
-install_dir="../install_${build_suffix}"
-compiler_conf="../.gitlab/conf/host-configs/${SYS_TYPE}/${toolchain}.cmake"
-raja_conf="../host-configs/${SYS_TYPE}/${host_config}"
+generic_conf="${project_dir}/.gitlab/conf/host-configs/${sys_type}/${toolchain}.cmake"
+raja_conf="${project_dir}/host-configs/${sys_type}/${host_config}"
 
-module load cmake/3.9.2
+module load cmake/3.14.5
 
 cmake \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} \
-  -C ${compiler_conf} \
+  -C ${generic_conf} \
   -C ${raja_conf} \
   -DENABLE_OPENMP=${ENABLE_OPENMP:-On} \
   -DCMAKE_INSTALL_PREFIX=${install_dir} \
-  ..
+  ${project_dir}
 make -j 8
 ctest -V -T test
