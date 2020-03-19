4f7ad00de489d1eb51c75e4733b79888733956de
Jason Burmark <burmark1@llnl.gov>
2020-03-19 14:21:19 -0700

    Implement tbb stable_sort with templated SortTasks

--- include/RAJA/policy/tbb/sort.hpp
+++ include/RAJA/policy/tbb/sort.hpp
@@ -44,8 +44,8 @@ namespace sort
 namespace detail
 {
 
-  template < typename Iter, typename Compare >
-  struct TbbStableSortTask : tbb::task
+  template < typename Sorter, typename Iter, typename Compare >
+  struct TbbSortTask : tbb::task
   {
     using difference_type =
         camp::decay<decltype(camp::val<Iter>() - camp::val<Iter>())>;
@@ -53,12 +53,14 @@ namespace detail
     // TODO: make this less arbitrary
     static const difference_type cutoff = 256;
 
+    Sorter sorter;
     const Iter begin;
     const Iter end;
     Compare comp;
 
-    TbbStableSortTask(Iter begin_, Iter end_, Compare comp_)
-      : begin(begin_)
+    TbbSortTask(Sorter sorter_, Iter begin_, Iter end_, Compare comp_)
+      : sorter(sorter_)
+      , begin(begin_)
       , end(end_)
       , comp(comp_)
     { }
@@ -70,21 +72,21 @@ namespace detail
       if (len <= cutoff) {
 
         // leaves sort their range
-        stable(::RAJA::loop_exec{}, begin, end, comp);
+        sorter(::RAJA::loop_exec{}, begin, end, comp);
 
       } else {
 
         Iter middle = begin + (len/2);
 
         // branching nodes break the sorting up recursively
-        TbbStableSortTask& stable_sort_tank_front =
-            *new( allocate_child() ) TbbStableSortTask(begin, middle, comp);
-        TbbStableSortTask& stable_sort_tank_back =
-            *new( allocate_child() ) TbbStableSortTask(middle, end, comp);
+        TbbSortTask& sort_tank_front =
+            *new( allocate_child() ) TbbSortTask(sorter, begin, middle, comp);
+        TbbSortTask& sort_tank_back =
+            *new( allocate_child() ) TbbSortTask(sorter, middle, end, comp);
 
         set_ref_count(3);
-        spawn(stable_sort_tank_back);
-        spawn_and_wait_for_all(stable_sort_tank_front);
+        spawn(sort_tank_back);
+        spawn_and_wait_for_all(sort_tank_front);
 
         // and merge the results
         std::inplace_merge(begin, middle, end, comp);
@@ -119,10 +121,11 @@ stable(const ExecPolicy&,
        Iter end,
        Compare comp)
 {
-  detail::TbbStableSortTask<Iter, Compare>& stable_sort_task =
-      *new(tbb::task::allocate_root())
-        detail::TbbStableSortTask<Iter, Compare>(begin, end, comp);
-  tbb::task::spawn_root_and_wait(stable_sort_task);
+  using Sorter = StableSorter;
+  using SortTask = detail::TbbSortTask<Sorter, Iter, Compare>;
+  SortTask& sort_task =
+      *new(tbb::task::allocate_root()) SortTask(Sorter{}, begin, end, comp);
+  tbb::task::spawn_root_and_wait(sort_task);
 }
 
 /*!
