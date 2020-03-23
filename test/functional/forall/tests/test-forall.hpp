b3e90f0e9932f036aa13c1a2fce555b6f57e23de
mdavis36 <davis291@llnl.gov>
2020-03-23 13:11:22 -0700

    Removing comments

--- test/functional/forall/tests/test-forall.hpp
+++ test/functional/forall/tests/test-forall.hpp
@@ -21,14 +21,4 @@ class ForallFunctionalSegmentTest : public ::testing::Test
 REGISTER_TYPED_TEST_SUITE_P(ForallFunctionalSegmentTest,
                             RangeSegmentForall);
 
-
-//TYPED_TEST_SUITE_P(ForallFunctionalReductionTest);
-//template <typename T>
-//class ForallFunctionalReductionTest : public ::testing::Test
-//{
-//};
-//#include "test-forall-reduction.hpp"
-//REGISTER_TYPED_TEST_SUITE_P(ForallFunctionalReductionTest,
-//                            ReductionForall);
-
 #endif  // __TEST_FORALL_HPP__
