b8c8231d6ea002c7a2259163af3299ae98206ce6
Robert Chen <chen59@llnl.gov>
2020-04-21 08:41:38 -0700

    Removed unnecessary macro file, and corrected dates.

--- test/functional/forall/atomic-ref/test-forall-atomic-ref-other-seq.cpp
+++ test/functional/forall/atomic-ref/test-forall-atomic-ref-other-seq.cpp
@@ -1,5 +1,5 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC.
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC.
 //
 // Produced at the Lawrence Livermore National Laboratory
 //
--- test/functional/forall/atomic-ref/tests/test-forall-atomic-ref-other.hpp
+++ test/functional/forall/atomic-ref/tests/test-forall-atomic-ref-other.hpp
@@ -1,6 +1,6 @@
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC.
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC.
 //
 // Produced at the Lawrence Livermore National Laboratory
 //
