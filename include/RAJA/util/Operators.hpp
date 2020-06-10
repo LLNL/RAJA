b3f795adb5cceeb00f7cc50dd69131fceef0e038
Kae Suarez <suarez29@llnl.gov>
2020-06-10 12:35:23 -0700

    Adding the necessary identity value, which is 0 for bitwise OR.

--- include/RAJA/util/Operators.hpp
+++ include/RAJA/util/Operators.hpp
@@ -416,6 +416,8 @@ struct bit_or : public detail::binary_function<Arg1, Arg2, Ret> {
   {
     return lhs | rhs;
   }
+
+RAJA_HOST_DEVICE static constexpr Ret identity() { return Ret{0}; }
 };
 
 template <typename Ret, typename Arg1 = Ret, typename Arg2 = Arg1>
