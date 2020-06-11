9c080c24179454e7aab8220da7f7f8b83df017ee
Kae Suarez <suarez29@llnl.gov>
2020-06-11 09:11:46 -0700

    Adding test for sequential bitwise OR reduction

--- test/functional/forall/reduce-sanity/tests/test-forall-reduce-sanity.hpp
+++ test/functional/forall/reduce-sanity/tests/test-forall-reduce-sanity.hpp
@@ -18,6 +18,11 @@ class ForallReduceSanityTest : public ::testing::Test
 {
 };
 
+TYPED_TEST_SUITE_P(ForallReduceSanityTestBit);
+template <typename T>
+class ForallReduceSanityTestBit : public ::testing::Test
+{
+};
 
 //
 // Data types for reduction sanity tests
@@ -26,7 +31,10 @@ using ReduceSanityDataTypeList = camp::list<int,
                                             float,
                                             double>;
 
+using ReduceSanityDataTypeListBit = camp::list<int>;
+
 #include "test-forall-reduce-sanity-sum.hpp"
+#include "test-forall-reduce-sanity-bitor.hpp"
 #include "test-forall-reduce-sanity-min.hpp"
 #include "test-forall-reduce-sanity-max.hpp"
 #include "test-forall-reduce-sanity-minloc.hpp"
@@ -38,5 +46,8 @@ REGISTER_TYPED_TEST_SUITE_P(ForallReduceSanityTest,
                             ReduceMaxSanityForall,
                             ReduceMinLocSanityForall,
                             ReduceMaxLocSanityForall);
+
+REGISTER_TYPED_TEST_SUITE_P(ForallReduceSanityTestBit,
+                            ReduceBitOrSanityForall);
 
 #endif  // __TEST_FORALL_REDUCE_SANITY_HPP__
