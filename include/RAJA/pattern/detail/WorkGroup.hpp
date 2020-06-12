cac8c6bbfdd3e5ac9f274a9439182c63791478ab
Jason Burmark <mr.burmark@gmail.com>
2020-06-12 16:30:32 -0700

    Add WorkStorage ragged_array_of_objects implementation

--- include/RAJA/pattern/detail/WorkGroup.hpp
+++ include/RAJA/pattern/detail/WorkGroup.hpp
@@ -331,6 +331,302 @@ private:
   }
 };
 
+template < typename ALLOCATOR_T, typename ... CallArgs >
+struct WorkStorage<RAJA::ragged_array_of_objects, ALLOCATOR_T, CallArgs...>
+{
+  using storage_policy = RAJA::ragged_array_of_objects;
+  using Allocator = ALLOCATOR_T;
+
+  using value_type = GenericWorkStruct<CallArgs...>;
+
+  struct const_iterator
+  {
+    using value_type = const typename WorkStorage::value_type;
+    using pointer = value_type*;
+    using reference = value_type&;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::random_access_iterator_tag;
+
+    const_iterator(const char* array_begin, const size_t* offset_iter)
+      : m_array_begin(array_begin)
+      , m_offset_iter(offset_iter)
+    { }
+
+    reference operator*() const
+    {
+      return *static_cast<const value_type*>(static_cast<const void*>(
+          m_array_begin + *m_offset_iter));
+    }
+
+    reference operator[](difference_type i) const
+    {
+      const_iterator copy = *this;
+      copy += i;
+      return *copy;
+    }
+
+    const_iterator& operator++()
+    {
+      ++m_offset_iter;
+      return *this;
+    }
+
+    const_iterator operator++(int)
+    {
+      const_iterator copy = *this;
+      ++(*this);
+      return copy;
+    }
+
+    const_iterator& operator--()
+    {
+      --m_offset_iter;
+      return *this;
+    }
+
+    const_iterator operator--(int)
+    {
+      const_iterator copy = *this;
+      --(*this);
+      return copy;
+    }
+
+    const_iterator& operator+=(difference_type n)
+    {
+      m_offset_iter += n;
+      return *this;
+    }
+
+    const_iterator& operator-=(difference_type n)
+    {
+      m_offset_iter -= n;
+      return *this;
+    }
+
+    friend inline const_iterator operator+(
+        const_iterator const& iter, difference_type n)
+    {
+      const_iterator copy = iter;
+      copy += n;
+      return copy;
+    }
+
+    friend inline const_iterator operator+(
+        difference_type n, const_iterator const& iter)
+    {
+      const_iterator copy = iter;
+      copy += n;
+      return copy;
+    }
+
+    friend inline const_iterator operator-(
+        const_iterator const& iter, difference_type n)
+    {
+      const_iterator copy = iter;
+      copy -= n;
+      return copy;
+    }
+
+    friend inline difference_type operator-(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return rhs_iter.m_offset_iter - lhs_iter.m_offset_iter;
+    }
+
+    friend inline bool operator==(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return lhs_iter.m_offset_iter == rhs_iter.m_offset_iter;
+    }
+
+    friend inline bool operator<(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return lhs_iter.m_offset_iter < rhs_iter.m_offset_iter;
+    }
+
+    friend inline bool operator<=(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return lhs_iter.m_offset_iter <= rhs_iter.m_offset_iter;
+    }
+
+    friend inline bool operator>(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return lhs_iter.m_offset_iter > rhs_iter.m_offset_iter;
+    }
+
+    friend inline bool operator>=(
+        const_iterator const& lhs_iter, const_iterator const& rhs_iter)
+    {
+      return lhs_iter.m_offset_iter >= rhs_iter.m_offset_iter;
+    }
+
+  private:
+    const char* m_array_begin;
+    const size_t* m_offset_iter;
+  };
+
+  using view_type = RAJA::Span<const_iterator, size_t>;
+
+  WorkStorage(Allocator aloc)
+    : m_offsets(std::forward<Allocator>(aloc))
+  { }
+
+  WorkStorage(WorkStorage const&) = delete;
+  WorkStorage& operator=(WorkStorage const&) = delete;
+
+  WorkStorage(WorkStorage&& o)
+    : m_offsets(std::move(o.m_offsets))
+    , m_array_begin(o.m_array_begin)
+    , m_array_end(o.m_array_end)
+    , m_array_cap(o.m_array_cap)
+  {
+    o.m_array_begin = nullptr;
+    o.m_array_end = nullptr;
+    o.m_array_cap = nullptr;
+  }
+
+  WorkStorage& operator=(WorkStorage&& o)
+  {
+    m_offsets     = std::move(o.m_offsets);
+    m_array_begin = o.m_array_begin;
+    m_array_end   = o.m_array_end  ;
+    m_array_cap   = o.m_array_cap  ;
+
+    o.m_array_begin = nullptr;
+    o.m_array_end   = nullptr;
+    o.m_array_cap   = nullptr;
+  }
+
+
+  void reserve(size_t num_loops, size_t loop_storage_size)
+  {
+    m_offsets.reserve(num_loops);
+    array_reserve(loop_storage_size);
+  }
+
+  // number of loops stored
+  size_t size() const
+  {
+    return m_offsets.size();
+  }
+
+  const_iterator begin() const
+  {
+    return const_iterator(m_array_begin, m_offsets.begin());
+  }
+
+  const_iterator end() const
+  {
+    return const_iterator(m_array_begin, m_offsets.end());
+  }
+
+  view_type get_view() const
+  {
+    return view_type(begin(), end());
+  }
+
+  template < typename loop_in >
+  void add(Vtable<CallArgs...>* vtable, loop_in&& loop)
+  {
+    m_offsets.emplace_back(
+        create_value(vtable, std::forward<loop_in>(loop)));
+  }
+
+  ~WorkStorage()
+  {
+    for (size_t count = size(); count > 0; --count) {
+      destroy_value(m_offsets.pop_back());
+    }
+    if (m_array_begin != nullptr) {
+      m_offsets.get_allocator().deallocate(m_array_begin);
+    }
+  }
+
+private:
+  SimpleVector<size_t, Allocator> m_offsets;
+  char* m_array_begin = nullptr;
+  char* m_array_end   = nullptr;
+  char* m_array_cap   = nullptr;
+
+  size_t array_size() const
+  {
+    return m_array_end - m_array_begin;
+  }
+
+  size_t array_capacity() const
+  {
+    return m_array_cap - m_array_begin;
+  }
+
+  size_t array_extra() const
+  {
+    return m_array_cap - m_array_end;
+  }
+
+  void array_reserve(size_t loop_storage_size)
+  {
+    if (loop_storage_size > array_capacity()) {
+      char* new_array_begin = static_cast<char*>(
+          m_offsets.get_allocator().allocate(loop_storage_size));
+      char* new_array_end   = new_array_begin + array_size();
+      char* new_array_cap   = new_array_begin + loop_storage_size;
+
+      for (size_t i = 0; i < size(); ++i) {
+        size_t offset = m_offsets[i];
+
+        value_type* old_value_ptr = m_array_begin + offset;
+        value_type* new_value_ptr = new_array_begin + offset;
+
+        new_value_ptr->vtable = old_value_ptr->vtable;
+        new_value_ptr->call = old_value_ptr->call;
+        new_value_ptr->vtable->move_construct(
+            &new_value_ptr->obj, &old_value_ptr->obj);
+        new_value_ptr->vtable->destroy(&old_value_ptr->obj);
+      }
+
+      m_offsets.get_allocator().deallocate(m_array_begin);
+
+      m_array_begin = new_array_begin;
+      m_array_end   = new_array_end  ;
+      m_array_cap   = new_array_cap  ;
+    }
+  }
+
+  template < typename loop_in >
+  size_t create_value(Vtable<CallArgs...>* vtable, loop_in&& loop)
+  {
+    using loop_type = camp::decay<loop_in>;
+    const size_t value_size = sizeof(WorkStruct<sizeof(loop_type), CallArgs...>);
+
+    if (value_size > array_extra()) {
+      array_reserve(std::max(array_size() + value_size, 2*array_capacity()));
+    }
+
+    size_t value_offset = array_size();
+    m_array_end += value_size;
+    value_type* value_ptr =
+        static_cast<value_type*>(static_cast<void*>(
+          m_array_begin + value_offset));
+
+    value_ptr->vtable = vtable;
+    value_ptr->call = vtable->call;
+    new(&value_ptr->obj) loop_type(std::forward<loop_in>(loop));
+
+    return value_offset;
+  }
+
+  void destroy_value(size_t value_offset)
+  {
+    value_type* value_ptr =
+        static_cast<value_type*>(static_cast<void*>(
+          m_array_begin + value_offset));
+    value_ptr->vtable->destroy(&value_ptr->obj);
+  }
+};
+
 }  // namespace detail
 
 }  // namespace RAJA
