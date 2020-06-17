a139c88703fb9ed52455a6cd27f26bcb6e12c72f
Jason Burmark <mr.burmark@gmail.com>
2020-06-17 09:48:09 -0700

    Move Vtable below SimpleVector

--- include/RAJA/pattern/detail/WorkGroup.hpp
+++ include/RAJA/pattern/detail/WorkGroup.hpp
@@ -34,59 +34,6 @@ namespace RAJA
 namespace detail
 {
 
-template < typename T, typename ... CallArgs >
-void Vtable_move_construct(void* dest, void* src)
-{
-  T* dest_as_T = static_cast<T*>(dest);
-  T* src_as_T = static_cast<T*>(src);
-  new(dest_as_T) T(std::move(*src_as_T));
-}
-
-template < typename T, typename ... CallArgs >
-void Vtable_call(const void* obj, CallArgs... args)
-{
-  const T* obj_as_T = static_cast<const T*>(obj);
-  (*obj_as_T)(std::forward<CallArgs>(args)...);
-}
-
-template < typename T, typename ... CallArgs >
-void Vtable_destroy(void* obj)
-{
-  T* obj_as_T = static_cast<T*>(obj);
-  (*obj_as_T).~T();
-}
-
-/*!
- * A vtable abstraction
- *
- * Provides function pointers for basic functions.
- */
-template < typename ... CallArgs >
-struct Vtable {
-  using move_sig = void(*)(void* /*dest*/, void* /*src*/);
-  using call_sig = void(*)(const void* /*obj*/, CallArgs... /*args*/);
-  using destroy_sig = void(*)(void* /*obj*/);
-
-  move_sig move_construct;
-  call_sig call;
-  destroy_sig destroy;
-  size_t size;
-};
-
-template < typename ... CallArgs >
-using Vtable_move_sig = typename Vtable<CallArgs...>::move_sig;
-template < typename ... CallArgs >
-using Vtable_call_sig = typename Vtable<CallArgs...>::call_sig;
-template < typename ... CallArgs >
-using Vtable_destroy_sig = typename Vtable<CallArgs...>::destroy_sig;
-
-/*!
- * Populate and return a Vtable object appropriate for the given policy
- */
-// template < typename T, typename ... CallArgs >
-// inline Vtable<CallArgs...> get_Vtable(work_policy const&);
-
-
 /*!
  * A simple vector like class
  */
@@ -219,6 +166,59 @@ private:
   T* m_end   = nullptr;
   T* m_cap   = nullptr;
 };
+
+
+template < typename T, typename ... CallArgs >
+void Vtable_move_construct(void* dest, void* src)
+{
+  T* dest_as_T = static_cast<T*>(dest);
+  T* src_as_T = static_cast<T*>(src);
+  new(dest_as_T) T(std::move(*src_as_T));
+}
+
+template < typename T, typename ... CallArgs >
+void Vtable_call(const void* obj, CallArgs... args)
+{
+  const T* obj_as_T = static_cast<const T*>(obj);
+  (*obj_as_T)(std::forward<CallArgs>(args)...);
+}
+
+template < typename T, typename ... CallArgs >
+void Vtable_destroy(void* obj)
+{
+  T* obj_as_T = static_cast<T*>(obj);
+  (*obj_as_T).~T();
+}
+
+/*!
+ * A vtable abstraction
+ *
+ * Provides function pointers for basic functions.
+ */
+template < typename ... CallArgs >
+struct Vtable {
+  using move_sig = void(*)(void* /*dest*/, void* /*src*/);
+  using call_sig = void(*)(const void* /*obj*/, CallArgs... /*args*/);
+  using destroy_sig = void(*)(void* /*obj*/);
+
+  move_sig move_construct;
+  call_sig call;
+  destroy_sig destroy;
+  size_t size;
+};
+
+template < typename ... CallArgs >
+using Vtable_move_sig = typename Vtable<CallArgs...>::move_sig;
+template < typename ... CallArgs >
+using Vtable_call_sig = typename Vtable<CallArgs...>::call_sig;
+template < typename ... CallArgs >
+using Vtable_destroy_sig = typename Vtable<CallArgs...>::destroy_sig;
+
+/*!
+ * Populate and return a Vtable object appropriate for the given policy
+ */
+// template < typename T, typename ... CallArgs >
+// inline Vtable<CallArgs...> get_Vtable(work_policy const&);
 
 
 /*!
