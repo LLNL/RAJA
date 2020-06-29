d047d20e99a8c10bf41c48bc952fa5f6b8091b96
Jason Burmark <mr.burmark@gmail.com>
2020-06-28 21:42:36 -0700

    Add WorkStorage test for move assign to non-empty

--- test/unit/workgroup/tests/test-workgroup-WorkStorage.hpp
+++ test/unit/workgroup/tests/test-workgroup-WorkStorage.hpp
@@ -166,32 +166,56 @@ void testWorkGroupWorkStorageInsert()
       callable, Vtable_type>(RAJA::seq_work{});
 
   {
+    auto test_empty = [&](WorkStorage_type& container) {
+
+      ASSERT_EQ(container.size(), (size_t)(0));
+      ASSERT_EQ(container.storage_size(), (size_t)0);
+    };
+
+    auto fill_contents = [&](WorkStorage_type& container, short init_val) {
+
+      container.template emplace<callable>(vtable, callable{init_val});
+
+      ASSERT_EQ(container.size(), (size_t)1);
+      ASSERT_TRUE(container.storage_size() >= sizeof(callable));
+    };
+
+    auto test_contents = [&](WorkStorage_type& container, short) {
+
+      ASSERT_EQ(container.size(), (size_t)1);
+      ASSERT_TRUE(container.storage_size() >= sizeof(callable));
+    };
+
+
     WorkStorage_type container(Allocator{});
 
-    ASSERT_EQ(container.size(), (size_t)(0));
-    ASSERT_EQ(container.storage_size(), (size_t)0);
+    test_empty(container);
+    fill_contents(container, 0);
+    test_contents(container, 0);
 
-    container.template emplace<callable>(vtable, callable{0});
-
-    ASSERT_EQ(container.size(), (size_t)1);
-    ASSERT_TRUE(container.storage_size() >= sizeof(callable));
 
     WorkStorage_type container2(std::move(container));
 
-    ASSERT_EQ(container.size(), (size_t)(0));
-    ASSERT_EQ(container.storage_size(), (size_t)0);
+    test_empty(container);
+    test_contents(container2, 0);
 
-    ASSERT_EQ(container2.size(), (size_t)1);
-    ASSERT_TRUE(container2.storage_size() >= sizeof(callable));
 
     WorkStorage_type container3(Allocator{});
     container3 = std::move(container2);
 
-    ASSERT_EQ(container2.size(), (size_t)(0));
-    ASSERT_EQ(container2.storage_size(), (size_t)0);
+    test_empty(container2);
+    test_contents(container3, 0);
 
-    ASSERT_EQ(container3.size(), (size_t)1);
-    ASSERT_TRUE(container3.storage_size() >= sizeof(callable));
+
+    WorkStorage_type container4(Allocator{});
+
+    fill_contents(container4, 1);
+    test_contents(container4, 1);
+
+    container4 = std::move(container3);
+
+    test_empty(container3);
+    test_contents(container4, 0);
   }
 
   ASSERT_TRUE(success);
@@ -298,58 +322,75 @@ void testWorkGroupWorkStorageInsertCall()
       callable, Vtable_type>(RAJA::seq_work{});
 
   {
-    WorkStorage_type container(Allocator{});
+    auto test_empty = [&](WorkStorage_type& container) {
 
-    ASSERT_EQ(container.size(), (size_t)(0));
-    ASSERT_EQ(container.storage_size(), (size_t)0);
+      ASSERT_EQ(container.size(), (size_t)(0));
+      ASSERT_EQ(container.storage_size(), (size_t)0);
+    };
 
-    callable c(1.23456789);
+    auto fill_contents = [&](WorkStorage_type& container, double init_val) {
 
-    ASSERT_FALSE(c.move_constructed);
-    ASSERT_FALSE(c.moved_from);
+      callable c(init_val);
 
-    container.template emplace<callable>(vtable, std::move(c));
+      ASSERT_FALSE(c.move_constructed);
+      ASSERT_FALSE(c.moved_from);
 
-    ASSERT_FALSE(c.move_constructed);
-    ASSERT_TRUE(c.moved_from);
+      container.template emplace<callable>(vtable, std::move(c));
 
-    auto test_contents = [&](WorkStorage_type& container) {
+      ASSERT_FALSE(c.move_constructed);
+      ASSERT_TRUE(c.moved_from);
+
+      ASSERT_EQ(container.size(), (size_t)1);
+      ASSERT_TRUE(container.storage_size() >= sizeof(callable));
+    };
+
+    auto test_contents = [&](WorkStorage_type& container, double init_val) {
 
       ASSERT_EQ(container.size(), (size_t)1);
       ASSERT_TRUE(container.storage_size() >= sizeof(callable));
 
-      {
-        auto iter = container.begin();
+      auto iter = container.begin();
 
-        double val = -1;
-        bool move_constructed = false;
-        bool moved_from = true;
-        WorkStruct_type::call(&*iter, (void*)&val, &move_constructed, &moved_from);
+      double test_val = -1;
+      bool move_constructed = false;
+      bool moved_from = true;
+      WorkStruct_type::call(&*iter, (void*)&test_val, &move_constructed, &moved_from);
 
-        ASSERT_EQ(val, 1.23456789);
-        ASSERT_TRUE(move_constructed);
-        ASSERT_FALSE(moved_from);
-      }
+      ASSERT_EQ(test_val, init_val);
+      ASSERT_TRUE(move_constructed);
+      ASSERT_FALSE(moved_from);
     };
 
-    test_contents(container);
+
+    WorkStorage_type container(Allocator{});
+
+    test_empty(container);
+    fill_contents(container, 1.23456789);
+    test_contents(container, 1.23456789);
 
 
     WorkStorage_type container2(std::move(container));
 
-    ASSERT_EQ(container.size(), (size_t)(0));
-    ASSERT_EQ(container.storage_size(), (size_t)0);
-
-    test_contents(container2);
+    test_empty(container);
+    test_contents(container2, 1.23456789);
 
 
     WorkStorage_type container3(Allocator{});
     container3 = std::move(container2);
 
-    ASSERT_EQ(container2.size(), (size_t)(0));
-    ASSERT_EQ(container2.storage_size(), (size_t)0);
+    test_empty(container2);
+    test_contents(container3, 1.23456789);
 
-    test_contents(container3);
+
+    WorkStorage_type container4(Allocator{});
+
+    fill_contents(container4, 2.34567891);
+    test_contents(container4, 2.34567891);
+
+    container4 = std::move(container3);
+
+    test_empty(container3);
+    test_contents(container4, 1.23456789);
   }
 
   ASSERT_TRUE(success);
@@ -404,21 +445,21 @@ void testWorkGroupWorkStorageMultiple(
   using type1 = TestArray<double, 6>;
   using type2 = TestArray<double, 14>;
 
-  auto make_type0 = [](size_t i) {
-    type0 obj(-(type0)i);
+  auto make_type0 = [](double init_val, size_t i) {
+    type0 obj(init_val - (double)i);
     return obj;
   };
-  auto make_type1 = [](size_t i) {
+  auto make_type1 = [](double init_val, size_t i) {
     type1 obj{};
     for (size_t j = 0; j < 6; ++j) {
-      obj[j] = 100.0 + 10.0 * j + i;
+      obj[j] = init_val + 10.0 * j + i;
     }
     return obj;
   };
-  auto make_type2 = [](size_t i) {
+  auto make_type2 = [](double init_val, size_t i) {
     type2 obj{};
     for (size_t j = 0; j < 14; ++j) {
-      obj[j] = 1000.0 + 10.0 * j + i;
+      obj[j] = init_val + 10.0 * j + i;
     }
     return obj;
   };
@@ -435,45 +476,55 @@ void testWorkGroupWorkStorageMultiple(
       callable2, Vtable_type>(RAJA::seq_work{});
 
   {
-    WorkStorage_type container(Allocator{});
+    auto test_empty = [&](WorkStorage_type& container) {
 
-    ASSERT_EQ(container.size(), (size_t)(0));
-    ASSERT_EQ(container.storage_size(), (size_t)0);
+      ASSERT_EQ(container.size(), (size_t)(0));
+      ASSERT_EQ(container.storage_size(), (size_t)0);
+    };
 
-    std::vector<callable0> vec0;
-    vec0.reserve(num0);
-    for (size_t i = 0; i < num0; ++i) {
-      vec0.emplace_back(make_type0(i));
-      ASSERT_FALSE(vec0[i].move_constructed);
-      ASSERT_FALSE(vec0[i].moved_from);
-      container.template emplace<callable0>(vtable0, std::move(vec0[i]));
-      ASSERT_FALSE(vec0[i].move_constructed);
-      ASSERT_TRUE (vec0[i].moved_from);
-    }
+    auto fill_contents = [&](WorkStorage_type& container, double init_val0, double init_val1, double init_val2) {
 
-    std::vector<callable1> vec1;
-    vec1.reserve(num1);
-    for (size_t i = 0; i < num1; ++i) {
-      vec1.emplace_back(make_type1(i));
-      ASSERT_FALSE(vec1[i].move_constructed);
-      ASSERT_FALSE(vec1[i].moved_from);
-      container.template emplace<callable1>(vtable1, std::move(vec1[i]));
-      ASSERT_FALSE(vec1[i].move_constructed);
-      ASSERT_TRUE (vec1[i].moved_from);
-    }
+      std::vector<callable0> vec0;
+      vec0.reserve(num0);
+      for (size_t i = 0; i < num0; ++i) {
+        vec0.emplace_back(make_type0(init_val0, i));
+        ASSERT_FALSE(vec0[i].move_constructed);
+        ASSERT_FALSE(vec0[i].moved_from);
+        container.template emplace<callable0>(vtable0, std::move(vec0[i]));
+        ASSERT_FALSE(vec0[i].move_constructed);
+        ASSERT_TRUE (vec0[i].moved_from);
+      }
 
-    std::vector<callable2> vec2;
-    vec2.reserve(num2);
-    for (size_t i = 0; i < num2; ++i) {
-      vec2.emplace_back(make_type2(i));
-      ASSERT_FALSE(vec2[i].move_constructed);
-      ASSERT_FALSE(vec2[i].moved_from);
-      container.template emplace<callable2>(vtable2, std::move(vec2[i]));
-      ASSERT_FALSE(vec2[i].move_constructed);
-      ASSERT_TRUE (vec2[i].moved_from);
-    }
+      std::vector<callable1> vec1;
+      vec1.reserve(num1);
+      for (size_t i = 0; i < num1; ++i) {
+        vec1.emplace_back(make_type1(init_val1, i));
+        ASSERT_FALSE(vec1[i].move_constructed);
+        ASSERT_FALSE(vec1[i].moved_from);
+        container.template emplace<callable1>(vtable1, std::move(vec1[i]));
+        ASSERT_FALSE(vec1[i].move_constructed);
+        ASSERT_TRUE (vec1[i].moved_from);
+      }
 
-    auto test_contents = [&](WorkStorage_type& container) {
+      std::vector<callable2> vec2;
+      vec2.reserve(num2);
+      for (size_t i = 0; i < num2; ++i) {
+        vec2.emplace_back(make_type2(init_val2, i));
+        ASSERT_FALSE(vec2[i].move_constructed);
+        ASSERT_FALSE(vec2[i].moved_from);
+        container.template emplace<callable2>(vtable2, std::move(vec2[i]));
+        ASSERT_FALSE(vec2[i].move_constructed);
+        ASSERT_TRUE (vec2[i].moved_from);
+      }
+
+      ASSERT_EQ(container.size(), num0+num1+num2);
+      ASSERT_GE(container.storage_size(),
+          num0*sizeof(callable0) +
+          num1*sizeof(callable1) +
+          num2*sizeof(callable2));
+    };
+
+    auto test_contents = [&](WorkStorage_type& container, double init_val0, double init_val1, double init_val2) {
 
       ASSERT_EQ(container.size(), num0+num1+num2);
       ASSERT_GE(container.storage_size(),
@@ -490,7 +541,7 @@ void testWorkGroupWorkStorageMultiple(
           bool moved_from = true;
           WorkStruct_type::call(&*iter, (void*)&val, &move_constructed, &moved_from);
 
-          type0 expected = make_type0(i);
+          type0 expected = make_type0(init_val0, i);
           ASSERT_EQ(val, expected);
           ASSERT_TRUE(move_constructed);
           ASSERT_FALSE(moved_from);
@@ -504,7 +555,7 @@ void testWorkGroupWorkStorageMultiple(
           bool moved_from = true;
           WorkStruct_type::call(&*iter, (void*)&val, &move_constructed, &moved_from);
 
-          type1 expected = make_type1(i);
+          type1 expected = make_type1(init_val1, i);
           ASSERT_EQ(val, expected);
           ASSERT_TRUE(move_constructed);
           ASSERT_FALSE(moved_from);
@@ -518,7 +569,7 @@ void testWorkGroupWorkStorageMultiple(
           bool moved_from = true;
           WorkStruct_type::call(&*iter, (void*)&val, &move_constructed, &moved_from);
 
-          type2 expected = make_type2(i);
+          type2 expected = make_type2(init_val2, i);
           ASSERT_EQ(val, expected);
           ASSERT_TRUE(move_constructed);
           ASSERT_FALSE(moved_from);
@@ -530,24 +581,35 @@ void testWorkGroupWorkStorageMultiple(
       }
     };
 
-    test_contents(container);
+    WorkStorage_type container(Allocator{});
+
+    test_empty(container);
+    fill_contents(container, 1.0, 100.0, 1000.0);
+    test_contents(container, 1.0, 100.0, 1000.0);
 
 
     WorkStorage_type container2(std::move(container));
 
-    ASSERT_EQ(container.size(), (size_t)0);
-    ASSERT_EQ(container.storage_size(), (size_t)0);
-
-    test_contents(container2);
+    test_empty(container);
+    test_contents(container2, 1.0, 100.0, 1000.0);
 
 
     WorkStorage_type container3(Allocator{});
     container3 = std::move(container2);
 
-    ASSERT_EQ(container2.size(), (size_t)(0));
-    ASSERT_EQ(container2.storage_size(), (size_t)0);
+    test_empty(container2);
+    test_contents(container3, 1.0, 100.0, 1000.0);
 
-    test_contents(container3);
+
+    WorkStorage_type container4(Allocator{});
+
+    fill_contents(container4, 1.5, 100.5, 1000.5);
+    test_contents(container4, 1.5, 100.5, 1000.5);
+
+    container4 = std::move(container3);
+
+    test_empty(container3);
+    test_contents(container4, 1.0, 100.0, 1000.0);
 
   }
 
