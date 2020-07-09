6474dcb0b60219bfadccf0bcaf967466213fe8a0
Jason Burmark <mr.burmark@gmail.com>
2020-07-09 12:07:12 -0700

    Use fewer lambdas in functional workgroup tests

--- test/functional/workgroup/tests/test-workgroup-Ordered.hpp
+++ test/functional/workgroup/tests/test-workgroup-Ordered.hpp
@@ -66,8 +66,7 @@ void testWorkGroupOrderedSingle(IndexType begin, IndexType end)
                                     &test_array);
 
 
-  auto set_test_data = [&]() {
-
+  {
     for (IndexType i = IndexType(0); i < N; i++) {
       test_array[i] = IndexType(0);
     }
@@ -77,10 +76,13 @@ void testWorkGroupOrderedSingle(IndexType begin, IndexType end)
     for (IndexType i = begin; i < end; ++i) {
       test_array[ i ] = IndexType(i);
     }
-  };
+  }
 
-  auto fill_pool = [&](WorkPool_type& pool, IndexType test_val) {
+  WorkPool_type pool(Allocator{});
 
+  IndexType test_val(5);
+
+  {
     pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin, end },
         [=] RAJA_HOST_DEVICE (IndexType i) {
       working_array[i] += i;
@@ -89,10 +91,13 @@ void testWorkGroupOrderedSingle(IndexType begin, IndexType end)
         [=] RAJA_HOST_DEVICE (IndexType i) {
       working_array[i] += test_val;
     });
-  };
+  }
 
-  auto check_test_data = [&](IndexType test_val) {
+  WorkGroup_type group = pool.instantiate();
 
+  WorkSite_type site = group.run();
+
+  {
     working_res.memcpy(check_array, working_array, sizeof(IndexType) * N);
 
     for (IndexType i = IndexType(0); i < begin; i++) {
@@ -104,20 +109,7 @@ void testWorkGroupOrderedSingle(IndexType begin, IndexType end)
     for (IndexType i = end;          i < N;     i++) {
       ASSERT_EQ(test_array[i], check_array[i]);
     }
-  };
-
-
-  set_test_data();
-
-  WorkPool_type pool(Allocator{});
-
-  fill_pool(pool, IndexType(5));
-
-  WorkGroup_type group = pool.instantiate();
-
-  WorkSite_type site = group.run();
-
-  check_test_data(IndexType(5));
+  }
 
 
   deallocateForallTestData<IndexType>(working_res,
@@ -224,166 +216,163 @@ void testWorkGroupOrderedMultiple(
                                 &test_array3);
 
 
-  auto set_test_data = [&]() {
-
-    for (IndexType j = IndexType(0); j < num1; j++) {
-      type1* test_ptr1 = test_array1 + N * j;
-      for (IndexType i = IndexType(0); i < N; i++) {
-        test_ptr1[i] = type1(0);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num2; j++) {
-      type2* test_ptr2 = test_array2 + N * j;
-      for (IndexType i = IndexType(0); i < N; i++) {
-        test_ptr2[i] = type2(0);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num3; j++) {
-      type3* test_ptr3 = test_array3 + N * j;
-      for (IndexType i = IndexType(0); i < N; i++) {
-        test_ptr3[i] = type3(0);
-      }
-    }
-
-
-    working_res.memcpy(working_array1, test_array1, sizeof(type1) * N * num1);
-
-    working_res.memcpy(working_array2, test_array2, sizeof(type2) * N * num2);
-
-    working_res.memcpy(working_array3, test_array3, sizeof(type3) * N * num3);
-
-
-    for (IndexType j = IndexType(0); j < num1; j++) {
-      type1* test_ptr1 = test_array1 + N * j;
-      for (IndexType i = begin1[j]; i < end1[j]; ++i) {
-        test_ptr1[ i ] = type1(i);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num2; j++) {
-      type2* test_ptr2 = test_array2 + N * j;
-      for (IndexType i = begin2[j]; i < end2[j]; ++i) {
-        test_ptr2[ i ] = type2(i);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num3; j++) {
-      type3* test_ptr3 = test_array3 + N * j;
-      for (IndexType i = begin3[j]; i < end3[j]; ++i) {
-        test_ptr3[ i ] = type3(i);
-      }
-    }
-  };
-
-  auto fill_pool = [&](WorkPool_type& pool, type1 test_val1, type2 test_val2, type3 test_val3) {
-
-    for (IndexType j = IndexType(0); j < num1; j++) {
-      type1* working_ptr1 = working_array1 + N * j;
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin1[j], end1[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr1[i] += type1(i);
-      });
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin1[j], end1[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr1[i] += test_val1;
-      });
-    }
-
-    for (IndexType j = IndexType(0); j < num2; j++) {
-      type2* working_ptr2 = working_array2 + N * j;
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin2[j], end2[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr2[i] += type2(i);
-      });
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin2[j], end2[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr2[i] += test_val2;
-      });
-    }
-
-    for (IndexType j = IndexType(0); j < num3; j++) {
-      type3* working_ptr3 = working_array3 + N * j;
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin3[j], end3[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr3[i] += type3(i);
-      });
-      pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin3[j], end3[j] },
-          [=] RAJA_HOST_DEVICE (IndexType i) {
-        working_ptr3[i] += test_val3;
-      });
-    }
-  };
-
-  auto check_test_data = [&](type1 test_val1, type2 test_val2, type3 test_val3) {
-
-    working_res.memcpy(check_array1, working_array1, sizeof(type1) * N * num1);
-
-    working_res.memcpy(check_array2, working_array2, sizeof(type2) * N * num2);
-
-    working_res.memcpy(check_array3, working_array3, sizeof(type3) * N * num3);
-
-
-    for (IndexType j = IndexType(0); j < num1; j++) {
-      type1* test_ptr1 = test_array1 + N * j;
-      type1* check_ptr1 = check_array1 + N * j;
-      for (IndexType i = IndexType(0); i < begin1[j]; i++) {
-        ASSERT_EQ(test_ptr1[i], check_ptr1[i]);
-      }
-      for (IndexType i = begin1[j];    i < end1[j];   i++) {
-        ASSERT_EQ(test_ptr1[i] + test_val1, check_ptr1[i]);
-      }
-      for (IndexType i = end1[j];      i < N;     i++) {
-        ASSERT_EQ(test_ptr1[i], check_ptr1[i]);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num2; j++) {
-      type2* test_ptr2 = test_array2 + N * j;
-      type2* check_ptr2 = check_array2 + N * j;
-      for (IndexType i = IndexType(0); i < begin2[j]; i++) {
-        ASSERT_EQ(test_ptr2[i], check_ptr2[i]);
-      }
-      for (IndexType i = begin2[j];    i < end2[j];   i++) {
-        ASSERT_EQ(test_ptr2[i] + test_val2, check_ptr2[i]);
-      }
-      for (IndexType i = end2[j];      i < N;     i++) {
-        ASSERT_EQ(test_ptr2[i], check_ptr2[i]);
-      }
-    }
-
-    for (IndexType j = IndexType(0); j < num3; j++) {
-      type3* test_ptr3 = test_array3 + N * j;
-      type3* check_ptr3 = check_array3 + N * j;
-      for (IndexType i = IndexType(0); i < begin3[j]; i++) {
-        ASSERT_EQ(test_ptr3[i], check_ptr3[i]);
-      }
-      for (IndexType i = begin3[j];    i < end3[j];   i++) {
-        ASSERT_EQ(test_ptr3[i] + test_val3, check_ptr3[i]);
-      }
-      for (IndexType i = end3[j];      i < N;     i++) {
-        ASSERT_EQ(test_ptr3[i], check_ptr3[i]);
-      }
-    }
-  };
-
-
   WorkPool_type pool(Allocator{});
 
   for (IndexType pr = 0; pr < pool_reuse; pr++) {
 
-    fill_pool(pool, type1(5), type2(7), type3(11));
+    type1 test_val1(5);
+    type2 test_val2(7);
+    type3 test_val3(11);
+
+    // fill_pool(pool, type1(5), type2(7), type3(11));
+    {
+      for (IndexType j = IndexType(0); j < num1; j++) {
+        type1* working_ptr1 = working_array1 + N * j;
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin1[j], end1[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr1[i] += type1(i);
+        });
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin1[j], end1[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr1[i] += test_val1;
+        });
+      }
+
+      for (IndexType j = IndexType(0); j < num2; j++) {
+        type2* working_ptr2 = working_array2 + N * j;
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin2[j], end2[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr2[i] += type2(i);
+        });
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin2[j], end2[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr2[i] += test_val2;
+        });
+      }
+
+      for (IndexType j = IndexType(0); j < num3; j++) {
+        type3* working_ptr3 = working_array3 + N * j;
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin3[j], end3[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr3[i] += type3(i);
+        });
+        pool.enqueue(RAJA::TypedRangeSegment<IndexType>{ begin3[j], end3[j] },
+            [=] RAJA_HOST_DEVICE (IndexType i) {
+          working_ptr3[i] += test_val3;
+        });
+      }
+    }
 
     WorkGroup_type group = pool.instantiate();
 
     for (IndexType gr = 0; gr < group_reuse; gr++) {
 
-      set_test_data();
+      // set_test_data();
+      {
+        for (IndexType j = IndexType(0); j < num1; j++) {
+          type1* test_ptr1 = test_array1 + N * j;
+          for (IndexType i = IndexType(0); i < N; i++) {
+            test_ptr1[i] = type1(0);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num2; j++) {
+          type2* test_ptr2 = test_array2 + N * j;
+          for (IndexType i = IndexType(0); i < N; i++) {
+            test_ptr2[i] = type2(0);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num3; j++) {
+          type3* test_ptr3 = test_array3 + N * j;
+          for (IndexType i = IndexType(0); i < N; i++) {
+            test_ptr3[i] = type3(0);
+          }
+        }
+
+
+        working_res.memcpy(working_array1, test_array1, sizeof(type1) * N * num1);
+
+        working_res.memcpy(working_array2, test_array2, sizeof(type2) * N * num2);
+
+        working_res.memcpy(working_array3, test_array3, sizeof(type3) * N * num3);
+
+
+        for (IndexType j = IndexType(0); j < num1; j++) {
+          type1* test_ptr1 = test_array1 + N * j;
+          for (IndexType i = begin1[j]; i < end1[j]; ++i) {
+            test_ptr1[ i ] = type1(i);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num2; j++) {
+          type2* test_ptr2 = test_array2 + N * j;
+          for (IndexType i = begin2[j]; i < end2[j]; ++i) {
+            test_ptr2[ i ] = type2(i);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num3; j++) {
+          type3* test_ptr3 = test_array3 + N * j;
+          for (IndexType i = begin3[j]; i < end3[j]; ++i) {
+            test_ptr3[ i ] = type3(i);
+          }
+        }
+      }
 
       WorkSite_type site = group.run();
 
-      check_test_data(type1(5), type2(7), type3(11));
+      // check_test_data(type1(5), type2(7), type3(11));
+      {
+        working_res.memcpy(check_array1, working_array1, sizeof(type1) * N * num1);
+
+        working_res.memcpy(check_array2, working_array2, sizeof(type2) * N * num2);
+
+        working_res.memcpy(check_array3, working_array3, sizeof(type3) * N * num3);
+
+
+        for (IndexType j = IndexType(0); j < num1; j++) {
+          type1* test_ptr1 = test_array1 + N * j;
+          type1* check_ptr1 = check_array1 + N * j;
+          for (IndexType i = IndexType(0); i < begin1[j]; i++) {
+            ASSERT_EQ(test_ptr1[i], check_ptr1[i]);
+          }
+          for (IndexType i = begin1[j];    i < end1[j];   i++) {
+            ASSERT_EQ(test_ptr1[i] + test_val1, check_ptr1[i]);
+          }
+          for (IndexType i = end1[j];      i < N;     i++) {
+            ASSERT_EQ(test_ptr1[i], check_ptr1[i]);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num2; j++) {
+          type2* test_ptr2 = test_array2 + N * j;
+          type2* check_ptr2 = check_array2 + N * j;
+          for (IndexType i = IndexType(0); i < begin2[j]; i++) {
+            ASSERT_EQ(test_ptr2[i], check_ptr2[i]);
+          }
+          for (IndexType i = begin2[j];    i < end2[j];   i++) {
+            ASSERT_EQ(test_ptr2[i] + test_val2, check_ptr2[i]);
+          }
+          for (IndexType i = end2[j];      i < N;     i++) {
+            ASSERT_EQ(test_ptr2[i], check_ptr2[i]);
+          }
+        }
+
+        for (IndexType j = IndexType(0); j < num3; j++) {
+          type3* test_ptr3 = test_array3 + N * j;
+          type3* check_ptr3 = check_array3 + N * j;
+          for (IndexType i = IndexType(0); i < begin3[j]; i++) {
+            ASSERT_EQ(test_ptr3[i], check_ptr3[i]);
+          }
+          for (IndexType i = begin3[j];    i < end3[j];   i++) {
+            ASSERT_EQ(test_ptr3[i] + test_val3, check_ptr3[i]);
+          }
+          for (IndexType i = end3[j];      i < N;     i++) {
+            ASSERT_EQ(test_ptr3[i], check_ptr3[i]);
+          }
+        }
+      }
     }
   }
 
