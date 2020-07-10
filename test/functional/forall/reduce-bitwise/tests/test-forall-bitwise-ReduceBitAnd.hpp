2699da976d2e01604ac9473f0f7ddb86086514c3
Kae Suarez <suarez29@llnl.gov>
2020-07-10 14:01:46 -0700

    Making new test set for bitwise reduction

--- test/functional/forall/reduce-bitwise/tests/test-forall-bitwise-ReduceBitAnd.hpp
+++ test/functional/forall/reduce-bitwise/tests/test-forall-bitwise-ReduceBitAnd.hpp
@@ -5,19 +5,15 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-#ifndef __TEST_FORALL_REDUCEBITOR_SANITY_HPP__
-#define __TEST_FORALL_REDUCEBITOR_SANITY_HPP__
-
-#include "RAJA/RAJA.hpp"
-
-#include "test-forall-reduce-sanity.hpp"
+#ifndef __TEST_FORALL_BITWISE_REDUCEBITAND_HPP__
+#define __TEST_FORALL_BITWISE_REDUCEBITAND_HPP__
 
 #include <cstdlib>
 #include <numeric>
 
 template <typename DATA_TYPE, typename WORKING_RES, 
           typename EXEC_POLICY, typename REDUCE_POLICY>
-void ForallReduceBitOrSanityTest(RAJA::Index_type first, RAJA::Index_type last)
+void ForallReduceBitAndBitwiseTestImpl(RAJA::Index_type first, RAJA::Index_type last)
 {
   RAJA::TypedRangeSegment<RAJA::Index_type> r1(first, last);
 
@@ -40,18 +36,18 @@ void ForallReduceBitOrSanityTest(RAJA::Index_type first, RAJA::Index_type last)
 
   DATA_TYPE ref_sum = 0;
   for (RAJA::Index_type i = first; i < last; ++i) {
-    ref_sum |= test_array[i]; 
+    ref_sum &= test_array[i]; 
   }
 
   working_res.memcpy(working_array, test_array, sizeof(DATA_TYPE) * last);
 
 
-  RAJA::ReduceBitOr<REDUCE_POLICY, DATA_TYPE> sum(0);
-  RAJA::ReduceBitOr<REDUCE_POLICY, DATA_TYPE> sum2(2);
+  RAJA::ReduceBitAnd<REDUCE_POLICY, DATA_TYPE> sum(0);
+  RAJA::ReduceBitAnd<REDUCE_POLICY, DATA_TYPE> sum2(2);
 
   RAJA::forall<EXEC_POLICY>(r1, [=] RAJA_HOST_DEVICE(RAJA::Index_type idx) {
-    sum  |= working_array[idx];
-    sum2 |= working_array[idx];
+    sum  &= working_array[idx];
+    sum2 &= working_array[idx];
   });
 
   ASSERT_EQ(static_cast<DATA_TYPE>(sum.get()), ref_sum);
@@ -63,7 +59,7 @@ void ForallReduceBitOrSanityTest(RAJA::Index_type first, RAJA::Index_type last)
 
   for (int j = 0; j < nloops; ++j) {
     RAJA::forall<EXEC_POLICY>(r1, [=] RAJA_HOST_DEVICE(RAJA::Index_type idx) {
-      sum |= working_array[idx];
+      sum &= working_array[idx];
     });
   }
 
@@ -77,19 +73,28 @@ void ForallReduceBitOrSanityTest(RAJA::Index_type first, RAJA::Index_type last)
 }
 
 
-TYPED_TEST_P(ForallReduceSanityTestBit, ReduceBitOrSanityForall)
+TYPED_TEST_SUITE_P(ForallReduceBitAndBitwiseTest);
+template <typename T>
+class ForallReduceBitAndBitwiseTest : public ::testing::Test
+{
+};
+
+TYPED_TEST_P(ForallReduceBitAndBitwiseTest, ReduceBitAndBitwiseForall)
 {
   using DATA_TYPE     = typename camp::at<TypeParam, camp::num<0>>::type;
   using WORKING_RES   = typename camp::at<TypeParam, camp::num<1>>::type;
   using EXEC_POLICY   = typename camp::at<TypeParam, camp::num<2>>::type;
   using REDUCE_POLICY = typename camp::at<TypeParam, camp::num<3>>::type;
 
-  ForallReduceBitOrSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitAndBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(0, 28);
-  ForallReduceBitOrSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitAndBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(3, 642);
-  ForallReduceBitOrSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitAndBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(0, 2057);
 }
 
-#endif  // __TEST_FORALL_REDUCEBITOR_SANITY_HPP__
+REGISTER_TYPED_TEST_SUITE_P(ForallReduceBitAndBitwiseTest,
+                            ReduceBitAndBitwiseForall);
+
+#endif  // __TEST_FORALL_BITWISE_REDUCEBITOR_HPP__
--- test/functional/forall/reduce-bitwise/tests/test-forall-bitwise-ReduceBitOr.hpp
+++ test/functional/forall/reduce-bitwise/tests/test-forall-bitwise-ReduceBitOr.hpp
@@ -5,19 +5,15 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-#ifndef __TEST_FORALL_REDUCEBITAND_SANITY_HPP__
-#define __TEST_FORALL_REDUCEBITAND_SANITY_HPP__
-
-#include "RAJA/RAJA.hpp"
-
-#include "test-forall-reduce-sanity.hpp"
+#ifndef __TEST_FORALL_BITWISE_REDUCEBITOR_HPP__
+#define __TEST_FORALL_BITWISE_REDUCEBITOR_HPP__
 
 #include <cstdlib>
 #include <numeric>
 
 template <typename DATA_TYPE, typename WORKING_RES, 
           typename EXEC_POLICY, typename REDUCE_POLICY>
-void ForallReduceBitAndSanityTest(RAJA::Index_type first, RAJA::Index_type last)
+void ForallReduceBitOrBitwiseTestImpl(RAJA::Index_type first, RAJA::Index_type last)
 {
   RAJA::TypedRangeSegment<RAJA::Index_type> r1(first, last);
 
@@ -40,18 +36,18 @@ void ForallReduceBitAndSanityTest(RAJA::Index_type first, RAJA::Index_type last)
 
   DATA_TYPE ref_sum = 0;
   for (RAJA::Index_type i = first; i < last; ++i) {
-    ref_sum &= test_array[i]; 
+    ref_sum |= test_array[i]; 
   }
 
   working_res.memcpy(working_array, test_array, sizeof(DATA_TYPE) * last);
 
 
-  RAJA::ReduceBitAnd<REDUCE_POLICY, DATA_TYPE> sum(0);
-  RAJA::ReduceBitAnd<REDUCE_POLICY, DATA_TYPE> sum2(2);
+  RAJA::ReduceBitOr<REDUCE_POLICY, DATA_TYPE> sum(0);
+  RAJA::ReduceBitOr<REDUCE_POLICY, DATA_TYPE> sum2(2);
 
   RAJA::forall<EXEC_POLICY>(r1, [=] RAJA_HOST_DEVICE(RAJA::Index_type idx) {
-    sum  &= working_array[idx];
-    sum2 &= working_array[idx];
+    sum  |= working_array[idx];
+    sum2 |= working_array[idx];
   });
 
   ASSERT_EQ(static_cast<DATA_TYPE>(sum.get()), ref_sum);
@@ -63,7 +59,7 @@ void ForallReduceBitAndSanityTest(RAJA::Index_type first, RAJA::Index_type last)
 
   for (int j = 0; j < nloops; ++j) {
     RAJA::forall<EXEC_POLICY>(r1, [=] RAJA_HOST_DEVICE(RAJA::Index_type idx) {
-      sum &= working_array[idx];
+      sum |= working_array[idx];
     });
   }
 
@@ -76,20 +72,29 @@ void ForallReduceBitAndSanityTest(RAJA::Index_type first, RAJA::Index_type last)
                                       test_array);
 }
 
+TYPED_TEST_SUITE_P(ForallReduceBitOrBitwiseTest);
+template <typename T>
+class ForallReduceBitOrBitwiseTest : public ::testing::Test
+{
+};
 
-TYPED_TEST_P(ForallReduceSanityTestBit, ReduceBitAndSanityForall)
+
+TYPED_TEST_P(ForallReduceBitOrBitwiseTest, ReduceBitOrBitwiseForall)
 {
   using DATA_TYPE     = typename camp::at<TypeParam, camp::num<0>>::type;
   using WORKING_RES   = typename camp::at<TypeParam, camp::num<1>>::type;
   using EXEC_POLICY   = typename camp::at<TypeParam, camp::num<2>>::type;
   using REDUCE_POLICY = typename camp::at<TypeParam, camp::num<3>>::type;
 
-  ForallReduceBitAndSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitOrBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(0, 28);
-  ForallReduceBitAndSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitOrBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(3, 642);
-  ForallReduceBitAndSanityTest<DATA_TYPE, WORKING_RES, 
+  ForallReduceBitOrBitwiseTestImpl<DATA_TYPE, WORKING_RES, 
                             EXEC_POLICY, REDUCE_POLICY>(0, 2057);
 }
 
-#endif  // __TEST_FORALL_REDUCEBITOR_SANITY_HPP__
+REGISTER_TYPED_TEST_SUITE_P(ForallReduceBitOrBitwiseTest,
+                            ReduceBitOrBitwiseForall);
+
+#endif  // __TEST_FORALL_BITWISE_REDUCEBITOR_HPP__
