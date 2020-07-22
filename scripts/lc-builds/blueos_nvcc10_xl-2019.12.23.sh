bd7e2cddd2fba6e3285cbfedba89f028ad35d07a
Rich Hornung <hornung1@llnl.gov>
2020-07-22 14:34:58 -0700

    Update build scripts: cull out some old ones, add new ones, update nvcc version in some...

--- scripts/lc-builds/blueos_nvcc10_xl-2019.12.23.sh
+++ scripts/lc-builds/blueos_nvcc10_xl-2019.12.23.sh
@@ -17,11 +17,11 @@ module load cmake/3.14.5
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_COMPILER=/usr/tce/packages/xl/xl-2019.12.23/bin/xlc++_r \
-  -C ../host-configs/lc-builds/blueos/nvcc_xl_2019_X.cmake \
+  -C ../host-configs/lc-builds/blueos/nvcc_xl_X.cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_CUDA=On \
-  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-10.1.243 \
-  -DCMAKE_CUDA_COMPILER=/usr/tce/packages/cuda/cuda-10.1.243/bin/nvcc \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-10.2.89 \
+  -DCMAKE_CUDA_COMPILER=/usr/tce/packages/cuda/cuda-10.2.89/bin/nvcc \
   -DCUDA_ARCH=sm_70 \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
--- scripts/lc-builds/blueos_xl-2020.06.25_omptarget.sh
+++ scripts/lc-builds/blueos_xl-2020.06.25_omptarget.sh
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-BUILD_SUFFIX=lc_blueos-xl_2019.04.19
+BUILD_SUFFIX=lc_blueos-xl_2020.06.25_omptarget
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
@@ -16,9 +16,11 @@ module load cmake/3.14.5
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_COMPILER=/usr/tce/packages/xl/xl-2019.04.19/bin/xlc++_r \
-  -C ../host-configs/lc-builds/blueos/xl_2019_X.cmake \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/xl/xl-2020.06.25/bin/xlc++_r \
+  -C ../host-configs/lc-builds/blueos/xl_X.cmake \
   -DENABLE_OPENMP=On \
+  -DENABLE_TARGET_OPENMP=On \
+  -DOpenMP_CXX_FLAGS="-qoffload;-qsmp=omp;-qnoeh;-qalias=noansi" \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
   ..
