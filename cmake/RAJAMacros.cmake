2631fe151765df2c49913f95e9d59565e10b87dc
JohnHynes <johnthehynes@gmail.com>
2020-08-11 16:51:47 -0400

    Changes to CMake

--- cmake/RAJAMacros.cmake
+++ cmake/RAJAMacros.cmake
@@ -49,6 +49,40 @@ macro(raja_add_executable)
     )
 endmacro(raja_add_executable)
 
+macro(raja_add_library)
+  set(options )
+  set(singleValueArgs NAME SHARED)
+  set(multiValueArgs SOURCES DEPENDS_ON)
+
+  cmake_parse_arguments(arg
+    "${options}" "${singleValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  list (APPEND arg_DEPENDS_ON RAJA)
+
+  if (ENABLE_OPENMP)
+    list (APPEND arg_DEPENDS_ON openmp)
+  endif ()
+
+  if (ENABLE_CUDA)
+    list (APPEND arg_DEPENDS_ON cuda)
+  endif ()
+
+  if (ENABLE_HIP)
+    list (APPEND arg_DEPENDS_ON hip)
+  endif ()
+
+  if (ENABLE_TBB)
+    list (APPEND arg_DEPENDS_ON tbb)
+  endif ()
+
+  blt_add_library(
+    NAME ${arg_NAME}
+    SOURCES ${arg_SOURCES}
+    DEPENDS_ON ${arg_DEPENDS_ON}
+    SHARED ${arg_SHARED}
+    )
+endmacro(raja_add_library)
+
 macro(raja_add_test)
   set(options )
   set(singleValueArgs NAME)
