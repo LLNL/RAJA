839f18f1c4dc364995496e5b40d389caca56b50a
Jason Burmark <burmark1@llnl.gov>
2020-08-25 14:58:06 -0700

    Make hip scipt and hostconfig more like the others

--- scripts/lc-builds/toss3_hipcc3.6.0.sh
+++ scripts/lc-builds/toss3_hipcc3.6.0.sh
@@ -15,7 +15,6 @@ mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
 
 module load cmake/3.14.5
 
-
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DHIP_ROOT_DIR="/opt/rocm-3.6.0/hip" \
@@ -23,6 +22,9 @@ cmake \
   -DCMAKE_C_COMPILER=/opt/rocm-3.6.0/llvm/bin/clang \
   -DCMAKE_CXX_COMPILER=/opt/rocm-3.6.0/llvm/bin/clang++ \
   -C ../host-configs/lc-builds/toss3/hip.cmake \
+  -DENABLE_HIP=ON \
+  -DENABLE_OPENMP=OFF \
+  -DENABLE_CUDA=OFF \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
   ..
