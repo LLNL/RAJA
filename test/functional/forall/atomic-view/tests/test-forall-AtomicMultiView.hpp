d0f872ffede9b448e3a43063768972b9b771c51b
Robert Chen <chen59@llnl.gov>
2020-10-26 17:54:19 -0700

    Tests for MultiView with atomic accesses.

---
+++ test/functional/forall/atomic-view/tests/test-forall-AtomicMultiView.hpp
@@ -0,0 +1,120 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+///
+/// Header file containing basic functional tests for atomic operations with forall and views.
+///
+
+#ifndef __TEST_FORALL_ATOMIC_MULTIVIEW_HPP__
+#define __TEST_FORALL_ATOMIC_MULTIVIEW_HPP__
+
+template <typename ExecPolicy,
+          typename AtomicPolicy,
+          typename WORKINGRES,
+          typename IdxType,
+          typename T>
+void ForallAtomicMultiViewTestImpl( IdxType N )
+{
+  RAJA::TypedRangeSegment<IdxType> seg(0, N);
+  RAJA::TypedRangeSegment<IdxType> seg_hundred(0, 100);
+  RAJA::TypedRangeSegment<IdxType> seg_twohundred(0, 200);
+
+  camp::resources::Resource work_res{WORKINGRES()};
+  camp::resources::Resource host_res{camp::resources::Host()};
+
+  T * actualsource = work_res.allocate<T>(N);
+  T * actualdest = work_res.allocate<T>(N/2);
+  T * check_array = host_res.allocate<T>(N/2);
+
+  T ** source = new T * [200];
+  for ( int ii = 0; ii < 200; ++ii )
+  {
+    source[ii] = actualsource+(ii*200);
+  }
+
+  T ** dest = new T * [100];
+  for ( int ii = 0; ii < 100; ++ii )
+  {
+    dest[ii] = actualdest+(ii*100);
+  }
+
+#if defined(RAJA_ENABLE_CUDA)
+  cudaErrchk(cudaDeviceSynchronize());
+#endif
+
+#if defined(RAJA_ENABLE_HIP)
+  hipErrchk(hipDeviceSynchronize());
+#endif
+
+  RAJA::forall<RAJA::seq_exec>(seg,
+                               [=](IdxType i) { actualsource[i] = (T)1; });
+
+  // use atomic add to reduce the array
+  // 1D defaut MultiView
+  RAJA::MultiView<T, RAJA::Layout<1>> vec_view(source, N);
+
+  // 1D MultiView with array-of-pointers index in 1st position
+  RAJA::MultiView<T, RAJA::Layout<1>, 1> sum_view(dest, N);
+  auto sum_atomic_view = RAJA::make_atomic_view<AtomicPolicy>(sum_view);
+
+
+  // Zero out dest using atomic MultiView
+  RAJA::forall<ExecPolicy>(seg_hundred, [=] RAJA_HOST_DEVICE(IdxType i) {
+    for ( int aopidx = 0; aopidx < 100; ++aopidx )
+    {
+      sum_atomic_view(i,aopidx) = (T)0;
+    }
+  });
+
+  // Assign values to dest using atomic MultiView
+  RAJA::forall<ExecPolicy>(seg_twohundred, [=] RAJA_HOST_DEVICE(IdxType i) {
+    for ( int aopidx = 0; aopidx < 100; ++aopidx )
+    {
+      sum_atomic_view(i / 2, aopidx) += vec_view(aopidx,i);
+    }
+  });
+
+  work_res.memcpy( check_array, actualdest, sizeof(T) * N/2 );
+
+#if defined(RAJA_ENABLE_CUDA)
+  cudaErrchk(cudaDeviceSynchronize());
+#endif
+
+#if defined(RAJA_ENABLE_HIP)
+  hipErrchk(hipDeviceSynchronize());
+#endif
+
+  for (IdxType i = 0; i < N / 2; ++i) {
+    EXPECT_EQ((T)2, check_array[i]);
+  }
+
+  work_res.deallocate( actualsource );
+  work_res.deallocate( actualdest );
+  host_res.deallocate( check_array );
+}
+
+TYPED_TEST_SUITE_P(ForallAtomicMultiViewTest);
+template <typename T>
+class ForallAtomicMultiViewTest : public ::testing::Test
+{
+};
+
+TYPED_TEST_P(ForallAtomicMultiViewTest, AtomicMultiViewForall)
+{
+  using AExec   = typename camp::at<TypeParam, camp::num<0>>::type;
+  using APol    = typename camp::at<TypeParam, camp::num<1>>::type;
+  using ResType = typename camp::at<TypeParam, camp::num<2>>::type;
+  using IdxType = typename camp::at<TypeParam, camp::num<3>>::type;
+  using DType   = typename camp::at<TypeParam, camp::num<4>>::type;
+
+  ForallAtomicMultiViewTestImpl<AExec, APol, ResType, IdxType, DType>( 20000 );
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ForallAtomicMultiViewTest,
+                            AtomicMultiViewForall);
+
+#endif  //__TEST_FORALL_ATOMIC_MULTIVIEW_HPP__
