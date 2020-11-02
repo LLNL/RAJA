7f8aebb6b0fb8d3f0c4799e5acc13dffed9c7142
Peter B. Robinson <robinson96@llnl.gov>
2020-11-02 11:34:23 -0800

    fix gcc4.9-debug build.

--- include/RAJA/util/Registry.hpp
+++ include/RAJA/util/Registry.hpp
@@ -25,7 +25,7 @@ namespace util {
 
     const std::string& getName() const { return Name; }
     const std::string& getDesc() const { return Desc; }
-    T * const get() const { return object.get(); }
+    T * get() const { return object.get(); }
   };
 
   /// A global registry used in conjunction with static constructors to make
