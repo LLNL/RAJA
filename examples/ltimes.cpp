9e441aa9f16b46018d450dc30208991ddab82bcd
Adam J. Kunen <kunen1@llnl.gov>
2020-11-17 14:49:45 -0800

    Cleanup

--- examples/ltimes.cpp
+++ examples/ltimes.cpp
@@ -14,31 +14,31 @@
 
 #include "RAJA/config.hpp"
 
-#define VARIANT_C                    0
-#define VARIANT_C_VIEWS              0
-#define VARIANT_RAJA_SEQ             0
-#define VARIANT_RAJA_SEQ_ARGS        0
-#define VARIANT_RAJA_TEAMS_SEQ       0
-#define VARIANT_RAJA_VECTOR          0
-#define VARIANT_RAJA_MATRIX          0
-#define VARIANT_RAJA_TEAMS_MATRIX    0
-#define VARIANT_RAJA_SEQ_SHMEM       0
-#define VARIANT_RAJA_MATRIX_SHMEM    0
+#define VARIANT_C                    1
+#define VARIANT_C_VIEWS              1
+#define VARIANT_RAJA_SEQ             1
+#define VARIANT_RAJA_SEQ_ARGS        1
+#define VARIANT_RAJA_TEAMS_SEQ       1
+#define VARIANT_RAJA_VECTOR          1
+#define VARIANT_RAJA_MATRIX          1
+#define VARIANT_RAJA_TEAMS_MATRIX    1
+#define VARIANT_RAJA_SEQ_SHMEM       1
+#define VARIANT_RAJA_MATRIX_SHMEM    1
 
 #if defined(RAJA_ENABLE_OPENMP)
-#define VARIANT_RAJA_OPENMP          0
+#define VARIANT_RAJA_OPENMP          1
 #endif
 
 #if defined(RAJA_ENABLE_CUDA)
-#define VARIANT_CUDA_KERNEL          0
-#define VARIANT_CUDA_TEAMS           0
+#define VARIANT_CUDA_KERNEL          1
+#define VARIANT_CUDA_TEAMS           1
 #define VARIANT_CUDA_TEAMS_MATRIX    1
-#define VARIANT_CUDA_KERNEL_SHMEM    0
+#define VARIANT_CUDA_KERNEL_SHMEM    1
 #endif
 
 #if defined(RAJA_ENABLE_HIP)
-#define RAJA_HIP_KERNEL              0
-#define RAJA_HIP_KERNEL_SHMEM        0
+#define RAJA_HIP_KERNEL              1
+#define RAJA_HIP_KERNEL_SHMEM        1
 #endif
 
 
@@ -125,7 +125,7 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 // Define array dimensions, allocate arrays, define Layouts and Views, etc.
   // Note: rand()/RAND_MAX is always zero, but forces the compiler to not
   // optimize out these values as compile time constants
-  const int num_m = 32 + (rand()/RAND_MAX);
+  const int num_m = 25 + (rand()/RAND_MAX);
   const int num_g = 160 + (rand()/RAND_MAX);
   const int num_d = 80 + (rand()/RAND_MAX);
 
