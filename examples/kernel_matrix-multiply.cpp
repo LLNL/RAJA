370f7e29539f1539e8199946a323b0432ea34628
Arturo Vargas <vargas45@llnl.gov>
2020-12-16 13:04:06 -0800

    fix kernel mat mat example compilation issues

--- examples/kernel_matrix-multiply.cpp
+++ examples/kernel_matrix-multiply.cpp
@@ -1,4 +1,4 @@
-1;95;0c//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Copyright (c) 2016-20, Lawrence Livermore National Security, LLC
 // and RAJA project contributors. See the RAJA/COPYRIGHT file for details.
 //
