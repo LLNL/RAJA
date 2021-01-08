b8f57adf7724c5a0763fffe918c1885bfa5c7149
Arturo Vargas <vargas45@llnl.gov>
2021-01-08 10:28:53 -0800

    build fixes for basics on teams file

--- examples/tut_teams_basic.cpp
+++ examples/tut_teams_basic.cpp
@@ -15,23 +15,41 @@
  *  Developing with RAJA Teams
  *
  *  This example serves as a basic overview of
- *  capabilities with the RAJA Teams API.  
+ *  capabilities with the RAJA Teams API.
  *
  *  RAJA features shown:
  *    -  RAJA::expt::launch
  */
 
 /*
- * Define host/device execution space in which 
- * to launch kernel. 
+ * The RAJA teams framework enables developers
+ * to expressed algorithms in terms of nested
+ * loops within an execution space. RAJA teams
+ * enables run time selection of a host or
+ * device execution space. As a starting point
+ * the example below choses a sequential
+ * execution space and either a CUDA or HIP
+ * execution device execution space.
 */
-using host_launch = RAJA::expt::seq_launch_t;
-#if defined(RAJA_DEVICE_ACTIVE)
-using device_launch = 
 
-using exec_space = RAJA::expt:LaunchPolicy<
-  RAJA::expt::seq_launch
->
+// __host_launch_start
+using host_launch = RAJA::expt::seq_launch_t;
+// __host_launch_end
+
+#if defined(RAJA_ENABLE_CUDA)
+// __host_device_start
+using device_launch = RAJA::expt::cuda_launch_t<false>;
+// __host_device_end
+#elif defined(RAJA_ENABLE_HIP)
+using device_launch = RAJA::expt::cuda_launch_t<false>;
+#endif
+
+using exec_space = RAJA::expt::LaunchPolicy<
+  host_launch
+#if defined(RAJA_DEVICE_ACTIVE)
+  ,device_launch
+#endif
+  >;
 
 
 int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
