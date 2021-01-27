09c6fa3b533ec083ef630df0a662d1fa904e35ed
mdavis36 <davis291@llnl.gov>
2021-01-27 11:28:48 -0800

    Making nested loop type information more generic so it can be used with more than the basic tests.

--- test/functional/kernel/nested-loop/tests/test-kernel-nested-loop-Basic.hpp
+++ test/functional/kernel/nested-loop/tests/test-kernel-nested-loop-Basic.hpp
@@ -8,14 +8,26 @@
 #ifndef __TEST_KERNEL_NESTED_LOOP_BASIC_HPP__
 #define __TEST_KERNEL_NESTED_LOOP_BASIC_HPP__
 
-struct DEPTH_2 {};
-struct DEPTH_3 {};
-
 #include <numeric>
 
-template <typename DEPTH, typename WORKING_RES, typename EXEC_POLICY>
+//
+//
+// Defining Nested Loop test type data, we probably want to define this in a header somewhere.
+//
+//
+struct DEPTH_2 {};
+struct DEPTH_2_COLLAPSE {};
+struct DEPTH_3 {};
+
+
+template <typename LOOP_DEPTH, typename WORKING_RES, typename EXEC_POLICY>
 struct KernelNestedLoopTest{ KernelNestedLoopTest(){} };
 
+//
+//
+// Depth 2 Nested Loop Test
+//
+//
 template <typename WORKING_RES, typename EXEC_POLICY>
 struct KernelNestedLoopTest<DEPTH_2, WORKING_RES, EXEC_POLICY>{
 
@@ -61,6 +73,26 @@ struct KernelNestedLoopTest<DEPTH_2, WORKING_RES, EXEC_POLICY>{
 
 };
 
+
+//
+//
+// Depth 2 Collapse Test just calls Depth 2 test
+//
+//
+template <typename WORKING_RES, typename EXEC_POLICY>
+struct KernelNestedLoopTest<DEPTH_2_COLLAPSE, WORKING_RES, EXEC_POLICY>{
+  template <typename... ExtraArgs>
+  KernelNestedLoopTest(const RAJA::Index_type dim0, const RAJA::Index_type dim1, ExtraArgs... Args){
+    KernelNestedLoopTest<DEPTH_2, WORKING_RES, EXEC_POLICY>(dim0,dim1, Args...);
+  }
+};
+
+
+//
+//
+// Depth 3 Nested Loop Test
+//
+//
 template <typename WORKING_RES, typename EXEC_POLICY>
 struct KernelNestedLoopTest<DEPTH_3, WORKING_RES, EXEC_POLICY>{
 
@@ -106,6 +138,58 @@ struct KernelNestedLoopTest<DEPTH_3, WORKING_RES, EXEC_POLICY>{
 
 };
 
+
+//
+//
+// Defining the Kernel Loop structure for Basic Nested Loop Tests.
+//
+//
+template<typename POLICY_TYPE, typename POLICY_DATA>
+struct BasicNestedLoopExec {};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_3, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::For<2, typename camp::at<POLICY_DATA, camp::num<1>>::type,
+        RAJA::statement::For<1, typename camp::at<POLICY_DATA, camp::num<2>>::type,
+          RAJA::statement::For<0, typename camp::at<POLICY_DATA, camp::num<3>>::type,
+            RAJA::statement::Lambda<0>
+          >
+        >
+      >
+    >;
+};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_2, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::For<1, typename camp::at<POLICY_DATA, camp::num<1>>::type,
+        RAJA::statement::For<0, typename camp::at<POLICY_DATA, camp::num<2>>::type,
+          RAJA::statement::Lambda<0>
+        >
+      >
+    >;
+};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_2_COLLAPSE, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::Collapse< typename camp::at<POLICY_DATA, camp::num<1>>::type,
+        RAJA::ArgList<1,0>,
+        RAJA::statement::Lambda<0>
+      >
+    >;
+};
+
+
+//
+//
+// Setup the Nested Loop Basic g-tests.
+//
+//
 TYPED_TEST_SUITE_P(KernelNestedLoopBasicTest);
 template <typename T>
 class KernelNestedLoopBasicTest : public ::testing::Test {};
@@ -114,12 +198,15 @@ TYPED_TEST_P(KernelNestedLoopBasicTest, NestedLoopBasicKernel) {
   using WORKING_RES = typename camp::at<TypeParam, camp::num<0>>::type;
   using EXEC_POL_DATA = typename camp::at<TypeParam, camp::num<1>>::type;
 
-  using KERNEL_DEPTH = typename camp::at<EXEC_POL_DATA, camp::num<0>>::type;
-  using EXEC_POLICY  = typename camp::at<EXEC_POL_DATA, camp::num<1>>::type;
+  // Attain the loop depth type from execpol data.
+  using LOOP_DEPTH = typename camp::at<EXEC_POL_DATA, camp::num<0>>::type;
+
+  // Build proper basic kernel exec policy type.
+  using EXEC_POLICY = typename BasicNestedLoopExec<LOOP_DEPTH, EXEC_POL_DATA>::type;
 
   // For double nested loop tests the third arg is ignored.
-  KernelNestedLoopTest<KERNEL_DEPTH, WORKING_RES, EXEC_POLICY>(1,1,1);
-  KernelNestedLoopTest<KERNEL_DEPTH, WORKING_RES, EXEC_POLICY>(40,30,20);
+  KernelNestedLoopTest<LOOP_DEPTH, WORKING_RES, EXEC_POLICY>(1,1,1);
+  KernelNestedLoopTest<LOOP_DEPTH, WORKING_RES, EXEC_POLICY>(40,30,20);
 }
 
 REGISTER_TYPED_TEST_SUITE_P(KernelNestedLoopBasicTest,
