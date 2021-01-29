20cad3069024a9e217dc5b8e09af2a2924211bf0
Rich Hornung <hornung1@llnl.gov>
2021-01-29 13:47:37 -0800

    Clean up and improved documentation of list segment class.

--- include/RAJA/index/ListSegment.hpp
+++ include/RAJA/index/ListSegment.hpp
@@ -1,9 +1,9 @@
 /*!
  ******************************************************************************
  *
- * \file
+ * \file ListSegment.hpp
  *
- * \brief   RAJA header file defining list segment classes.
+ * \brief  Header file containing definition of RAJA list segment class.
  *
  ******************************************************************************
  */
@@ -37,43 +37,85 @@ namespace RAJA
 /*!
  ******************************************************************************
  *
- * \brief  Class representing an arbitrary collection of indices.
+ * \class ListSegment
  *
- *         Length indicates number of indices in index array.
- *         Traversal executes as:
- *            for (i = 0; i < getLength(); ++i) {
- *               expression using m_indx[i] as array index.
- *            }
+ * \brief  Segment class representing an arbitrary collection of indices.
+ *
+ * \tparam StorageT underlying data type for the segment indices (required)
+ *
+ * A TypedRangeSegment models an Iterable interface:
+ *
+ *  begin() -- returns a StorageT*
+ *  end() -- returns a StorageT*
+ *  size() -- returns size of the Segment iteration space (RAJA::Index_type)
+ *
+ * Usage:
+ *
+ * A common C-style loop traversal pattern using an indirection array would be:
+ *
+ * \verbatim
+ * const T* indices = ...; 
+ * for (T i = begin; i < end; ++i) {
+ *   // loop body -- use indices[i] as index value
+ * }
+ * \endverbatim
+ *
+ * A TypedListSegment would be used with a RAJA forall execution template as:
+ * 
+ * \verbatim
+ * camp::resources::Resource resource{ camp resource type };
+ * TypedListSegment<T> listseg(indices, length, resource);
+ *
+ * forall<exec_pol>(listseg, [=] (T i) {
+ *   // loop body -- use i as index value
+ * });
+ * \endverbatim
+ *
+ * NOTE: TypedListSegment supports the option for the segment to own the 
+ *       its index data or simply use the index array passed to the constructor.
+ *       Owning the index data is the default; an array is created in the 
+ *       memory space specified by the camp resource object and the values are
+ *       copied from the input array to that. Ownership of the indices is 
+ *       determined by an optional ownership enum value passed to the 
+ *       constructor.
  *
  ******************************************************************************
  */
-template <typename T>
+template <typename StorageT>
 class TypedListSegment
 {
 public:
-  //! value type for storage
-  using value_type = T;
 
-  //! iterator type for storage (will be a pointer)
-  using iterator = T*;
+  //@{
+  //!   @name Types used in implementation based on template parameter.
+ 
+  //! The underlying value type for index storage
+  using value_type = StorageT;
 
-  //! expose underlying index type
-  using IndexType = T;
+  //! The underlying terator type
+  using iterator = StorageT*;
 
-  //! prevent compiler from providing a default constructor
-  TypedListSegment() = delete;
+  //! Expose underlying index type for consistency with other segment types
+  using IndexType = StorageT;
 
-  ///
-  /// \brief Construct list segment from given array with specified length
-  ///        and use given camp resource to allocate list segment index data
-  ///        if owned by this list segment.
-  ///
-  /// By default the ctor performs a deep copy of array elements.
-  ///
-  /// If 'Unowned' is passed as last argument, the constructed object
-  /// does not own the segment data and will hold a pointer to given
-  /// array's data. In this case, caller must manage object lifetimes properly.
-  ///
+  //@}
+
+  //@{
+  //!   @name Constructors and destructor.
+
+  /*!
+   * \brief Construct a list segment from given array with specified length
+   *        and use given camp resource to allocate list segment index data
+   *        if owned by this list segment.
+   * 
+   * \param values array of indices defining iteration space of segment
+   * \param length number of indices
+   * \param resource camp resource defining memory space where index data live
+   * \param owned optional enum value indicating whether segment owns indices (Owned or Unowned). Default is Owned.   
+   *
+   * If 'Unowned' is passed as last argument, the segment will not own its
+   * index data. In this case, caller must manage array lifetime properly.
+   */
   TypedListSegment(const value_type* values,
                    Index_type length,
                    camp::resources::Resource& resource,
@@ -83,19 +125,24 @@ public:
     initIndexData(values, length, owned);
   }
 
-  ///
-  /// Construct list segment from arbitrary object holding
-  /// indices using a deep copy of given data.
-  ///
-  /// The object must provide methods: begin(), end(), size().
-  ///
+  /*!
+   * \brief Construct a list segment from given container of indices.
+   *
+   * \param container container of indices for segment
+   * \param resource camp resource defining memory space where index data live
+   *
+   * The given container must provide methods begin(), end(), and size(). The
+   * segment constructor will make a copy of the container's index data in
+   * the memory space defined by the resource argument.
+   *
+   * Constructor assumes container data lives in host memory space.
+   */
   template <typename Container>
   TypedListSegment(const Container& container,
                    camp::resources::Resource& resource)
     : m_resource(resource),
       m_owned(Unowned), m_data(nullptr), m_size(container.size())
   {
-
     if (m_size > 0) {
 
       camp::resources::Resource host_res{camp::resources::Host()};
@@ -120,9 +167,10 @@ public:
     }
   }
 
-  ///
-  /// Copy-constructor for list segment.
-  ///
+  //! Disable compiler generated constructor
+  TypedListSegment() = delete;
+
+  //! Copy constructor for list segment
   TypedListSegment(const TypedListSegment& other)
     : m_resource(other.m_resource),
       m_owned(Unowned), m_data(nullptr), m_size(0)
@@ -131,9 +179,7 @@ public:
     initIndexData(other.m_data, other.m_size, other.m_owned, from_copy_ctor);
   }
 
-  ///
-  /// Move-constructor for list segment.
-  ///
+  //! Move constructor for list segment
   TypedListSegment(TypedListSegment&& rhs)
     : m_resource(rhs.m_resource),
       m_owned(rhs.m_owned), m_data(rhs.m_data), m_size(rhs.m_size)
@@ -142,9 +188,7 @@ public:
     rhs.m_owned = Unowned;
   }
 
-  ///
-  /// Destroy segment including its contents
-  ///
+  //! List segment destructor
   ~TypedListSegment()
   {
     if (m_data != nullptr && m_owned == Owned) {
@@ -152,32 +196,48 @@ public:
     }
   }
 
+  //@}
 
-  ///
-  /// Swap function for copy-and-swap idiom.
-  ///
-  RAJA_HOST_DEVICE void swap(TypedListSegment& other)
-  {
-    camp::safe_swap(m_resource, other.m_resource);
-    camp::safe_swap(m_data, other.m_data);
-    camp::safe_swap(m_size, other.m_size);
-    camp::safe_swap(m_owned, other.m_owned);
-  }
+  //@{
+  //!   @name Accessor methods
 
-  //! accessor to get the end iterator for a TypedListSegment
-  RAJA_HOST_DEVICE iterator end() const { return m_data + m_size; }
-
-  //! accessor to get the begin iterator for a TypedListSegment
+  /*!
+   * \brief Get iterator to the beginning of this segment
+   */
   RAJA_HOST_DEVICE iterator begin() const { return m_data; }
 
-  //! accessor to retrieve the total number of elements in a TypedListSegment
+  /*!
+   * \brief Get iterator to the end of this segment
+   */
+  RAJA_HOST_DEVICE iterator end() const { return m_data + m_size; }
+
+  /*!
+   * \brief Get size of this segment (number of indices)
+   */
   RAJA_HOST_DEVICE Index_type size() const { return m_size; }
 
-  //! get ownership of the data (Owned/Unowned)
+  /*!
+   * \brief Get ownership of index data (Owned/Unowned)
+   */
   RAJA_HOST_DEVICE IndexOwnership getIndexOwnership() const { return m_owned; }
 
-  //! checks a pointer and size (Span) for equality to all elements in the
-  //! TypedListSegment
+  //@}
+
+  //@{
+  //!   @name Segment comparison methods
+
+  /*!
+   * \brief Compare this segment's indices to an array of values
+   *
+   * \param container pointer to array of values
+   * \param len number of values to compare
+   *
+   * \return true if segment size is same as given length value and values in
+   *         given array match segment index values, else false
+   *
+   * Method assumes values in given array and segment indices both live in host 
+   * memory space.
+   */
   RAJA_HOST_DEVICE bool indicesEqual(const value_type* container,
                                      Index_type len) const
   {
@@ -189,26 +249,48 @@ public:
     return true;
   }
 
-  ///
-  /// Equality operator returns true if segments are equal; else false.
-  ///
+  /*!
+   * \brief Compare this segment to another for equality
+   *
+   * \return true if both segments are the same size and indices match, 
+   *         else false
+   * 
+   * Method assumes indices in both segments live in host memory space.
+   */
   RAJA_HOST_DEVICE bool operator==(const TypedListSegment& other) const
   {
     return (indicesEqual(other.m_data, other.m_size));
   }
 
-  ///
-  /// Inequality operator returns true if segments are not equal, else false.
-  ///
+  /*!
+   * \brief Compare this segment to another for inequality
+   *
+   * \return true if segments are not the same size or indices do not match, 
+   *         else false
+   * 
+   * Method assumes indices in both segments live in host memory space.
+   */
   RAJA_HOST_DEVICE bool operator!=(const TypedListSegment& other) const
   {
     return (!(*this == other));
   }
 
+  //@}
+
+  /*!
+   * \brief Swap this segment with another
+   */
+  RAJA_HOST_DEVICE void swap(TypedListSegment& other)
+  {
+    camp::safe_swap(m_resource, other.m_resource);
+    camp::safe_swap(m_data, other.m_data);
+    camp::safe_swap(m_size, other.m_size);
+    camp::safe_swap(m_owned, other.m_owned);
+  }
+
 private:
   //
-  // Initialize segment data properly based on whether object
-  // owns the index data.
+  // Initialize segment data based on whether object owns the index data.
   //
   void initIndexData(const value_type* container,
                      Index_type len,
@@ -263,17 +345,17 @@ private:
   // Copy of camp resource passed to ctor
   camp::resources::Resource m_resource;
 
-  // ownership flag to guide data copying/management
+  // Ownership flag to guide data copying/management
   IndexOwnership m_owned;
 
-  // buffer storage for list data
+  // Buffer storage for segment index data
   value_type* RAJA_RESTRICT m_data;
 
-  // size of list segment
+  // Size of list segment
   Index_type m_size;
 };
 
-//! alias for A TypedListSegment with storage type @Index_type
+//! Alias for A TypedListSegment<Index_type>
 using ListSegment = TypedListSegment<Index_type>;
 
 }  // namespace RAJA
@@ -281,12 +363,10 @@ using ListSegment = TypedListSegment<Index_type>;
 namespace std
 {
 
-/*!
- *  Specialization of std::swap for TypedListSegment
- */
-template <typename T>
-RAJA_INLINE void swap(RAJA::TypedListSegment<T>& a,
-                      RAJA::TypedListSegment<T>& b)
+//! Specialization of std::swap for TypedListSegment
+template <typename StorageT>
+RAJA_INLINE void swap(RAJA::TypedListSegment<StorageT>& a,
+                      RAJA::TypedListSegment<StorageT>& b)
 {
   a.swap(b);
 }
