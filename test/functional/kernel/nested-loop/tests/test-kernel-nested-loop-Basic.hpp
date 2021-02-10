364a2a43159f8a2402a89bed47b31c4e351a0ee1
mdavis36 <davis291@llnl.gov>
2021-02-10 11:44:03 -0800

    Adding some basic nested loop CUDA tests.

--- test/functional/kernel/nested-loop/tests/test-kernel-nested-loop-Basic.hpp
+++ test/functional/kernel/nested-loop/tests/test-kernel-nested-loop-Basic.hpp
@@ -18,126 +18,106 @@
 struct DEPTH_2 {};
 struct DEPTH_2_COLLAPSE {};
 struct DEPTH_3 {};
+struct OFFLOAD {};
 
 
-template <typename LOOP_DEPTH, typename WORKING_RES, typename EXEC_POLICY>
-struct KernelNestedLoopTest{ KernelNestedLoopTest(){} };
+template <typename WORKING_RES, typename EXEC_POLICY, typename... ExtraArgs>
+void KernelNestedLoopTest(const DEPTH_2&,
+                          const RAJA::Index_type dim0,
+                          const RAJA::Index_type dim1,
+                          ExtraArgs...){
+  camp::resources::Resource work_res{WORKING_RES::get_default()};
 
-//
-//
-// Depth 2 Nested Loop Test
-//
-//
+  RAJA::Index_type flatSize = dim0 * dim1;
+  RAJA::Index_type* work_array;
+  RAJA::Index_type* check_array;
+  RAJA::Index_type* test_array;
+
+  allocateForallTestData<RAJA::Index_type>(flatSize,
+                                     work_res,
+                                     &work_array,
+                                     &check_array,
+                                     &test_array);
+
+  RAJA::TypedRangeSegment<RAJA::Index_type> rangeflat(0,flatSize);
+  RAJA::TypedRangeSegment<RAJA::Index_type> range0(0, dim0);
+  RAJA::TypedRangeSegment<RAJA::Index_type> range1(0, dim1);
+
+  std::iota(test_array, test_array + RAJA::stripIndexType(flatSize), 0);
+
+  constexpr int Depth = 2;
+  RAJA::View< RAJA::Index_type, RAJA::Layout<Depth> > work_view(work_array, dim1, dim0);
+
+  RAJA::kernel<EXEC_POLICY>(RAJA::make_tuple(range1, range0),
+                            [=] RAJA_HOST_DEVICE (RAJA::Index_type j, RAJA::Index_type i) {
+                              work_view(j,i) = (j * dim0) + i;
+                            });
+
+  work_res.memcpy(check_array, work_array, sizeof(RAJA::Index_type) * RAJA::stripIndexType(flatSize));
+  RAJA::forall<RAJA::seq_exec>(rangeflat, [=] (RAJA::Index_type i) {
+    ASSERT_EQ(test_array[RAJA::stripIndexType(i)], check_array[RAJA::stripIndexType(i)]);
+  });
+
+  deallocateForallTestData<RAJA::Index_type>(work_res,
+                                       work_array,
+                                       check_array,
+                                       test_array);
+}
+
+template <typename WORKING_RES, typename EXEC_POLICY, typename... Args>
+void KernelNestedLoopTest(const DEPTH_2_COLLAPSE&, Args... args){
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY>(DEPTH_2(), args...);
+}
+
+template <typename WORKING_RES, typename EXEC_POLICY, typename... Args>
+void KernelNestedLoopTest(const OFFLOAD&, Args... args){
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY>(DEPTH_2(), args...);
+}
+
+
 template <typename WORKING_RES, typename EXEC_POLICY>
-struct KernelNestedLoopTest<DEPTH_2, WORKING_RES, EXEC_POLICY>{
+void KernelNestedLoopTest(const DEPTH_3&,
+                          const RAJA::Index_type dim0,
+                          const RAJA::Index_type dim1,
+                          const RAJA::Index_type dim2){
+  camp::resources::Resource work_res{WORKING_RES::get_default()};
 
-  template <typename... ExtraArgs>
-  KernelNestedLoopTest(const RAJA::Index_type dim0, const RAJA::Index_type dim1, ExtraArgs...){
-    camp::resources::Resource work_res{WORKING_RES::get_default()};
+  RAJA::Index_type flatSize = dim0 * dim1 * dim2;
+  RAJA::Index_type* work_array;
+  RAJA::Index_type* check_array;
+  RAJA::Index_type* test_array;
 
-    RAJA::Index_type flatSize = dim0 * dim1;
-    RAJA::Index_type* work_array;
-    RAJA::Index_type* check_array;
-    RAJA::Index_type* test_array;
+  allocateForallTestData<RAJA::Index_type>(flatSize,
+                                     work_res,
+                                     &work_array,
+                                     &check_array,
+                                     &test_array);
 
-    allocateForallTestData<RAJA::Index_type>(flatSize,
-                                       work_res,
-                                       &work_array,
-                                       &check_array,
-                                       &test_array);
+  RAJA::TypedRangeSegment<RAJA::Index_type> rangeflat(0,flatSize);
+  RAJA::TypedRangeSegment<RAJA::Index_type> range0(0, dim0);
+  RAJA::TypedRangeSegment<RAJA::Index_type> range1(0, dim1);
+  RAJA::TypedRangeSegment<RAJA::Index_type> range2(0, dim2);
 
-    RAJA::TypedRangeSegment<RAJA::Index_type> rangeflat(0,flatSize);
-    RAJA::TypedRangeSegment<RAJA::Index_type> range0(0, dim0);
-    RAJA::TypedRangeSegment<RAJA::Index_type> range1(0, dim1);
+  std::iota(test_array, test_array + RAJA::stripIndexType(flatSize), 0);
 
-    std::iota(test_array, test_array + RAJA::stripIndexType(flatSize), 0);
+  constexpr int Depth = 3;
+  RAJA::View< RAJA::Index_type, RAJA::Layout<Depth> > work_view(work_array, dim2, dim1, dim0);
 
-    constexpr int Depth = 2;
-    RAJA::View< RAJA::Index_type, RAJA::Layout<Depth> > work_view(work_array, dim1, dim0);
+  RAJA::kernel<EXEC_POLICY>(RAJA::make_tuple(range2, range1, range0),
+                            [=] RAJA_HOST_DEVICE (RAJA::Index_type k, RAJA::Index_type j, RAJA::Index_type i) {
+                              work_view(k,j,i) = (dim0 * dim1 * k) + (dim0 * j) + i;
+                            });
 
-    RAJA::kernel<EXEC_POLICY>(RAJA::make_tuple(range1, range0),
-                              [=] RAJA_HOST_DEVICE (RAJA::Index_type j, RAJA::Index_type i) {
-                                work_view(j,i) = (j * dim0) + i;
-                              });
+  work_res.memcpy(check_array, work_array, sizeof(RAJA::Index_type) * RAJA::stripIndexType(flatSize));
+  RAJA::forall<RAJA::seq_exec>(rangeflat, [=] (RAJA::Index_type i) {
+    ASSERT_EQ(test_array[RAJA::stripIndexType(i)], check_array[RAJA::stripIndexType(i)]);
+  });
 
-    work_res.memcpy(check_array, work_array, sizeof(RAJA::Index_type) * RAJA::stripIndexType(flatSize));
-    RAJA::forall<RAJA::seq_exec>(rangeflat, [=] (RAJA::Index_type i) {
-      ASSERT_EQ(test_array[RAJA::stripIndexType(i)], check_array[RAJA::stripIndexType(i)]);
-    });
-
-    deallocateForallTestData<RAJA::Index_type>(work_res,
-                                         work_array,
-                                         check_array,
-                                         test_array);
-  }
-
-};
-
-
-//
-//
-// Depth 2 Collapse Test just calls Depth 2 test
-//
-//
-template <typename WORKING_RES, typename EXEC_POLICY>
-struct KernelNestedLoopTest<DEPTH_2_COLLAPSE, WORKING_RES, EXEC_POLICY>{
-  template <typename... ExtraArgs>
-  KernelNestedLoopTest(const RAJA::Index_type dim0, const RAJA::Index_type dim1, ExtraArgs... Args){
-    KernelNestedLoopTest<DEPTH_2, WORKING_RES, EXEC_POLICY>(dim0,dim1, Args...);
-  }
-};
-
-
-//
-//
-// Depth 3 Nested Loop Test
-//
-//
-template <typename WORKING_RES, typename EXEC_POLICY>
-struct KernelNestedLoopTest<DEPTH_3, WORKING_RES, EXEC_POLICY>{
-
-  KernelNestedLoopTest(const RAJA::Index_type dim0, const RAJA::Index_type dim1, const RAJA::Index_type dim2){
-    camp::resources::Resource work_res{WORKING_RES::get_default()};
-
-    RAJA::Index_type flatSize = dim0 * dim1 * dim2;
-    RAJA::Index_type* work_array;
-    RAJA::Index_type* check_array;
-    RAJA::Index_type* test_array;
-
-    allocateForallTestData<RAJA::Index_type>(flatSize,
-                                       work_res,
-                                       &work_array,
-                                       &check_array,
-                                       &test_array);
-
-    RAJA::TypedRangeSegment<RAJA::Index_type> rangeflat(0,flatSize);
-    RAJA::TypedRangeSegment<RAJA::Index_type> range0(0, dim0);
-    RAJA::TypedRangeSegment<RAJA::Index_type> range1(0, dim1);
-    RAJA::TypedRangeSegment<RAJA::Index_type> range2(0, dim2);
-
-    std::iota(test_array, test_array + RAJA::stripIndexType(flatSize), 0);
-
-    constexpr int Depth = 3;
-    RAJA::View< RAJA::Index_type, RAJA::Layout<Depth> > work_view(work_array, dim2, dim1, dim0);
-
-    RAJA::kernel<EXEC_POLICY>(RAJA::make_tuple(range2, range1, range0),
-                              [=] RAJA_HOST_DEVICE (RAJA::Index_type k, RAJA::Index_type j, RAJA::Index_type i) {
-                                work_view(k,j,i) = (dim0 * dim1 * k) + (dim0 * j) + i;
-                              });
-
-    work_res.memcpy(check_array, work_array, sizeof(RAJA::Index_type) * RAJA::stripIndexType(flatSize));
-    RAJA::forall<RAJA::seq_exec>(rangeflat, [=] (RAJA::Index_type i) {
-      ASSERT_EQ(test_array[RAJA::stripIndexType(i)], check_array[RAJA::stripIndexType(i)]);
-    });
-
-    deallocateForallTestData<RAJA::Index_type>(work_res,
-                                         work_array,
-                                         check_array,
-                                         test_array);
-  }
-
-};
-
+  deallocateForallTestData<RAJA::Index_type>(work_res,
+                                       work_array,
+                                       check_array,
+                                       test_array);
+}
 
 //
 //
@@ -184,6 +164,24 @@ struct BasicNestedLoopExec<DEPTH_2_COLLAPSE, POLICY_DATA> {
     >;
 };
 
+#if defined(RAJA_ENABLE_CUDA)
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<OFFLOAD, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::CudaKernel<
+        RAJA::statement::For<1, typename camp::at<POLICY_DATA, camp::num<1>>::type,  // row
+          RAJA::statement::For<0, typename camp::at<POLICY_DATA, camp::num<2>>::type,  // col
+            RAJA::statement::Lambda<0>
+          >
+        >
+      > // end CudaKernel
+    >;
+};
+
+#endif  // RAJA_ENABLE_CUDA
+
 
 //
 //
@@ -199,14 +197,14 @@ TYPED_TEST_P(KernelNestedLoopBasicTest, NestedLoopBasicKernel) {
   using EXEC_POL_DATA = typename camp::at<TypeParam, camp::num<1>>::type;
 
   // Attain the loop depth type from execpol data.
-  using LOOP_DEPTH = typename camp::at<EXEC_POL_DATA, camp::num<0>>::type;
+  using POLICY_TYPE = typename camp::at<EXEC_POL_DATA, camp::num<0>>::type;
 
   // Build proper basic kernel exec policy type.
-  using EXEC_POLICY = typename BasicNestedLoopExec<LOOP_DEPTH, EXEC_POL_DATA>::type;
+  using EXEC_POLICY = typename BasicNestedLoopExec<POLICY_TYPE, EXEC_POL_DATA>::type;
 
   // For double nested loop tests the third arg is ignored.
-  KernelNestedLoopTest<LOOP_DEPTH, WORKING_RES, EXEC_POLICY>(1,1,1);
-  KernelNestedLoopTest<LOOP_DEPTH, WORKING_RES, EXEC_POLICY>(40,30,20);
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY>( POLICY_TYPE(), 1,1,1);
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY>( POLICY_TYPE(), 40,30,20);
 }
 
 REGISTER_TYPED_TEST_SUITE_P(KernelNestedLoopBasicTest,
