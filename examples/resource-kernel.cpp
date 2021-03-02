fa6d28cda4068051e139da8039d35a1f57d57661
mdavis36 <davis291@llnl.gov>
2021-03-02 14:26:49 -0800

    Adding Multiple Stream kernel example.

--- examples/resource-kernel.cpp
+++ examples/resource-kernel.cpp
@@ -117,6 +117,7 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 
 
 #if defined(RAJA_ENABLE_CUDA)
+#if 1
 {
   std::cout << "\n Running RAJA CUDA version of LTimes...\n";
 
@@ -216,7 +217,80 @@ int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
 #endif
 
 //----------------------------------------------------------------------------//
+{
+  std::cout << "\n Running RAJA Resource Kernel on Multiple Streams...\n";
 
+  constexpr int N = 10;
+  constexpr int M = 1000000;
+
+  RAJA::resources::Cuda def_cuda_res{RAJA::resources::Cuda::get_default()};
+  RAJA::resources::Host def_host_res{RAJA::resources::Host::get_default()};
+  int* d_array = def_cuda_res.allocate<int>(N*M);
+  int* h_array = def_host_res.allocate<int>(N*M);
+
+  RAJA::RangeSegment one_range(0, 1);
+  RAJA::RangeSegment m_range(0, M);
+  RAJA::RangeSegment n_range(0, N);
+
+  using TEST_POL =
+    RAJA::KernelPolicy<
+      statement::CudaKernelAsync<
+        statement::For<0, cuda_block_x_loop,
+          statement::For<1, cuda_thread_x_loop,
+            statement::Lambda<0>
+          >
+        >
+      >
+    >;
+
+  RAJA::forall<RAJA::loop_exec>(def_host_res, n_range,
+    [=, &def_cuda_res](int i){
+      RAJA::resources::Cuda res_cuda; 
+
+      RAJA::resources::Event e = RAJA::kernel_resource<TEST_POL>(
+        RAJA::make_tuple(one_range,
+                         m_range),
+
+        res_cuda,
+
+        [=] RAJA_DEVICE (int k, int j) {
+          d_array[i*M + j] = i * M + j;  
+        }
+      );
+
+      def_cuda_res.wait_for(&e);
+    }
+  );
+
+
+  def_cuda_res.memcpy(h_array, d_array, sizeof(int) * N * M);
+
+  using CHECK_POL =
+    RAJA::KernelPolicy<
+      statement::For<0, loop_exec,
+        statement::For<1, loop_exec,
+          statement::Lambda<0>
+        >
+      >
+    >;
+
+  int ec_count = 0;
+  RAJA::kernel<CHECK_POL>(
+    RAJA::make_tuple(n_range, m_range),
+    [=, &ec_count](int i, int j){
+      if (h_array[i*M+j] != i*M+j)ec_count++;
+    }
+  );
+
+
+  if (ec_count > 0) 
+    std::cout << "FAIL : error count = " << ec_count << "\n";
+  else
+    std::cout << "PASS!\n";
+
+
+}
+#endif
   std::cout << "\n DONE!...\n";
 
   return 0;
