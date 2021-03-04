0ec7da3fbd36752d28f325d287da2dcb30da5db1
Robert Chen <chen59@llnl.gov>
2021-03-04 11:46:20 -0800

    Shift XL GitLab flag logic.

--- scripts/uberenv/packages/raja/package.py
+++ scripts/uberenv/packages/raja/package.py
@@ -225,12 +225,6 @@ class Raja(CMakePackage, CudaPackage):
                 cfg.write(cmake_cache_entry("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
                 "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
 
-        if ("%xl" in spec) and ("+cuda" not in spec):
-            xl_release_flags = "-qthreaded -std=c++14 -O3 -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
-            xl_reldebinf_flags = "-qthreaded -std=c++14 -O3 -g -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
-            xl_debug_flags = "-qthreaded -std=c++14 -O0 -g -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
-            cfg.write(cmake_cache_string("CMAKE_CXX_FLAGS", xl_release_flags))
-
         if "+cuda" in spec:
             cfg.write("#------------------{0}\n".format("-" * 60))
             cfg.write("# Cuda\n")
@@ -259,6 +253,11 @@ class Raja(CMakePackage, CudaPackage):
 
         else:
             cfg.write(cmake_cache_option("ENABLE_CUDA", False))
+            if ("%xl" in spec):
+                xl_release_flags = "-qthreaded -std=c++14 -O3 -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
+                xl_reldebinf_flags = "-qthreaded -std=c++14 -O3 -g -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
+                xl_debug_flags = "-qthreaded -std=c++14 -O0 -g -qxlcompatmacros -qlanglvl=extended0x -qalias=noansi -qhot -qpic -qsmp=omp -std=c++11"
+                cfg.write(cmake_cache_string("CMAKE_CXX_FLAGS", xl_release_flags))
 
         cfg.write("#------------------{0}\n".format("-" * 60))
         cfg.write("# Other\n")
