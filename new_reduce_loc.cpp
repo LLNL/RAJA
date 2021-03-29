eff8f3fc20b990bc3a9ba1d44b84df4074a12af6
mdavis36 <davis291@llnl.gov>
2021-03-29 14:32:12 -0700

    Min and Max ReduceLoc impl.

--- new_reduce_loc.cpp
+++ new_reduce_loc.cpp
@@ -5,24 +5,28 @@
 #include "new_reduce/reduce_basic.hpp"
 #include "new_reduce/forall_param.hpp"
 
-template<typename T>
+template<typename T, bool min = true>
 class ValLoc {
-  T val;
+  T val = min ? RAJA::operators::limits<T>::max() : RAJA::operators::limits<T>::min();
   RAJA::Index_type loc;
 public:
 
-  ValLoc() : val(RAJA::operators::limits<T>::min()), loc(-1) {}
+  ValLoc() : loc(-1) {}
   ValLoc(T v) : val(v), loc(-1) {}
   ValLoc(T v, RAJA::Index_type l) : val(v), loc(l) {}
 
-  ValLoc operator () (T v, RAJA::Index_type l) { val = v; loc = l; return *this; }
-  //ValLoc operator () (T v, RAJA::Index_type l) { if (v < val) { val = v; loc = l; } return *this; }
-  //ValLoc operator () (T v, RAJA::Index_type l) { if (v > val) { val = v; loc = l; } return *this; }
+  ValLoc constexpr operator () (T v, RAJA::Index_type l) {
+    if (min) 
+      {if (v < val) { val = v; loc = l; } } 
+    else
+      {if (v > val) { val = v; loc = l; } }
+    return *this;
+  }
 
-  bool operator < (const ValLoc& rhs) const { return val < rhs.val; }
-  bool operator <=(const ValLoc& rhs) const { return val < rhs.val; }
-  bool operator > (const ValLoc& rhs) const { return val > rhs.val; }
-  bool operator >=(const ValLoc& rhs) const { return val > rhs.val; }
+  bool constexpr operator < (const ValLoc& rhs) const { return val < rhs.val; }
+  bool constexpr operator <=(const ValLoc& rhs) const { return val < rhs.val; }
+  bool constexpr operator > (const ValLoc& rhs) const { return val > rhs.val; }
+  bool constexpr operator >=(const ValLoc& rhs) const { return val > rhs.val; }
 
   T getVal() {return val;}
   RAJA::Index_type getLoc() {return loc;}
@@ -31,6 +35,13 @@ public:
 template<typename T>
 ValLoc<T>& make_valloc(T v, RAJA::Index_type l) { return ValLoc<T>(v, l); }
 
+template<typename T>
+using ValLocMin = ValLoc<T, true>;
+
+template<typename T>
+using ValLocMax = ValLoc<T, false>;
+
+
 namespace RAJA
 {
 
@@ -38,14 +49,25 @@ namespace operators
 {
 
 template <typename T>
-struct limits<ValLoc<T>> {
-  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLoc<T> min()
+struct limits<ValLocMin<T>> {
+  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLocMin<T> min()
   {
-    return ValLoc<T>(RAJA::operators::limits<T>::min());
+    return ValLocMin<T>(RAJA::operators::limits<T>::min());
   }
-  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLoc<T> max()
+  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLocMin<T> max()
   {
-    return ValLoc<T>(RAJA::operators::limits<T>::max());
+    return ValLocMin<T>(RAJA::operators::limits<T>::max());
+  }
+};
+template <typename T>
+struct limits<ValLocMax<T>> {
+  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLocMax<T> min()
+  {
+    return ValLocMax<T>(RAJA::operators::limits<T>::min());
+  }
+  RAJA_INLINE RAJA_HOST_DEVICE static constexpr ValLocMax<T> max()
+  {
+    return ValLocMax<T>(RAJA::operators::limits<T>::max());
   }
 };
 }}
@@ -104,18 +126,23 @@ int main(int argc, char *argv[])
     t.reset();
     t.start();
 
-    ValLoc<double> mL;
+    ValLocMin<double> mL;
+    ValLocMax<double> ML;
 
     forall_param<RAJA::omp_parallel_for_exec>(N,
-                 [=](int i, ValLoc<double> &mL_) {
+                 [=](int i, ValLocMin<double> &mL_, ValLocMax<double> &ML_) {
                    mL_(a[i], i);
+                   ML_(a[i], i);
                  },
-                 Reduce<RAJA::operators::minimum>(&mL));
-                 //Reduce<RAJA::operators::maximum>(&mL));
+                 //ReduceLoc(&mL),
+                 //ReduceLoc(&ML));
+                 Reduce<RAJA::operators::minimum>(&mL),
+                 Reduce<RAJA::operators::maximum>(&ML));
     t.stop();
     
     std::cout << "t : " << t.elapsed() << "\n";
     std::cout << "mL : " << mL.getLoc() <<"\n";
+    std::cout << "ML : " << ML.getLoc() <<"\n";
   }
 #endif
 
