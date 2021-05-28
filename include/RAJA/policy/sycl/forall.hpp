0637d3996a8ca27fcebd7ea5e6a206101b037ea7
Brian Homerding <bhomerding@anl.gov>
2021-05-28 20:07:01 +0000

    Use Index_type

--- include/RAJA/policy/sycl/forall.hpp
+++ include/RAJA/policy/sycl/forall.hpp
@@ -191,7 +191,7 @@ RAJA_INLINE resources::EventProxy<resources::Sycl> forall_impl(resources::Sycl &
       h.parallel_for( cl::sycl::nd_range<1>{gridSize, blockSize},
                       [=]  (cl::sycl::nd_item<1> it) {
 
-        size_t ii = it.get_global_id(0);
+        Index_type ii = it.get_global_id(0);
 
         if (ii < len) {
           (*lbody)((*beg)[ii]);
