b374aaf76d80ca752ecacff7bc0bfd9428088d2f
Peter B. Robinson <robinson96@llnl.gov>
2021-09-02 11:04:22 -0700

    clean up comment.

--- include/RAJA/policy/hip.hpp
+++ include/RAJA/policy/hip.hpp
@@ -38,6 +38,6 @@
 #include "RAJA/policy/hip/WorkGroup.hpp"
 
 
-#endif  // closing endif for if defined(RAJA_ENABLE_HIP)
+#endif  // closing endif for if defined(RAJA_HIP_ACTIVE)
 
 #endif  // closing endif for header file include guard
