c81bdc16c67cfe15d666d7796b0949039bc961d8
mdavis36 <davis291@llnl.gov>
2021-10-12 10:57:08 -0700

    return new reduce value with a reduction combination operation.

--- include/RAJA/policy/openmp/new_reduce.hpp
+++ include/RAJA/policy/openmp/new_reduce.hpp
@@ -14,21 +14,21 @@ namespace detail {
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< type_traits::is_openmp_policy<EXEC_POL> >
   init(Reducer<OP, T>& red) {
-    red.val = Reducer<OP,T>::op::identity();
+    red.val = OP::identity();
   }
 
   // Combine
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< type_traits::is_openmp_policy<EXEC_POL> >
   combine(Reducer<OP, T>& out, const Reducer<OP, T>& in) {
-    out.val = typename Reducer<OP,T>::op{}(out.val, in.val);
+    out.val = OP{}(out.val, in.val);
   }
 
   // Resolve
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< type_traits::is_openmp_policy<EXEC_POL> >
   resolve(Reducer<OP, T>& red) {
-    *red.target = red.val;
+    *red.target = OP{}(red.val, *red.target);
   }
 
 #endif
--- include/RAJA/policy/sequential/new_reduce.hpp
+++ include/RAJA/policy/sequential/new_reduce.hpp
@@ -11,19 +11,19 @@ namespace detail {
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< std::is_same< EXEC_POL, RAJA::seq_exec> >
   init(Reducer<OP, T>& red) {
-    red.val = Reducer<OP,T>::op::identity();
+    red.val = OP::identity();
   }
   // Combine
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< std::is_same< EXEC_POL, RAJA::seq_exec> >
   combine(Reducer<OP, T>& out, const Reducer<OP, T>& in) {
-    out.val = typename Reducer<OP,T>::op{}(out.val, in.val);
+    out.val = OP{}(out.val, in.val);
   }
   // Resolve
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< std::is_same< EXEC_POL, RAJA::seq_exec> >
   resolve(Reducer<OP, T>& red) {
-    *red.target = red.val;
+    *red.target = OP{}(red.val, *red.target);
   }
 
 } //  namespace detail
