a30656b52286cf46c34714a55987221e88920fe6
Adam J. Kunen <kunen1@llnl.gov>
2021-11-12 09:01:19 -0800

    Starting to transition tensor tests to new framework

---
+++ test/functional/tensor/register/tests/test-tensor-register-SegmentedSumInner.hpp
@@ -0,0 +1,108 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-21, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#ifndef __TEST_TESNOR_REGISTER_SegmentedSumInner_HPP__
+#define __TEST_TESNOR_REGISTER_SegmentedSumInner_HPP__
+
+#include <numeric>
+#if 0
+
+template <typename EXEC_POLICY, typename WORKING_RES, typename OP_TYPE>
+void ScanExclusiveTestImpl(int N,
+                           typename OP_TYPE::result_type offset =
+                           OP_TYPE::identity())
+{
+  using T = typename OP_TYPE::result_type;
+
+  WORKING_RES res{WORKING_RES::get_default()};
+  camp::resources::Resource working_res{res};
+
+  T* work_in;
+  T* work_out;
+  T* host_in;
+  T* host_out;
+
+  allocScanTestData(N,
+                    working_res,
+                    &work_in, &work_out,
+                    &host_in, &host_out);
+
+  std::iota(host_in, host_in + N, 1);
+
+  // test interface without resource
+  res.memcpy(work_in, host_in, sizeof(T) * N);
+  res.wait();
+
+  RAJA::exclusive_scan<EXEC_POLICY>(RAJA::make_span(work_in, N),
+                                    RAJA::make_span(work_out, N),
+                                    OP_TYPE{},
+                                    offset);
+
+  res.memcpy(host_out, work_out, sizeof(T) * N);
+  res.wait();
+
+  ASSERT_TRUE(check_exclusive<OP_TYPE>(host_out, host_in, N, offset));
+
+  // test interface with resource
+  res.memcpy(work_in, host_in, sizeof(T) * N);
+
+  RAJA::exclusive_scan<EXEC_POLICY>(res,
+                                    RAJA::make_span(work_in, N),
+                                    RAJA::make_span(work_out, N),
+                                    OP_TYPE{},
+                                    offset);
+
+  res.memcpy(host_out, work_out, sizeof(T) * N);
+  res.wait();
+
+  ASSERT_TRUE(check_exclusive<OP_TYPE>(host_out, host_in, N, offset));
+
+  deallocScanTestData(working_res,
+                      work_in, work_out,
+                      host_in, host_out);
+}
+
+
+#endif
+
+TYPED_TEST_P(TestTensorRegister, SesgmentedSumInnerTest)
+{
+  printf("hello!\n");
+#if 0
+  using EXEC_POLICY      = typename camp::at<TypeParam, camp::num<0>>::type;
+  using WORKING_RESOURCE = typename camp::at<TypeParam, camp::num<1>>::type;
+  using OP_TYPE          = typename camp::at<TypeParam, camp::num<2>>::type;
+
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                              WORKING_RESOURCE,
+                              OP_TYPE>(0);
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                              WORKING_RESOURCE,
+                              OP_TYPE>(357);
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                              WORKING_RESOURCE,
+                              OP_TYPE>(32000);
+
+  //
+  // Perform some non-identity offset tests
+  //
+  using T = typename OP_TYPE::result_type;
+
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                        WORKING_RESOURCE,
+                        OP_TYPE>(0, T(13));
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                        WORKING_RESOURCE,
+                        OP_TYPE>(357, T(15));
+  ScanExclusiveTestImpl<EXEC_POLICY,
+                        WORKING_RESOURCE,
+                        OP_TYPE>(32000, T(2));
+#endif
+}
+
+
+#endif // __TEST_SCAN_EXCLUSIVE_HPP__
