6e7d30db78151dcd82b3a79b924f6a6faa07a1ca
mdavis36 <davis291@llnl.gov>
2021-11-12 10:53:53 -0800

    Adding 2 and 3 level collapse nested loop tests.

--- test/functional/kernel/nested-loop/tests/nested-loop-Basic-impl.hpp
+++ test/functional/kernel/nested-loop/tests/nested-loop-Basic-impl.hpp
@@ -35,6 +35,9 @@ using BasicSupportedLoopTypeList = camp::list<
   DEPTH_2,
   DEPTH_2_COLLAPSE,
   DEPTH_3,
+  DEPTH_3_COLLAPSE,
+  DEPTH_3_COLLAPSE_SEQ_INNER,
+  DEPTH_3_COLLAPSE_SEQ_OUTER,
   DEVICE_DEPTH_2>;
 
 //
@@ -146,6 +149,19 @@ void KernelNestedLoopTest(const DEPTH_3&,
                                        test_array);
 }
 
+// DEPTH_3_COLLAPSE and DEVICE_DEPTH_3 execution policies use the above DEPTH_3 test.
+template <typename WORKING_RES, typename EXEC_POLICY, bool USE_RESOURCE, typename... Args>
+void KernelNestedLoopTest(const DEPTH_3_COLLAPSE&, Args... args){
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY, USE_RESOURCE>(DEPTH_3(), args...);
+}
+template <typename WORKING_RES, typename EXEC_POLICY, bool USE_RESOURCE, typename... Args>
+void KernelNestedLoopTest(const DEPTH_3_COLLAPSE_SEQ_OUTER&, Args... args){
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY, USE_RESOURCE>(DEPTH_3(), args...);
+}
+template <typename WORKING_RES, typename EXEC_POLICY, bool USE_RESOURCE, typename... Args>
+void KernelNestedLoopTest(const DEPTH_3_COLLAPSE_SEQ_INNER&, Args... args){
+  KernelNestedLoopTest<WORKING_RES, EXEC_POLICY, USE_RESOURCE>(DEPTH_3(), args...);
+}
 //
 //
 // Defining the Kernel Loop structure for Basic Nested Loop Tests.
@@ -187,6 +203,43 @@ struct BasicNestedLoopExec<DEPTH_2_COLLAPSE, POLICY_DATA> {
       RAJA::statement::Collapse< typename camp::at<POLICY_DATA, camp::num<0>>::type,
         RAJA::ArgList<1,0>,
         RAJA::statement::Lambda<0>
+      >
+    >;
+};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_3_COLLAPSE, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::Collapse< typename camp::at<POLICY_DATA, camp::num<0>>::type,
+        RAJA::ArgList<0,1,2>,
+        RAJA::statement::Lambda<0>
+      >
+    >;
+};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_3_COLLAPSE_SEQ_OUTER, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::For<0, RAJA::seq_exec,
+      RAJA::statement::Collapse< typename camp::at<POLICY_DATA, camp::num<0>>::type,
+        RAJA::ArgList<1,2>,
+          RAJA::statement::Lambda<0>
+        >
+      >
+    >;
+};
+
+template<typename POLICY_DATA>
+struct BasicNestedLoopExec<DEPTH_3_COLLAPSE_SEQ_INNER, POLICY_DATA> {
+  using type = 
+    RAJA::KernelPolicy<
+      RAJA::statement::Collapse< typename camp::at<POLICY_DATA, camp::num<0>>::type,
+        RAJA::ArgList<0,1>,
+        RAJA::statement::For<2, RAJA::seq_exec,
+          RAJA::statement::Lambda<0>
+        >
       >
     >;
 };
