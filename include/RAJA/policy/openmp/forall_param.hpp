817423974ed7ff667d9560dcdc7eb3e1b8eea279
mdavis36 <davis291@llnl.gov>
2022-04-21 18:28:09 -0700

    Missing schedule(static) in one of the omp policies.

--- include/RAJA/policy/openmp/forall_param.hpp
+++ include/RAJA/policy/openmp/forall_param.hpp
@@ -82,7 +82,7 @@ namespace expt
       RAJA_OMP_DECLARE_REDUCTION_COMBINE;
 
       RAJA_EXTRACT_BED_IT(iter);
-      #pragma omp parallel for reduction(combine : f_params)
+      #pragma omp parallel for schedule(static) reduction(combine : f_params)
       for (decltype(distance_it) i = 0; i < distance_it; ++i) {
         RAJA::expt::invoke_body(f_params, loop_body, begin_it[i]);
       }
