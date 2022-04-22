f165b09c0d89cfc611223f2ab0bf3a5530fb0e79
Robert Chen <chen59@llnl.gov>
2022-04-21 19:29:25 -0700

    Build scripts for SYCL on Corona.

---
+++ host-configs/alcf-builds/corona_sycl.cmake
@@ -0,0 +1,32 @@
+#
+## Copyright (c) 2016-19, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-689114
+##
+## All rights reserved.
+##
+## This file is part of RAJA.
+##
+## For details about use and distribution, please read RAJA/LICENSE.
+##
+
+set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
+
+set(CMAKE_CXX_COMPILER "clang++" CACHE PATH "")
+#set(CMAKE_CXX_COMPILER "dpcpp" CACHE PATH "")
+
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+#set(CMAKE_CXX_FLAGS_RELEASE "-O3 -std=c++17 -fsycl -fsycl-unnamed-lambda --gcc-toolchain=/usr/tce/packages/gcc/gcc-7.1.0" CACHE STRING "")
+#set(CMAKE_CXX_FLAGS_RELWITHDEBINFO " -O3 -g  -std=c++17 -fsycl -fsycl-unnamed-lambda --gcc-toolchain=/usr/tce/packages/gcc/gcc-7.1.0" CACHE STRING "")
+#set(CMAKE_CXX_FLAGS_DEBUG " -O0 -g -std=c++17 -fsycl -fsycl-unnamed-lambda --gcc-toolchain=/usr/tce/packages/gcc/gcc-7.1.0" CACHE STRING "")
+#set(CMAKE_CXX_LINK_FLAGS "-fsycl -Wl,-rpath,/usr/tce/packages/oneapi/oneapi-2021.2/compiler/2021.2.0/linux/compiler/lib/intel64_lin/"  CACHE STRING "")
+
+set(RAJA_RANGE_ALIGN 4 CACHE INT "")
+set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
+set(RAJA_DATA_ALIGN 64 CACHE INT "")
+
+set(RAJA_HOST_CONFIG_LOADED On CACHE Bool "")
---
+++ scripts/alcf-builds/corona_sycl.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+##
+## Copyright (c) 2017-19, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read RAJAPerf/LICENSE.
+##
+
+BUILD_SUFFIX=corona-sycl
+: ${BUILD_TYPE:=RelWithDebInfo}
+RAJA_HOSTCONFIG=../host-configs/alcf-builds/corona_sycl.cmake
+
+rm -rf build_${BUILD_SUFFIX}_${USER} >/dev/null
+mkdir build_${BUILD_SUFFIX}_${USER} && cd build_${BUILD_SUFFIX}_${USER}
+
+DATE=$(printf '%(%Y-%m-%d)T\n' -1)
+
+## NOTE: RAJA tests are turned off due to compilation issues.
+
+cmake \
+  -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=Off \
+  -DENABLE_CUDA=Off \
+  -DRAJA_ENABLE_TARGET_OPENMP=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DRAJA_ENABLE_SYCL=On \
+  -DCMAKE_LINKER=clang++ \
+  -DCMAKE_CXX_STANDARD=17 \
+  -DENABLE_TESTS=Off \
+  -DENABLE_EXAMPLES=On \
+  "$@" \
+  ..
+
+make -j 8
+
+#-DCMAKE_INSTALL_PREFIX=/home/bhomerding/install/raja.${DATE} \
