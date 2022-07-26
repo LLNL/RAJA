4e4cab2202d680823a608a2577f4524d2ae51f09
Tom Scogland <scogland1@llnl.gov>
2022-07-26 10:14:27 -0700

    add spack workaround, update camp, load recent cmake

--- scripts/spack_packages/raja/package.py
+++ scripts/spack_packages/raja/package.py
@@ -6,8 +6,9 @@
 
 from spack import *
 
-import socket
+import glob
 import os
+import socket
 
 from os import environ as env
 from os.path import join as pjoin
@@ -106,6 +107,7 @@ class Raja(CMakePackage, CudaPackage, ROCmPackage):
                    when='cuda_arch={0}'.format(sm_))
 
     conflicts('+openmp', when='+rocm')
+    depends_on('rocprim', when='+rocm')
 
     phases = ['hostconfig', 'cmake', 'build', 'install']
 
@@ -309,6 +311,15 @@ class Raja(CMakePackage, CudaPackage, ROCmPackage):
             hip_arch = spec.variants['amdgpu_target'].value
             cfg.write(cmake_cache_entry("HIP_ROOT_DIR",
                                         hip_root))
+            # there is only one dir like this, but the version component is unknown
+            cfg.write(
+                cmake_cache_path(
+                    "HIP_CLANG_INCLUDE_PATH",
+                    glob.glob(
+                        "{}/lib/clang/*/include".format(spec['llvm-amdgpu'].prefix)
+                    )[0]
+                )
+            )
             cfg.write(cmake_cache_entry("ROCM_ROOT_DIR",
                                         rocm_root))
             cfg.write(cmake_cache_entry("HIP_PATH",
