da73ea8cad538001e650fcad48852190b5cd4324
Arturo Vargas <ac.vargas@jlselogin5.ftm.alcf.anl.gov>
2022-09-19 20:04:52 +0000

    fix sycl device touch

--- include/RAJA/policy/sycl/reduce.hpp
+++ include/RAJA/policy/sycl/reduce.hpp
@@ -219,7 +219,7 @@ struct TargetReduce
     return *this;
 #else
     auto i = 0;
-    Reducer{}(val.device[i], rhsVal);
+    Reducer{}(val.host[i], rhsVal);
     return *this;
 #endif
   }
@@ -234,7 +234,7 @@ struct TargetReduce
     return *this;
 #else
     auto i = 0;
-    Reducer{}(val.device[i], rhsVal);
+    Reducer{}(val.host[i], rhsVal);
     return *this;
 #endif
   }
