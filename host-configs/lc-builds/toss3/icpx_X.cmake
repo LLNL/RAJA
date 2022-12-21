905bea1522865806cddf1ffb3cac0c14422bad37
Rich Hornung <hornung1@llnl.gov>
2022-12-21 14:03:46 -0800

    Add intel oneapi build script that works on toss3 machines.
    disabling tests that generate a clang fron-end error with oneapi

--- host-configs/lc-builds/toss3/icpx_X.cmake
+++ host-configs/lc-builds/toss3/icpx_X.cmake
@@ -5,12 +5,12 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ###############################################################################
 
-set(CMAKE_CXX_FLAGS_RELEASE "${COMMON_FLAGS} -O3 -march=native -finline-functions" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${COMMON_FLAGS} -O3 -march=native -finline-functions --gcc-toolchain=/usr/tce/packages/gcc/gcc-8.1.0" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${COMMON_FLAGS} -O3 -g -march=native -finline-functions" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "${COMMON_FLAGS} -O0 -g" CACHE STRING "")
 
-set(RAJA_RANGE_ALIGN 4 CACHE INT "")
-set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
-set(RAJA_DATA_ALIGN 64 CACHE INT "")
+set(RAJA_RANGE_ALIGN 4 CACHE STRING "")
+set(RAJA_RANGE_MIN_LENGTH 32 STRING INT "")
+set(RAJA_DATA_ALIGN 64 CACHE STRING "")
 
 set(RAJA_HOST_CONFIG_LOADED On CACHE BOOL "")
