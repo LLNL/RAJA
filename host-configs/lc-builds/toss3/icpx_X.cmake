906649a966c6f3fe38b03a3eb3c12dd5f6804a8d
Rich Hornung <hornung1@llnl.gov>
2022-12-21 15:06:42 -0800

    point at gcc headers for all build types

--- host-configs/lc-builds/toss3/icpx_X.cmake
+++ host-configs/lc-builds/toss3/icpx_X.cmake
@@ -6,7 +6,7 @@
 ###############################################################################
 
 set(CMAKE_CXX_FLAGS_RELEASE "${COMMON_FLAGS} -O3 -march=native -finline-functions --gcc-toolchain=/usr/tce/packages/gcc/gcc-8.1.0" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${COMMON_FLAGS} -O3 -g -march=native -finline-functions" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_DEBUG "${COMMON_FLAGS} -O0 -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${COMMON_FLAGS} -O3 -g -march=native -finline-functions --gcc-toolchain=/usr/tce/packages/gcc/gcc-8.1.0" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "${COMMON_FLAGS} -O0 -g --gcc-toolchain=/usr/tce/packages/gcc/gcc-8.1.0" CACHE STRING "")
 
 set(RAJA_HOST_CONFIG_LOADED On CACHE BOOL "")
