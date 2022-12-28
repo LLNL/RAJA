0a2c1ac9f4d6b4a86b271777393f559e425efe55
Jason Burmark <burmark1@llnl.gov>
2022-12-28 10:38:35 -0800

    Use LaunchConfig in hip_exec in HipKernelExt
    Avoids problem with using void as we're now inheriting from
    the given class

--- include/RAJA/policy/hip/kernel/HipKernel.hpp
+++ include/RAJA/policy/hip/kernel/HipKernel.hpp
@@ -87,7 +87,7 @@ namespace statement
  */
 template <typename LaunchConfig, typename... EnclosedStmts>
 struct HipKernelExt
-    : public internal::Statement<::RAJA::policy::hip::hip_exec<void>, EnclosedStmts...> {
+    : public internal::Statement<::RAJA::policy::hip::hip_exec<LaunchConfig>, EnclosedStmts...> {
 };
 
 
