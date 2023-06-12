baaf718f295376f1d76fd37ee7be7df146f60c20
Arturo Vargas <vargas45@llnl.gov>
2023-06-12 10:16:24 -0700

    clean up pass

--- test/include/RAJA_test-launch-teams_threads_1D_execpol.hpp
+++ test/include/RAJA_test-launch-teams_threads_1D_execpol.hpp
@@ -9,81 +9,84 @@
 // Execution policy lists used throughout launch tests
 //
 
-#ifndef __RAJA_test_launch_teams_threads_1D_execpol_HPP__
-#define __RAJA_test_launch_teams_threads_1D_execpol_HPP__
+#ifndef __RAJA_TEST_LAUNCH_TEAMS_THREADS_1D_EXECPOL_HPP__
+#define __RAJA_TEST_LAUNCH_TEAMS_THREADS_1D_EXECPOL_HPP__
 
 #include "RAJA/RAJA.hpp"
 #include "camp/list.hpp"
 
 //Launch policies
-using seq_policies = camp::list<
-  RAJA::LaunchPolicy<RAJA::seq_launch_t>,
-  RAJA::LoopPolicy<RAJA::seq_exec>,
-  RAJA::LoopPolicy<RAJA::seq_exec>
-  >;
+using seq_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::seq_launch_t>,
+             RAJA::LoopPolicy<RAJA::seq_exec>,
+             RAJA::LoopPolicy<RAJA::seq_exec>
+            >;
 
-using Sequential_launch_policies = camp::list<
-  seq_policies
-  >;
+using Sequential_launch_policies = camp::list<seq_policies>;
 
 #if defined(RAJA_ENABLE_OPENMP)
-using omp_policies = camp::list<
-         RAJA::LaunchPolicy<RAJA::omp_launch_t>,
-         RAJA::LoopPolicy<RAJA::omp_for_exec>,
-         RAJA::LoopPolicy<RAJA::seq_exec>
-  >;
 
-using OpenMP_launch_policies = camp::list<
-  omp_policies
-  >;
+using omp_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::omp_launch_t>,
+             RAJA::LoopPolicy<RAJA::omp_for_exec>,
+             RAJA::LoopPolicy<RAJA::seq_exec>
+            >;
+
+using OpenMP_launch_policies = camp::list<omp_policies>;
 
 #endif  // RAJA_ENABLE_OPENMP
 
 #if defined(RAJA_ENABLE_CUDA)
 
-using cuda_policies = camp::list<
-  RAJA::LaunchPolicy<RAJA::cuda_launch_t<false>>,
-  RAJA::LoopPolicy<RAJA::cuda_block_x_direct>,
-  RAJA::LoopPolicy<RAJA::cuda_thread_x_direct>
-  >;
+using cuda_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::cuda_launch_t<false>>,
+             RAJA::LoopPolicy<RAJA::cuda_block_x_direct>,
+             RAJA::LoopPolicy<RAJA::cuda_thread_x_direct>
+            >;
 
+using cuda_explicit_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::policy::cuda::cuda_launch_explicit_t<true, 0, 0>>,
+             RAJA::LoopPolicy<RAJA::cuda_block_x_direct>,
+             RAJA::LoopPolicy<RAJA::cuda_thread_x_direct>
+            >;
 
-using cuda_explicit_policies = camp::list<
-  RAJA::LaunchPolicy<RAJA::policy::cuda::cuda_launch_explicit_t<true, 0, 0>>,
-  RAJA::LoopPolicy<RAJA::cuda_block_x_direct>,
-  RAJA::LoopPolicy<RAJA::cuda_thread_x_direct>
-  >;
+using Cuda_launch_policies =
+  camp::list<
+             cuda_policies,
+             cuda_explicit_policies
+            >;
 
-using Cuda_launch_policies = camp::list<
-  cuda_policies,
-  cuda_explicit_policies
-  >;
 #endif  // RAJA_ENABLE_CUDA
 
 #if defined(RAJA_ENABLE_HIP)
 
-using hip_policies = camp::list<
-  RAJA::LaunchPolicy<RAJA::hip_launch_t<true>>,
-  RAJA::LoopPolicy<RAJA::hip_block_x_direct>,
-  RAJA::LoopPolicy<RAJA::hip_thread_x_direct>
-  >;
+using hip_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::hip_launch_t<true>>,
+             RAJA::LoopPolicy<RAJA::hip_block_x_direct>,
+             RAJA::LoopPolicy<RAJA::hip_thread_x_direct>
+            >;
 
-using Hip_launch_policies = camp::list<
-      hip_policies
-       >;
+using Hip_launch_policies = camp::list<hip_policies>;
+
 #endif // RAJA_ENABLE_HIP
 
 #if defined(RAJA_ENABLE_SYCL)
-using sycl_direct_policies = camp::list<
-  RAJA::LaunchPolicy<RAJA::sycl_launch_t<true>>,
-  RAJA::LoopPolicy<RAJA::sycl_group_0_direct>,
-  RAJA::LoopPolicy<RAJA::sycl_local_0_direct>
-  >;
 
-using Sycl_launch_policies = camp::list<
-  sycl_direct_policies
-  >;
+using sycl_direct_policies =
+  camp::list<
+             RAJA::LaunchPolicy<RAJA::sycl_launch_t<true>>,
+             RAJA::LoopPolicy<RAJA::sycl_group_0_direct>,
+             RAJA::LoopPolicy<RAJA::sycl_local_0_direct>
+             >;
+
+using Sycl_launch_policies = camp::list<sycl_direct_policies>;
+
 #endif
 
 
-#endif  // __RAJA_test_launch_teams_threads_1D_execpol_HPP__
+#endif  // __RAJA_TEST_LAUNCH_TEAMS_THREADS_1D_EXECPOL_HPP__
