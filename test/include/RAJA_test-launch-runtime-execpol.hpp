107c7ffe359f7f4b4c8e0ed5cbdd13963be102bc
Arturo Vargas <vargas45@llnl.gov>
2023-06-12 11:48:48 -0700

    clean up pass

--- test/include/RAJA_test-launch-runtime-execpol.hpp
+++ test/include/RAJA_test-launch-runtime-execpol.hpp
@@ -17,12 +17,11 @@
 
 //Launch policies
 #if defined(RAJA_ENABLE_CUDA)
-
 using seq_cuda_policies =
   camp::list<
-             RAJA::LaunchPolicy<RAJA::seq_launch_t,RAJA::cuda_launch_t<true>>,
-             RAJA::LoopPolicy<RAJA::seq_exec, RAJA::cuda_block_x_direct>,
-             RAJA::LoopPolicy<RAJA::seq_exec,RAJA::cuda_thread_x_loop>
+              RAJA::LaunchPolicy<RAJA::seq_launch_t,RAJA::cuda_launch_t<true>>,
+              RAJA::LoopPolicy<RAJA::seq_exec, RAJA::cuda_block_x_direct>,
+              RAJA::LoopPolicy<RAJA::seq_exec,RAJA::cuda_thread_x_loop>
             >;
 
 using seq_cuda_explicit_policies =
@@ -48,26 +47,15 @@ using seq_hip_policies =
 
 using Sequential_launch_policies = camp::list<seq_hip_policies>;
 
-#elif defined(RAJA_ENABLE_SYCL)
-
-using seq_sycl_policies =
-  camp::list<
-             RAJA::LaunchPolicy<RAJA::seq_launch_t,RAJA::sycl_launch_t<true>>,
-             RAJA::LoopPolicy<RAJA::seq_exec, RAJA::sycl_group_0_direct>,
-             RAJA::LoopPolicy<RAJA::seq_exec,RAJA::sycl_local_0_loop>
-            >;
-
-using Sequential_launch_policies = camp::list<seq_sycl_policies>;
-
 #else
-
 using Sequential_launch_policies =
-        camp::list<
-                   RAJA::LaunchPolicy<RAJA::seq_launch_t>,
-                   RAJA::LoopPolicy<RAJA::seq_exec>,
-                   RAJA::LoopPolicy<RAJA::seq_exec>
-                  >;
-
+  camp::list<
+    camp::list<
+               RAJA::LaunchPolicy<RAJA::seq_launch_t>,
+               RAJA::LoopPolicy<RAJA::seq_exec>,
+               RAJA::LoopPolicy<RAJA::seq_exec>
+              >
+            >;
 #endif // Sequential
 
 
@@ -77,15 +65,15 @@ using Sequential_launch_policies =
 
 using omp_cuda_policies =
   camp::list<
-             RAJA::LaunchPolicy<RAJA::omp_launch_t,RAJA::cuda_launch_t<false>>,
-             RAJA::LoopPolicy<RAJA::omp_for_exec, RAJA::cuda_block_x_direct>,
-             RAJA::LoopPolicy<RAJA::seq_exec,RAJA::cuda_thread_x_loop>
+              RAJA::LaunchPolicy<RAJA::omp_launch_t,RAJA::cuda_launch_t<false>>,
+              RAJA::LoopPolicy<RAJA::omp_parallel_for_exec, RAJA::cuda_block_x_direct>,
+              RAJA::LoopPolicy<RAJA::seq_exec,RAJA::cuda_thread_x_loop>
             >;
 
 using omp_cuda_explicit_policies =
   camp::list<
              RAJA::LaunchPolicy<RAJA::omp_launch_t,RAJA::policy::cuda::cuda_launch_explicit_t<false, 0, 0>>,
-             RAJA::LoopPolicy<RAJA::omp_for_exec, RAJA::cuda_block_x_direct>,
+             RAJA::LoopPolicy<RAJA::omp_parallel_for_exec, RAJA::cuda_block_x_direct>,
              RAJA::LoopPolicy<RAJA::seq_exec,RAJA::cuda_thread_x_loop>
             >;
 
@@ -100,32 +88,22 @@ using OpenMP_launch_policies =
 using omp_hip_policies =
   camp::list<
              RAJA::LaunchPolicy<RAJA::omp_launch_t,RAJA::hip_launch_t<false>>,
-             RAJA::LoopPolicy<RAJA::omp_for_exec, RAJA::hip_block_x_direct>,
+             RAJA::LoopPolicy<RAJA::omp_parallel_for_exec, RAJA::hip_block_x_direct>,
              RAJA::LoopPolicy<RAJA::seq_exec,RAJA::hip_thread_x_loop>
             >;
 
 using OpenMP_launch_policies = camp::list<omp_hip_policies>;
 
-#elif defined(RAJA_ENABLE_SYCL)
-
-using omp_sycl_policies =
-  camp::list<
-             RAJA::LaunchPolicy<RAJA::omp_launch_t,RAJA::sycl_launch_t<false>>,
-             RAJA::LoopPolicy<RAJA::omp_for_exec, RAJA::sycl_group_0_direct>,
-             RAJA::LoopPolicy<RAJA::seq_exec,RAJA::sycl_local_0_loop>
-            >;
-
-using OpenMP_launch_policies = camp::list<omp_sycl_policies>;
-
 #else
 
 using OpenMP_launch_policies =
   camp::list<
-             RAJA::LaunchPolicy<RAJA::omp_launch_t>,
-             RAJA::LoopPolicy<RAJA::omp_for_exec>,
-             RAJA::LoopPolicy<RAJA::seq_exec>
-            >;
-
+    camp::list<
+                RAJA::LaunchPolicy<RAJA::omp_launch_t>,
+                RAJA::LoopPolicy<RAJA::omp_parallel_for_exec>,
+                RAJA::LoopPolicy<RAJA::seq_exec>
+               >
+             >;
 #endif
 
 #endif  // RAJA_ENABLE_OPENMP
@@ -136,33 +114,25 @@ using Cuda_launch_policies =
   camp::list<
              seq_cuda_policies
             ,seq_cuda_explicit_policies
+
 #if defined(RAJA_ENABLE_OPENMP)
             ,omp_cuda_policies
             ,omp_cuda_explicit_policies
 #endif
-          >;
 
+           >;
 #endif  // RAJA_ENABLE_CUDA
 
 #if defined(RAJA_ENABLE_HIP)
 
-using Hip_launch_policies = 
-  camp::list<
-             seq_hip_policies
-#if defined(RAJA_ENABLE_OPENMP)
-             ,omp_hip_policies
-#endif
-             >;
-#endif // RAJA_ENABLE_HIP
+using Hip_launch_policies = camp::list<
+         seq_hip_policies
 
-#if defined(RAJA_ENABLE_SYCL)
-using Sycl_launch_policies = 
-  camp::list<
-             seq_sycl_policies
 #if defined(RAJA_ENABLE_OPENMP)
-            ,omp_sycl_policies
+         , omp_hip_policies
 #endif
-            >;
-#endif // RAJA_ENABLE_SYCL
+        >;
+
+#endif // RAJA_ENABLE_HIP
 
 #endif  // __RAJA_TEST_LAUNCH_RUNTIME_EXECPOL_HPP__
