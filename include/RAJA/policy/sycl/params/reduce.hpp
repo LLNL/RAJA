d83c79fffc02bfa832b0b3647a4979f310aafb75
Brian Homerding <homerdin@uan-0002.head.cm.americas.sgi.com>
2023-08-29 13:28:51 +0000

    Remove debugging prints

--- include/RAJA/policy/sycl/params/reduce.hpp
+++ include/RAJA/policy/sycl/params/reduce.hpp
@@ -13,7 +13,6 @@ namespace detail {
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   init(Reducer<OP, T>& red) {
-//    std::cout << "BRIAN: setting to " << OP::identity() << '\n';
     red.val = OP::identity();
   }
 
@@ -22,17 +21,13 @@ namespace detail {
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   SYCL_EXTERNAL
   combine(Reducer<OP, T>& out, const Reducer<OP, T>& in) {
-//    std::cout << "BRIAN: combining " << out.val << " with " << in.val << '\n';
     out.val = OP{}(out.val, in.val);
-//    out.val = OP{}(in.val);
   }
 
   // Resolve
   template<typename EXEC_POL, typename OP, typename T>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   resolve(Reducer<OP, T>& red) {
-    
-    std::cout << "BRIAN: resolving " << red.val << " with " << *red.target << '\n';
     *red.target = OP{}(red.val, *red.target);
   }
 
