72b8b19962917321500d21f3b32267c1a44ffb47
Rich Hornung <hornung1@llnl.gov>
2024-07-19 14:56:47 -0700

    Add copyright header and include guard

--- include/RAJA/util/sycl_compat.hpp
+++ include/RAJA/util/sycl_compat.hpp
@@ -1,3 +1,23 @@
+/*!
+ ******************************************************************************
+ *
+ * \file
+ *
+ * \brief   RAJA header file for handling different SYCL header include paths
+ *
+ ******************************************************************************
+ */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-24, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#ifndef RAJA_util_sycl_compat_HPP
+#define RAJA_util_sycl_compat_HPP
+
 #if (__INTEL_CLANG_COMPILER && __INTEL_CLANG_COMPILER < 20230000)
 // older version, use legacy header locations
 #include <CL/sycl.hpp>
@@ -5,3 +25,5 @@
 // SYCL 2020 standard header
 #include <sycl/sycl.hpp>
 #endif
+
+#endif  // RAJA_util_sycl_compat_HPP
