bd5ec5f18e8cb19ab642ab5c80ff660eb58b765f
Robert Chen <chen59@llnl.gov>
2024-09-06 15:18:43 -0700

    Update SYCL backend for ValOp.

--- include/RAJA/policy/sycl/params/reduce.hpp
+++ include/RAJA/policy/sycl/params/reduce.hpp
@@ -10,26 +10,45 @@ namespace detail {
 #if defined(RAJA_ENABLE_SYCL)
 
   // Init
-  template<typename EXEC_POL, typename OP, typename T>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
-  init(Reducer<OP, T>& red) {
-    red.val = OP::identity();
+  init(Reducer<OP, T, ValOp<T,OP>>& red) {
+    red.val.val = OP<T,T,T>::identity();
   }
-
   // Combine
-  template<typename EXEC_POL, typename OP, typename T>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
-  SYCL_EXTERNAL
-  combine(Reducer<OP, T>& out, const Reducer<OP, T>& in) {
-    out.val = OP{}(out.val, in.val);
+  combine(Reducer<OP, T, ValOp<T,OP>>& out, const Reducer<OP, T, ValOp<T,OP>>& in) {
+    out.val.val = OP<T,T,T>{}(out.val.val, in.val.val);
+  }
+  // Resolve
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T>
+  camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
+  resolve(Reducer<OP, T, ValOp<T,OP>>& red) {
+    red.target->val = OP<T,T,T>{}(red.target->val, red.val.val);
   }
 
-  // Resolve
-  template<typename EXEC_POL, typename OP, typename T>
-  camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
-  resolve(Reducer<OP, T>& red) {
-    *red.target = OP{}(*red.target, red.val);
-  }
+//  // Init
+//  template<typename EXEC_POL, typename OP, typename T>
+//  camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
+//  init(Reducer<OP, T>& red) {
+//    red.val = OP::identity();
+//  }
+//
+//  // Combine
+//  template<typename EXEC_POL, typename OP, typename T>
+//  camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
+//  SYCL_EXTERNAL
+//  combine(Reducer<OP, T>& out, const Reducer<OP, T>& in) {
+//    out.val = OP{}(out.val, in.val);
+//  }
+//
+//  // Resolve
+//  template<typename EXEC_POL, typename OP, typename T>
+//  camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
+//  resolve(Reducer<OP, T>& red) {
+//    *red.target = OP{}(*red.target, red.val);
+//  }
 
 #endif
 
