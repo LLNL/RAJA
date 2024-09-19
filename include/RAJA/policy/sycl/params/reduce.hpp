1e13b59f370b2ac5d3c77fce244dcd68f53d5ab8
Robert Chen <chen59@llnl.gov>
2024-09-19 09:53:14 -0700

    Fix SYCL typo.

--- include/RAJA/policy/sycl/params/reduce.hpp
+++ include/RAJA/policy/sycl/params/reduce.hpp
@@ -10,21 +10,21 @@ namespace detail {
 #if defined(RAJA_ENABLE_SYCL)
 
   // Init
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   init(Reducer<OP, T, VType, D, I, true>& red) {
     red.valop_m.val = OP<T,T,T>::identity();
   }
 
   // Combine
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   combine(Reducer<OP, T, VType, D, I, true>& out, const Reducer<OP, T, VType, D, I, true>& in) {
     out.valop_m.val = OP<T,T,T>{}(out.valop_m.val, in.valop_m.val);
   }
 
   // Resolve
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   resolve(Reducer<OP, T, VType, D, I, true>& red) {
     *red.target = OP<T,T,T>{}(*red.target, red.valop_m.val);
@@ -33,21 +33,21 @@ namespace detail {
   }
 
   // Init
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   init(Reducer<OP, T, VType, D, I, false>& red) {
     red.valop_m.val = OP<T,T,T>::identity();
   }
 
   // Combine
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   combine(Reducer<OP, T, VType, D, I, false>& out, const Reducer<OP, T, VType, D, I, false>& in) {
     out.valop_m.val = OP<T,T,T>{}(out.valop_m.val, in.valop_m.val);
   }
 
   // Resolve
-  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I typename VType>
+  template<typename EXEC_POL, template <typename, typename, typename> class OP, typename T, typename D, typename I, typename VType>
   camp::concepts::enable_if< type_traits::is_sycl_policy<EXEC_POL> >
   resolve(Reducer<OP, T, VType, D, I, false>& red) {
     *red.target = OP<T,T,T>{}(*red.target, red.valop_m.val);
