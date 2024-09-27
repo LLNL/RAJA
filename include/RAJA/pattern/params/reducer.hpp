2be498934db22d5646128849c140a8e1fa180b70
Robert Chen <chen59@llnl.gov>
2024-09-26 17:06:27 -0700

    Simplify Reducer internals.

--- include/RAJA/pattern/params/reducer.hpp
+++ include/RAJA/pattern/params/reducer.hpp
@@ -56,50 +56,92 @@ namespace detail
   //
   //
 
-  // For the standard use case, T can be a basic data type or a ValLoc, and DataType is the same as T.
-  // For the pass through use case, T is a ValLoc, and DataType is the underlying basic data type within the ValLoc.
-  // By default, we expect the standard use case, which is why passthru is set to false.
-  template <template <typename, typename, typename> class Op, typename T, typename VType, typename DataType = T, typename IndexType = RAJA::Index_type, bool passthru = false>
+  // Basic data type Reducer
+  // T must be a basic data type
+  // IndexType not used here
+  // VType must be ValOp<T, Op>
+  template <template <typename, typename, typename> class Op, typename T, typename IndexType, typename VType>
   struct Reducer : public ForallParamBase {
     using op = Op<T,T,T>;
-    using value_type = T; // Can be basic data types, or ValLoc.
+    using value_type = T; // This is a basic data type
 
     Reducer() = default;
-    // Standard constructor
+
+    // Basic data type constructor
     RAJA_HOST_DEVICE Reducer(value_type *target_in) : valop_m(VType{}), target(target_in){}
-    // Pass through constructor
-    RAJA_HOST_DEVICE Reducer(DataType *data_in, IndexType *index_in) : valop_m(VType(*data_in, *index_in)), target(&valop_m.val), passthruval(data_in), passthruindex(index_in) {}
 
     Reducer(Reducer const &) = default;
     Reducer(Reducer &&) = default;
     Reducer& operator=(Reducer const &) = default;
     Reducer& operator=(Reducer &&) = default;
 
-    // VType should be a ValOp, and can accept basic data types or ValLoc's for T.
-    // Most internal reduction operations are performed on this.
-    // In the pass through use case, T is a ValLoc, and VType is ValOp<ValLoc>.
+    // Internal ValOp object that is used within RAJA::forall/launch
     VType valop_m = VType{};
 
-    // Points to the actual basic data type or ValLoc.
-    // This is set after valop_m has the final value.
-    // Note that in the pass through use case, target points to the ValLoc in valop_m.val, because the user did not provide a ValLoc target.
+    // Points to the user specified result variable
     value_type *target = nullptr;
 
-    // Used when the pass through ReduceLoc(*data, *index) is called.
-    // These point to the actual data and index being passed in.
-    // These are set after valop_m has the final value.
-    DataType *passthruval = nullptr;
-    IndexType *passthruindex = nullptr; 
+    // Used in the resolve() call to set target in each backend
+    RAJA_HOST_DEVICE void set(value_type in) { *target = in; }
 
-    template <typename U = VType, std::enable_if_t<std::is_same<U,ValOp<T,Op>>::value>* = nullptr>
     RAJA_HOST_DEVICE
     value_type &
     getVal() { return valop_m.val; }
 
-    template <typename U = VType, std::enable_if_t<std::is_same<U,ValOp<ValLoc<T,IndexType>,Op>>::value>* = nullptr>
+#if defined(RAJA_CUDA_ACTIVE) || defined(RAJA_HIP_ACTIVE) || defined(RAJA_SYCL_ACTIVE)
+    // Device related attributes.
+    value_type * devicetarget = nullptr;
+    RAJA::detail::SoAPtr<value_type, device_mem_pool_t> device_mem;
+    unsigned int * device_count = nullptr;
+#endif
+
+    // These are types and parameters extracted from this struct, and given to the forall.
+    using ARG_TUP_T = camp::tuple<VType*>;
+    RAJA_HOST_DEVICE ARG_TUP_T get_lambda_arg_tup() { return camp::make_tuple(&valop_m); }
+
+    using ARG_LIST_T = typename ARG_TUP_T::TList;
+    static constexpr size_t num_lambda_args = camp::tuple_size<ARG_TUP_T>::value ;
+  };
+
+  // Partial specialization of Reducer for ValLoc
+  // T must be a basic data type
+  // VType must be ValOp<ValLoc<T,IndexType>,Op>
+  template <template <typename, typename, typename> class Op, typename T, typename IndexType>
+  struct Reducer<Op, T, IndexType, ValOp<ValLoc<T,IndexType>, Op>> : public ForallParamBase {
+    using value_type = ValLoc<T,IndexType>; // Note that this struct operates on ValLoc
+    using op = Op<value_type,value_type,value_type>;
+    using VType = ValOp<ValLoc<T,IndexType>, Op>;
+
+    Reducer() = default;
+
+    // ValLoc constructor
+    // Note that the passthru variables point to the val and loc within the user defined target ValLoc
+    RAJA_HOST_DEVICE Reducer(value_type *target_in) : valop_m(VType{}), target(target_in), passthruval(&target_in->val), passthruindex(&target_in->loc) {}
+
+    // Pass through constructor for ReduceLoc<>(data, index) case
+    // The passthru variables point to vars defined by the user
+    RAJA_HOST_DEVICE Reducer(T *data_in, IndexType *index_in) : valop_m(VType(*data_in, *index_in)), target(&valop_m.val), passthruval(data_in), passthruindex(index_in) {}
+
+    Reducer(Reducer const &) = default;
+    Reducer(Reducer &&) = default;
+    Reducer& operator=(Reducer const &) = default;
+    Reducer& operator=(Reducer &&) = default;
+
+    VType valop_m = VType{};
+
+    value_type *target = nullptr;
+
+    T *passthruval = nullptr;
+    IndexType *passthruindex = nullptr;
+
+    // This single-argument set() matches the basic data type Reducer::set()'s
+    // function signature to make the init/combine/resolve backend calls easier
+    // to maintain, but operates on the passthru variables instead
+    RAJA_HOST_DEVICE void set(value_type in) { *passthruval = in.val; *passthruindex = in.loc; }
+
     RAJA_HOST_DEVICE
     value_type &
-    getVal() { return valop_m.val.val; }
+    getVal() { return valop_m.val; }
 
 #if defined(RAJA_CUDA_ACTIVE) || defined(RAJA_HIP_ACTIVE) || defined(RAJA_SYCL_ACTIVE)
     // Device related attributes.
@@ -119,18 +161,26 @@ namespace detail
 } // namespace detail
 
 // Standard use case.
-template <template <typename, typename, typename> class Op, typename T, typename VType = ValOp<T, Op>>
+template <template <typename, typename, typename> class Op, typename T, typename IndexType = RAJA::Index_type,
+          std::enable_if_t<std::is_integral<T>::value || std::is_floating_point<T>::value> * = nullptr>
 auto constexpr Reduce(T *target)
 {
-  return detail::Reducer<Op, T, VType>(target);
+  return detail::Reducer<Op, T, IndexType, ValOp<T, Op>>(target);
 }
 
-// Pass through use case where reduction value and location are separate, non-ValLoc types.
-template <template <typename, typename, typename> class Op, typename T, typename IndexType = RAJA::Index_type, bool passingthru = true,
+// User-defined ValLoc case.
+template <template <typename, typename, typename> class Op, typename T, typename IndexType>
+auto constexpr Reduce(ValLoc<T, IndexType> *target)
+{
+  return detail::Reducer<Op, T, IndexType, ValOp<ValLoc<T, IndexType>, Op>>(target);
+}
+
+// Pass through use case where reduction value and location are separate, non-ValLoc types supplied by the user.
+template <template <typename, typename, typename> class Op, typename T, typename IndexType,
           std::enable_if_t<std::is_integral<T>::value || std::is_floating_point<T>::value> * = nullptr>
 auto constexpr ReduceLoc(T *target, IndexType *index)
 {
-  return detail::Reducer<Op, ValLoc<T, IndexType>, ValLocOp<T, IndexType, Op>, T, IndexType, passingthru>(target, index);
+  return detail::Reducer<Op, T, IndexType, ValOp<ValLoc<T, IndexType>, Op>>(target, index);
 }
 
 } // namespace expt
