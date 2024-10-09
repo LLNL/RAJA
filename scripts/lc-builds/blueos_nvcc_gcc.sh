84d9c9f34685c9680affa99db57ad09af169adce
Arturo Vargas <vargas45@llnl.gov>
2024-10-09 11:18:00 -0700

    optional benchmark

--- scripts/lc-builds/blueos_nvcc_gcc.sh
+++ scripts/lc-builds/blueos_nvcc_gcc.sh
@@ -9,22 +9,30 @@
 
 if [[ $# -lt 3 ]]; then
   echo
-  echo "You must pass 3 arguments to the script (in this order): "
+  echo "You must pass at least 3 arguments to the script (in this order): "
   echo "   1) compiler version number for nvcc"
   echo "   2) CUDA compute architecture (number only, not 'sm_70' for example)"
   echo "   3) compiler version number for gcc"
+  echo "   4) (optional) benchmarks On"
   echo
   echo "For example: "
-  echo "    blueos_nvcc_gcc.sh 10.2.89 70 8.3.1"
+  echo "    blueos_nvcc_gcc.sh 10.2.89 70 8.3.1 On"
   exit
 fi
 
 COMP_NVCC_VER=$1
 COMP_ARCH=$2
 COMP_GCC_VER=$3
-shift 3
 
-BUILD_SUFFIX=lc_blueos-nvcc${COMP_NVCC_VER}-${COMP_ARCH}-gcc${COMP_GCC_VER}
+if [[ $# -gt 3 ]]; then 
+  BENCHMARKS=$4
+  shift 4
+  BUILD_SUFFIX=lc_blueos-nvcc${COMP_NVCC_VER}-${COMP_ARCH}-gcc${COMP_GCC_VER}-benchmark_${BENCHMARKS}
+else
+  shift 3
+  BENCHMARKS=Off
+  BUILD_SUFFIX=lc_blueos-nvcc${COMP_NVCC_VER}-${COMP_ARCH}-gcc${COMP_GCC_VER}
+fi
 
 echo
 echo "Creating build directory build_${BUILD_SUFFIX} and generating configuration in it"
@@ -44,7 +52,7 @@ cmake \
   -C ../host-configs/lc-builds/blueos/nvcc_gcc_X.cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_CUDA=On \
-  -DENABLE_BENCHMARKS=On \
+  -DENABLE_BENCHMARKS=${BENCHMARKS} \
   -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-${COMP_NVCC_VER} \
   -DCMAKE_CUDA_COMPILER=/usr/tce/packages/cuda/cuda-${COMP_NVCC_VER}/bin/nvcc \
   -DCMAKE_CUDA_ARCHITECTURES=${COMP_ARCH} \
