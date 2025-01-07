6620b8d4d6c94b0bc26c0a2b4ed71264a4cfe0f6
Rich Hornung <hornung1@llnl.gov>
2025-01-07 11:53:32 -0800

    UPdate copyright statements in files to 2025

--- include/RAJA/policy/cuda/kernel/Lambda.hpp
+++ include/RAJA/policy/cuda/kernel/Lambda.hpp
@@ -10,7 +10,7 @@
  */
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Copyright (c) 2016-24, Lawrence Livermore National Security, LLC
+// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC
 // and RAJA project contributors. See the RAJA/LICENSE file for details.
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
