9963841a021ef1e6dea30a972a302cdfb293b9f7
Jason Burmark <burmark1@llnl.gov>
2025-02-10 10:25:10 -0800

    Allocate local mem with new[]/delete[]
    Sometimes the amount of memory is only known at runtime and
    dynamically sized local arrays are not allowed in c++.

--- include/RAJA/pattern/kernel/InitLocalMem.hpp
+++ include/RAJA/pattern/kernel/InitLocalMem.hpp
@@ -83,23 +83,15 @@ struct StatementExecutor<statement::InitLocalMem<RAJA::cpu_tile_mem,
         Pos, typename camp::decay<Data>::param_tuple_t>::value_type;
 
     // Initialize memory
-#ifdef RAJA_COMPILER_MSVC
-    // MSVC doesn't like taking a pointer to stack allocated data?!?!
     varType* ptr = new varType[camp::get<Pos>(data.param_tuple).size()];
     camp::get<Pos>(data.param_tuple).set_data(ptr);
-#else
-    varType Array[camp::get<Pos>(data.param_tuple).size()];
-    camp::get<Pos>(data.param_tuple).set_data(&Array[0]);
-#endif
 
     // Initialize others and execute
     exec_expanded<others...>(data);
 
     // Cleanup and return
     camp::get<Pos>(data.param_tuple).set_data(nullptr);
-#ifdef RAJA_COMPILER_MSVC
     delete[] ptr;
-#endif
   }
 
   template<typename Data>
