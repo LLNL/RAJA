84dcf32c3ffffe8bbb94abc92abb81e707d6cb70
Rich Hornung <hornung1@llnl.gov>
2025-02-20 15:51:10 -0800

    Squash unused arg/var warnings.S

--- include/RAJA/policy/sycl/launch.hpp
+++ include/RAJA/policy/sycl/launch.hpp
@@ -43,7 +43,7 @@ struct LaunchExecute<RAJA::sycl_launch_t<async, 0>>
       RAJA::expt::type_traits::is_ForallParamPack_empty<ReduceParams>>
   exec(RAJA::resources::Resource res,
        const LaunchParams& params,
-       const char* kernel_name,
+       const char* RAJA_UNUSED_ARG(ernel_name),
        BODY_IN&& body_in,
        ReduceParams& RAJA_UNUSED_ARG(launch_reducers))
   {
@@ -114,7 +114,7 @@ struct LaunchExecute<RAJA::sycl_launch_t<async, 0>>
           RAJA::expt::type_traits::is_ForallParamPack_empty<ReduceParams>>>
   exec(RAJA::resources::Resource res,
        const LaunchParams& launch_params,
-       const char* kernel_name,
+       const char* RAJA_UNUSED_ARG(kernel_name),
        BODY_IN&& body_in,
        ReduceParams launch_reducers)
   {
