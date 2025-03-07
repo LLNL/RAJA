9d1fa5ec490f7c2e8bb6e8914bc915164d89e0a2
Arturo Vargas <vargas45@llnl.gov>
2025-03-07 07:01:02 -0800

    add default name option

--- include/RAJA/pattern/params/kernel_name.hpp
+++ include/RAJA/pattern/params/kernel_name.hpp
@@ -3,6 +3,8 @@
 
 #include "RAJA/pattern/params/params_base.hpp"
 
+#define RAJA_KERNEL_NAME RAJA::expt::detail::defaultKernelName(__func__, __FILE__, __LINE__)
+
 namespace RAJA
 {
 namespace expt
@@ -10,6 +12,15 @@ namespace expt
 namespace detail
 {
 
+inline const char* defaultKernelName(const char *const func_name,
+                                     const char *const file_name,
+                                     const int line_number)
+{
+   std::string defaultName;
+   defaultName += std::string(func_name) +  " " + std::string(strrchr(file_name, '/')) + ":L" + std::to_string(line_number);
+   return defaultName.c_str();
+}
+  
 struct KernelName : public ForallParamBase
 {
   RAJA_HOST_DEVICE KernelName() {}
