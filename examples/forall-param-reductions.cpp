626d6e12df9df8b5c5f53bbaea31ba4a72e6e3e9
Arturo Vargas <vargas45@llnl.gov>
2025-03-28 16:15:46 -0700

    move KernelName around

--- examples/forall-param-reductions.cpp
+++ examples/forall-param-reductions.cpp
@@ -140,9 +140,9 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
     RAJA::expt::Reduce<RAJA::operators::maximum>(&seq_max),
     RAJA::expt::Reduce<RAJA::operators::minimum>(&seq_minloc),
     RAJA::expt::Reduce<RAJA::operators::maximum>(&seq_maxloc),
+    RAJA::expt::KernelName("RAJA Reduce Seq Kernel"),
     RAJA::expt::ReduceLoc<RAJA::operators::minimum>(&seq_min2, &seq_minloc2),
     RAJA::expt::ReduceLoc<RAJA::operators::maximum>(&seq_max2, &seq_maxloc2),
-    RAJA::expt::KernelName("RAJA Reduce Seq Kernel"),
     [=](int i,
         VALOP_INT_SUM &_seq_sum,
         VALOP_INT_MIN &_seq_min,
@@ -335,6 +335,7 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
   RAJA::Index_type cuda_maxloc2(-1);
 
   RAJA::forall<EXEC_POL3>(cuda_res, arange,
+    RAJA::expt::KernelName("RAJA Reduce CUDA Kernel"),
     RAJA::expt::Reduce<RAJA::operators::plus>(&cuda_sum),
     RAJA::expt::Reduce<RAJA::operators::minimum>(&cuda_min),
     RAJA::expt::Reduce<RAJA::operators::maximum>(&cuda_max),
@@ -342,7 +343,6 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
     RAJA::expt::Reduce<RAJA::operators::maximum>(&cuda_maxloc),
     RAJA::expt::ReduceLoc<RAJA::operators::minimum>(&cuda_min2, &cuda_minloc2),
     RAJA::expt::ReduceLoc<RAJA::operators::maximum>(&cuda_max2, &cuda_maxloc2),
-    RAJA::expt::KernelName("RAJA Reduce CUDA Kernel"),
     [=] RAJA_DEVICE ( int i,
                       VALOP_INT_SUM &_cuda_sum,
                       VALOP_INT_MIN &_cuda_min,
@@ -356,11 +356,11 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
       _cuda_min.min(d_a[i]);
       _cuda_max.max(d_a[i]);
 
-      _cuda_minloc.minloc(a[i], i);
-      _cuda_maxloc.maxloc(a[i], i);
+      _cuda_minloc.minloc(d_a[i], i);
+      _cuda_maxloc.maxloc(d_a[i], i);
 
-      _cuda_minloc2.minloc(a[i], i);
-      _cuda_maxloc2.maxloc(a[i], i);
+      _cuda_minloc2.minloc(d_a[i], i);
+      _cuda_maxloc2.maxloc(d_a[i], i);
     }
   );
 
