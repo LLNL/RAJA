c769ca8e01b48eae4c126dd376120b8cfc8770d2
Jason Burmark <burmark1@llnl.gov>
2025-04-04 10:57:21 -0700

    Add test for old deprecated plugin name
    Remember to remove when we remove support for the old plugin name

---
+++ test/integration/test_plugin_old_dynamic.cpp
@@ -0,0 +1,21 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+#include "RAJA/RAJA.hpp"
+#include "gtest/gtest.h"
+
+TEST(PluginTestDynamic, Exception)
+{
+  RAJA::util::init_plugins("../../lib/libdynamic_plugin_old.so");
+  int* a = new int[10];
+
+  ASSERT_ANY_THROW({
+    RAJA::forall<RAJA::seq_exec>(RAJA::RangeSegment(0, 10),
+                               [=](int i) { a[i] = 0; });
+  });
+
+  delete[] a;
+}
