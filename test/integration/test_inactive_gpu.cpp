681de235b61bf891c7a65ae767cf7f5e9df1d6a2
Jason Burmark <burmark1@llnl.gov>
2025-04-04 12:40:22 -0700

    Add hacky test for inactive gpu includes

---
+++ test/integration/test_inactive_gpu.cpp
@@ -0,0 +1,14 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "RAJA/RAJA.hpp"
+#include "gtest/gtest.h"
+
+TEST(InactiveTestGPU, Inactive)
+{
+
+}
