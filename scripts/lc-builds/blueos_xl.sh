5ee735d5ced8abf95868a19a43022ee3ee45ca35
john bowen <bowen36@llnl.gov>
2025-08-21 09:50:56 -0700

    Add clang format 14 to scripts

--- scripts/lc-builds/blueos_xl.sh
+++ scripts/lc-builds/blueos_xl.sh
@@ -38,7 +38,6 @@ cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_BENCHMARKS=On \
   -DENABLE_CLANGFORMAT=On \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCLANGFORMAT_EXECUTABLE=/usr/tce/packages/clang/clang-14.0.4/bin/clang-format \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
