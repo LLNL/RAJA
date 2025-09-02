95b909e4ab3bfa8edd60b391aad91a185217156b
Robert Chen <chen59@llnl.gov>
2025-09-02 16:24:59 -0700

    Hopefully running clang-format.

--- examples/plugin/test-plugin.cpp
+++ examples/plugin/test-plugin.cpp
@@ -7,13 +7,14 @@
 
 #include "RAJA/RAJA.hpp"
 
-int main(int RAJA_UNUSED_ARG(argc), char **RAJA_UNUSED_ARG(argv[]))
+int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 {
   double* a = new double[10];
 
-  for (int ii = 0; ii < 10; ii++) {
-    RAJA::forall<RAJA::seq_exec>(RAJA::RangeSegment(0,10), [=] (int i) {
-        a[i] = 0;
+  for (int ii = 0; ii < 10; ii++)
+  {
+    RAJA::forall<RAJA::seq_exec>(RAJA::RangeSegment(0, 10), [=](int i) {
+      a[i] = 0;
     });
   }
 }
